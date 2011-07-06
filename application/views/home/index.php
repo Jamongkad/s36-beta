06ac0c622d5066cadc6e0636745c7707f9e0a9d3
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-07-07 07:01:20 +0800

    committing to dev

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,12 +1,22 @@
 <div class="g4of5">
     
     <?foreach($feedback as $feed):?>
-        <div style="padding: 10px; background-color: #fff; border-bottom: solid 1px #e4e4e4 ">
-            <b><?=$feed->firstname?> <?=$feed->lastname?></b>
-            <p style="font-size: 0.8em"><?=$feed->text?></p>
-            <span class="light_blue">Status:</span><span><?=$feed->status?></span>
-            <span class="light_blue">Priority:</span><span><?=$feed->priority?></span>
-            <?=HTML::link('/', 'Modify Additional info')?> 
+        <div class="grids" style="padding: 10px; background-color: #fff; border-bottom: solid 1px #e4e4e4 ">
+            <div class="g1of2">
+                <div style="font-size: 0.9em; font-weight: bold"><?=$feed->firstname?> <?=$feed->lastname?></div>
+                <p style="font-size: 0.77em"><?=$feed->text?></p>
+                <span class="light_blue">Status:</span><span><?=$feed->status?></span>
+                <span class="light_blue">Priority:</span><span><?=$feed->priority?></span>
+                <?=HTML::link('/', 'Modify Additional info')?> 
+            </div>
+
+            <div class="g1of2">
+                <div style="font-size: 0.9em; font-weight: bold"><?=$feed->firstname?> <?=$feed->lastname?></div>
+                <p style="font-size: 0.77em"><?=$feed->text?></p>
+                <span class="light_blue">Status:</span><span><?=$feed->status?></span>
+                <span class="light_blue">Priority:</span><span><?=$feed->priority?></span>
+                <?=HTML::link('/', 'Modify Additional info')?> 
+            </div>
         </div>
     <?endforeach?>
 </div>
