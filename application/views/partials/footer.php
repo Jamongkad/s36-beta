775a0ff71e40e9447942dcc0c1af88b7e364b901
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-08-18 18:15:16 +0800

    refactored

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -9,16 +9,6 @@ if(S36Auth::check()):
         <h4>36Stories © 2011. </h4>
         <p>Keep in touch by following us on <a href="#">Facebook</a>, <a href="#">Twitter</a>, subscribing to our <a href="#">blog's feed</a> and joining our <a href="#">email newsletter</a>.</p>
     </div>
-
-    <?=HTML::script('js/jquery-1.6.2.min.js')?>
-    <?=HTML::script('js/jquery.tmpl.js')?>
-    <?=HTML::script('js/knockout-1.2.1.js')?>
-    <?=HTML::script('js/s36.js')?>
-    <?=HTML::script('js/Checky.js')?> 
-    <?=HTML::script('js/FeedbackBlockDisplay.js')?>
-    <?=HTML::script('js/DropDownChange.js')?>
-    <?=HTML::script('js/jquery.switcharoo.js')?>
-    <?//=HTML::script('js/s36-knockout.js')?>
 <?endif?>
 </div>
 </body>
--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -6,6 +6,16 @@
     <?=HTML::style('css/romanticc.css')?>
     <?=HTML::style('css/admin.css')?>
     <?=HTML::style('css/zebra_pagination.css')?>
+
+    <?=HTML::script('js/jquery-1.6.2.min.js')?>
+    <?=HTML::script('js/jquery.tmpl.js')?>
+    <?=HTML::script('js/knockout-1.2.1.js')?>
+    <?=HTML::script('js/s36.js')?>
+    <?=HTML::script('js/Checky.js')?> 
+    <?=HTML::script('js/FeedbackBlockDisplay.js')?>
+    <?=HTML::script('js/DropDownChange.js')?>
+    <?=HTML::script('js/jquery.switcharoo.js')?>
+    <?//=HTML::script('js/s36-knockout.js')?>
     </meta>    
 </head>
 <body>
