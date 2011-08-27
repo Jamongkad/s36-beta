13b310139c48e5779a161ca96456eea3af8881e8
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-08-28 02:39:35 +0800

    ok gently now...

--- application/models/contact.php
+++ application/models/contact.php
@@ -14,30 +14,6 @@ class Contact {
 
 
     public function insert_new_contact($opts) {
-        /*
-         $sql = "INSERT INTO Contact (siteId, firstName, lastName, email, countryId, position, city, companyName, website, avatar) 
-                              VALUES (:siteId, :firstName, :lastName, :email, :countryId, :position, :city, :companyName, :website, :avatar)";
-
-         $stmt = $this->dbh->prepare($sql);
-
-         $countryCode = DB::table('Country', 'master')->where('code', '=', $opts->countryId)->first();
-
-         $stmt->bindParam(':siteId', $opts->site_id);
-         $stmt->bindParam(':firstName', $opts->firstName);
-         $stmt->bindParam(':lastName', $opts->lastName);
-         $stmt->bindParam(':email', $opts->email);
-         $stmt->bindParam(':countryId', $countryCode->countryid);
-         $stmt->bindParam(':position', $opts->position);
-         $stmt->bindParam(':city', $opts->city);
-         $stmt->bindParam(':companyName', $opts->companyName);
-         $stmt->bindParam(':website', $opts->website);
-         $stmt->bindParam(':avatar', $opts->avatar);
-         $stmt->execute(); 
-        
-         //print_r($this->dbh->lastInsertId());
-         */
-
-         $countryCode = DB::table('Country', 'master')->where('code', '=', $opts->countryId)->first();
          $id = DB::table('Contact', 'master')->insert_get_id($opts);
          print_r($id);
     }
--- modules/api/routes.php
+++ modules/api/routes.php
@@ -69,28 +69,11 @@ return array(
           , 'website'   => Input::get('email')
           , 'avatar'    => Input::get('cropped_image_dir')
         );
+
+        $ct->insert_new_contact($contact_data);
         //print_r($contact_data);
-        $id = db::table('Contact')->insert_get_id($contact_data);
-        print_r($id);
-
-
-        //$ct->insert_new_contact($contact_data);
-        /*
-        $contact_data = Array(
-            'siteId'    => 1
-          , 'firstName' => "Mathew"
-          , 'lastName'  => "Wong"
-          , 'email'     => "wrm932@gmail.com"
-          , 'countryId' => 166
-          , 'position'  => "CTO"
-          , 'city'      => "Manila"
-          , 'companyName' => "Zenith Labs"
-          , 'website'   => "http://www.mathew.com"
-          , 'avatar'    => "penguin.png"
-        );
-        $id = db::table('Contact')->insert_get_id($contact_data);
-        print_r($id);
-        */
+        //$id = db::table('Contact')->insert_get_id($contact_data);
+        //print_r($id);
     },
 
     'GET /api/test_blob' => function() {
