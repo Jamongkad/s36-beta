8c07947bc0022a28599035bd78996fa7342c84ed
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-08-31 09:19:59 +0800

    removed contact creation test

--- application/routes/dashboard.php
+++ application/routes/dashboard.php
@@ -4,24 +4,4 @@ return array(
     'GET /dashboard' => Array('name' => 'dashboard', 'before' => 's36_auth', 'do' => function() {
         return View::of_layout()->partial('contents', 'dashboard/index');
     }),
-
-    'GET /dashboard/test' => function() {  
-        $ct = new Contact;
-        //fuck naive assumption...
-        $contact_data = Array(
-            'siteId'    => 1
-          , 'firstName' => "Mathew"
-          , 'lastName'  => "Wong"
-          , 'email'     => "wrm932@gmail.com"
-          , 'countryId' => 166
-          , 'position'  => "CTO"
-          , 'city'      => "Manila"
-          , 'companyName' => "Zenith Labs"
-          , 'website'   => "http://www.mathew.com"
-          , 'avatar'    => "penguin.png"
-        );
-        //$ct->insert_new_contact($contact_data);
-        $id = db::table('Contact')->insert_get_id($contact_data);
-        print_r($id);
-    },
 );
