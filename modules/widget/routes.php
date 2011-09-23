70ba829ee43150c04d39f7c63eaea45ebaf459bd
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-09-23 16:24:14 +0800

    testing embedded widget

--- modules/widget/routes.php
+++ modules/widget/routes.php
@@ -11,23 +11,60 @@ return array(
         ));
     },
 
+    'GET /widget/embedded' => function() {
+
+        $feedback = new Feedback;
+
+        $company_id = null;
+        $site_id = null;
+        $is_published = 0;
+        $is_featured = 0;
+        $limit = 10;
+        $offset = 0;
+        
+        if(Input::get('company_id')) $company_id = (int)Input::get('company_id'); 
+
+        if(Input::get('site_id')) $site_id = (int)Input::get('site_id');
+
+        if(Input::get('offset')) $offset = (int)Input::get('offset');
+        
+        if(Input::get('limit')) $limit = (int)Input::get('limit');   
+        
+        if(Input::get('is_published')) $is_published = (int)Input::get('is_published');   
+        
+        if(Input::get('is_featured')) $is_featured = (int)Input::get('is_featured');   
+       
+        $params = Array(
+            'company_id'   => $company_id
+          , 'site_id'      => $site_id
+          , 'is_published' => $is_published
+          , 'is_featured'  => $is_featured
+          , 'limit'        => $limit
+          , 'offset'       => $offset
+        );
+        
+        $data = $feedback->pull_feedback_by_company($params);
+
+        return View::make('widget::widget_embedded_view', array( 
+            'feedback'   => $data
+          , 'units'		 => Input::get('units') ? Input::get('units') : 3
+          , 'transition' => Input::get('transition') ? Input::get('transition') : 'scrollVert'
+          , 'speed'      => Input::get('speed') ? Input::get('speed') : 500
+          , 'timeout'    => Input::get('timeout') ? Input::get('timeout') : 5000
+          , 'type'       => Input::get('type') ? Input::get('type') : 'horizontal'
+        ));
+    },
+
     'GET /widget/form/crop' => function() { 
 
-        $fb_login = $_GET['fb_login']; 
-        $x  = $_GET['x_coords'];
-        $y  = $_GET['y_coords'];
-        $wd = $_GET['wd'];
-        $ht = $_GET['ht'];		
-        $src = $fb_login ? $_GET['src'] : "/var/www/s36-beta/public/".$_GET['src'];
-        $ophoto = $_GET['oldphoto'];
+        $fb_login = Input::get('fb_login');
+        $x  = Input::get('x_coords');
+        $y  = Input::get('y_coords');
+        $wd = Input::get('wd');
+        $ht = Input::get('ht');
+        $src = $fb_login ? Input::get('src') : "/var/www/s36-beta/public/".Input::get('src');
+        $ophoto = Input::get('oldphoto');
         
-        /*
-        if((strlen(trim($ophoto)) > 0) || ($ophoto == "0")){
-            @unlink("/var/www/s36-upload-images/uploaded_cropped/150x150/".$ophoto);
-            @unlink("/var/www/s36-upload-images/uploaded_cropped/48x48/".$ophoto);		
-        }
-        */
-
         if($ophoto != 0){
             @unlink("/var/www/s36-upload-images/uploaded_cropped/150x150/".$ophoto);
             @unlink("/var/www/s36-upload-images/uploaded_cropped/48x48/".$ophoto);		
@@ -36,37 +73,34 @@ return array(
         $targ_w = $targ_h = 150;
         $jpeg_quality = 100;
                     
-                    if(strstr(strtolower($src),"graph.facebook.com")){
-                        $extension = ".jpg";
-                    }else{
-                        $extension = strtolower(strrchr($src, '.'));
-                    }
-                    
-                    switch($extension)
-                    {
-                        case '.jpg':
-                        case '.jpeg':
-                            $img_r150 = @imagecreatefromjpeg($src);
-                            $img_r48 = @imagecreatefromjpeg($src);
-                            break;
-                        case '.gif':
-                            $img_r150 = @imagecreatefromgif($src);
-                            $img_r48 = @imagecreatefromgif($src);
-                            break;
-                        case '.png':
-                            $img_r150 = @imagecreatefrompng($src);
-                            $img_r48 = @imagecreatefrompng($src);
-                            break;
-                        default:
-                            $img_r150 = false;
-                            $img_r48 = false;
-                            break;
-                    }
-                    
-        $dst_r150 = ImageCreateTrueColor( $targ_w, $targ_h );
+        if(strstr(strtolower($src),"graph.facebook.com")){
+            $extension = ".jpg";
+        }else{
+            $extension = strtolower(strrchr($src, '.'));
+        }
         
-        $dst_r48 = ImageCreateTrueColor( 48, 48 );
-        
+        switch($extension) {
+            case '.jpg':
+            case '.jpeg':
+                $img_r150 = @imagecreatefromjpeg($src);
+                $img_r48 = @imagecreatefromjpeg($src);
+                break;
+            case '.gif':
+                $img_r150 = @imagecreatefromgif($src);
+                $img_r48 = @imagecreatefromgif($src);
+                break;
+            case '.png':
+                $img_r150 = @imagecreatefrompng($src);
+                $img_r48 = @imagecreatefrompng($src);
+                break;
+            default:
+                $img_r150 = false;
+                $img_r48 = false;
+                break;
+        }
+                    
+        $dst_r150 = ImageCreateTrueColor( $targ_w, $targ_h ); 
+        $dst_r48 = ImageCreateTrueColor( 48, 48 ); 
         $date = date("mdyhis");
         
         $out150 = "/var/www/s36-upload-images/uploaded_cropped/150x150/".$date."-cropped.jpg";
@@ -133,8 +167,7 @@ return array(
                 $maxwidth = 350;
                 $maxheight = 230;
                 $move = move_uploaded_file($_FILES[$file]['tmp_name'],"/var/www/s36-upload-images/uploaded_tmp/".$filename);
-                if($move){
-                    
+                if($move){    
                      //start image resizing..
                      $resizeObj = new Resize($filedir);
                      $resizeObj->resizeImage($maxwidth, $maxheight, 'auto');
@@ -143,15 +176,13 @@ return array(
                      //get the optimal dimensions
                      $dims = $resizeObj->getDimensions($maxwidth, $maxheight, 'auto'); 
                      $width = $dims['optimalWidth'];
-                }
-                
-        }
-        
-            echo json_encode(Array(
-                "error" => $error
-              , "dir" => $filedir
-              , "wid" => $width
-            ));
+                }       
+        } 
 
-        }
+        echo json_encode(Array(
+            "error" => $error
+          , "dir" => $filedir
+          , "wid" => $width
+        ));
+    }
 );
