8fd0d7f0443fd6eeb70b1a7e7037ac11414c7f1f
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-10-11 13:56:29 +0800

    testing email

---
+++ application/models/email.php
@@ -0,0 +1,63 @@
+<?php
+
+class Email {
+
+    private $postmark, $feedback;  
+
+    public function __construct() { 
+        $this->postmark = new PostMark("11c0c3be-3d0c-47b2-99a6-02fb1c4eed71", "news@36stories.com");
+    }
+
+    public function process_email(EmailFixture $email) { 
+        if($email) {
+
+            foreach($email->get_addresses() as $em) { 
+                if($this->postmark->to($em->email)
+                                  ->subject("36Stories Feedback Notification")
+                                  ->html_message($email->get_message())
+                                  ->send()){
+                    echo "Message sent";
+                } else {
+                   echo "Message not sent";
+                }
+            }
+
+        } else {
+           throw new Exception("No email object found!");
+        }
+    }
+
+}
+
+abstract class EmailFixture {
+
+    private $addresses, $feedback_data;    
+
+    public function __construct($addresses, $feedback_data) {
+        $this->addresses = $addresses; 
+        $this->feedback_data = $feedback_data;
+    }
+    
+    public function get_addresses() {}
+    public function get_message() {}
+}
+
+class NewFeedbackSubmission extends EmailFixture {
+    
+    public function __construct($addresses, $feedback_data) {
+        $this->addresses = $addresses; 
+        $this->feedback_data = $feedback_data;
+    }
+
+    public function get_addresses() {
+        return $this->addresses;
+    }
+
+    public function get_message() {
+        return View::make('email/new_feedback_submission_view', 
+            Array(
+                'feedback_data' => $this->feedback_data
+              , 'addresses' => $this->addresses
+        ));     
+    }
+}
---
+++ application/views/email/new_feedback_submission_view.php
@@ -0,0 +1,123 @@
+<body bgcolor="#dde0e3" style="padding:0;margin:0;font-size:14px;font-family:Arial, Helvetica, sans-serif;">
+<table bgcolor="#dde0e3" width="716" cellpadding="0" cellspacing="0">
+	<tr>
+    	<td style="padding:30px 30px 10px;">
+        	<table bgcolor="white" style="background: #fff" cellpadding="0" cellspacing="0">
+            	<tr><td style="padding:20px;" cellpadding="0" cellspacing="0">
+                	<table width="616" cellpadding="0" cellspacing="0">
+                    	<!-- header -->
+                        <tr>
+                        	<td width="33.33%"><?=HTML::image('img/36storieslogo.jpg')?>
+                            </td><td width="33.33%"></td><td width="33.33%" align="right"></td>
+                        </tr>
+                        <tr height="20">
+                        	<td colspan="3"></td>
+                        </tr>
+                        <tr height="1" bgcolor="#e7e9eb">
+                        	<td colspan="3"></td>
+                        </tr>
+                        <tr height="20">
+                        	<td colspan="3"></td>
+                        </tr>
+                        
+                        <!-- end of header -->
+                        <!-- contents -->
+                        <tr>
+                        	<td colspan="3">
+                            	<table>
+                                	<tr>
+                                    	<td><?=HTML::image('uploaded_cropped/48x48/'.$feedback_data->avatar)?></td>
+                                        <td width="10"></td>
+                                        <td style="font-size:12px;">
+                                        	<h2 style="color:#0078aa;margin:0px;"><?=$feedback_data->firstname?> <?=$feedback_data->lastname?></h2>
+                                            <strong><?=$feedback_data->position?></strong><br />
+                                            <?=$feedback_data->companyname?><br />
+                                            <a href="#" target="_blank"><?=$feedback_data->url?></a><br />
+                                            <?=$feedback_data->city?> 
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr height="20">
+                        	<td colspan="3"></td>
+                        </tr>
+                        <tr>
+                        	<td colspan="3" style="line-height:20px;">
+                                <?=$feedback_data->text?> 
+                                <br /><br />
+                                
+                                <?=$feedback_data->permission?> 
+                                <?if($feedback_data->permission == "FULL PERMISSION"):?>
+                                <div style="padding:15px 20px 15px 70px;font-size:12px;background:#d9ebd6 url(<?=URL::to('/')?>img/ico-large-check.png) no-repeat left center;-webkit-border-radius:8px;-moz-border-radius:8px;border-radius:8px;">
+                                	<?=$feedback_data->firstname?> has granted you FULL permission to quote his/her feedback and profile as a quote in your website and marketing collaterals.
+                                </div>
+                                <?endif?>
+
+                                <?if($feedback_data->permission == "LIMITED PERMISSION"):?>
+                                <div style="padding:15px 20px 15px 70px;font-size:12px;background:#d9ebd6 url(<?=URL::to('/')?>img/ico-large-check.png) no-repeat left center;-webkit-border-radius:8px;-moz-border-radius:8px;border-radius:8px;">
+                                	<?=$feedback_data->firstname?> has granted you LIMITED permission to quote his/her feedback and profile as a quote in your website and marketing collaterals.
+                                </div>
+                                <?endif?>
+
+                                <?if($feedback_data->permission == "PRIVATE"):?>
+                                <div style="padding:15px 20px 15px 70px;font-size:12px;background:#d9ebd6 url(<?=URL::to('/')?>img/ico-large-check.png) no-repeat left center;-webkit-border-radius:8px;-moz-border-radius:8px;border-radius:8px;">
+                                	<?=$feedback_data->firstname?> has asked you to keep his/her feedback and profile PRIVATE. 
+                                </div>
+                                <?endif?>
+                                
+                                <br />
+                                
+                                <a href="#" style="text-decoration:none;margin-right:10px;font-size:11px;background:#ccf2cd url(<?=URL::to('/')?>img/ico-check.png) no-repeat left center;padding:7px 20px 7px 35px;color:#464646;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;">
+                                Publish this feedback now
+                                </a> 
+                                <a href="#" style="text-decoration:none;margin-right:10px;font-size:11px;background:#d2dbe1 url(<?=URL::to('/')?>img/ico-manage.png) no-repeat left center;padding:7px 20px 7px 35px;color:#464646;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;">
+                                Manage Feedback
+                                </a>
+                                
+                            </td>
+                        </tr>
+                        <tr height="80">
+                        	<td colspan="3"></td>
+                        </tr>
+                        
+                        <!-- end of contents -->
+                        
+                        <!-- sig -->
+                        <tr>
+                        	<td colspan="3">
+                            				Thanks, <br />
+											The 36Stories Team
+                            </td>
+                        </tr>
+                        <tr height="20">
+                        	<td colspan="3"></td>
+                        </tr>
+                        <tr>
+                        	<td colspan="3"><a href="#" style="color:#6e8cca;">No - I am not interested, do not ever mail me again.</a></td>
+                        </tr>
+                        <tr height="20">
+                        	<td colspan="3"></td>
+                        </tr>
+                        
+                        <!-- end sig -->
+                    </table>
+                </td></tr>
+            </table>
+        </td>
+    </tr>
+    <!-- footer -->
+    <tr>
+        <td style="font-size:10px;padding:0px 30px;line-height:16px;">
+            This message was intended for
+            <?foreach($addresses as $address):?>
+                <?=$address->email?>
+            <?endforeach?>
+            <br/> 
+            If you do not wish to receive this type of email from 36Stories in the future, please click here to unsubscribe.<br />
+            36Stories, Inc. P.O. Box 10005, Palo Alto, CA 94303
+       </td>
+    </tr>
+    <!-- end footer -->    
+</table>
+</body>
