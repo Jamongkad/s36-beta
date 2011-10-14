c2467f506d2635b39f51b1e77ae17f3e5b989a57
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-10-14 18:38:53 +0800

    updating

--- application/routes/control.php
+++ application/routes/control.php
@@ -6,10 +6,10 @@ return array(
         $password_string = "p455w0rd";
         $password = crypt($password_string);
 
-        return DB::table("User", "master")->where('username', '=', 'nicholas')
+        return DB::table("User", "master")->where('username', '=', 'dan')
                                           ->update(Array(
                                               'password' => $password
-                                            , 'encryptString' => $encrypt->encrypt("tofumonkey@gmail.com"."|".$password_string)
+                                            , 'encryptString' => $encrypt->encrypt("budocski15@gmail.com"."|".$password_string)
                                           ));
 
     },
