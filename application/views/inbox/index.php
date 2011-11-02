3cec9c543656f54512361b71d71b6a5950e76a40
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-11-02 16:41:15 +0800

    fucking shit

--- application/views/inbox/index.php
+++ application/views/inbox/index.php
@@ -26,7 +26,7 @@
                                 <input type="button" class="check" 
                                 <?=Helpers::switchable($feed->ispublished, $id, URL::to('/feedback/change_feedback_state'), ' style="background-position: 0px bottom"') ?>/>
                             <?else:?>
-                                <input type="button" class="check" tooltip="blah"/>
+                                <input type="button" class="check" tooltip="This Feedback cannot be published."/>
                             <?endif?>
                             <input type="button" class="save fileas" />
                             <div class="base-popup category-picker-holder">
