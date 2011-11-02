d2741d2ca82af83accb42f00f9fe0ce728775952
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-11-02 13:09:34 +0800

    heh

--- application/views/email/new_feedback_submission_view.php
+++ application/views/email/new_feedback_submission_view.php
@@ -26,8 +26,10 @@
                         	<td colspan="3">
                             	<table>
                                 	<tr>
-                                    	<td><?=HTML::image('uploaded_cropped/150x150/'.$feedback_data->avatar, 'Avatar', array('width' => '91', 'height' => '91'))?></td>
-                                        <td width="10"></td>
+                                        <?if($feedback_data->avatar):?>
+                                            <td><?=HTML::image('uploaded_cropped/150x150/'.$feedback_data->avatar, 'Avatar', array('width' => '91', 'height' => '91'))?></td>
+                                            <td width="10"></td>
+                                        <?endif?>
                                         <td style="font-size:12px;">
                                         	<h2 style="color:#0078aa;margin:0px;"><?=$feedback_data->firstname?> <?=$feedback_data->lastname?></h2>
                                             <strong><?=$feedback_data->position?></strong><br />
