25a13d9b9d9f636c5a9e7877c79774251bcab416
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-11-05 14:25:55 +0800

    debugging

--- application/routes/admin.php
+++ application/routes/admin.php
@@ -3,11 +3,16 @@
 return array(
     'GET /admin' => Array('name' => 'admin', 'before' => 's36_auth', 'do' => function() {
         $company_id = S36Auth::user()->companyid;
+        $admins = DB::Table('User', 'master')
+                      ->join('AuthAssignment', 'AuthAssignment.userId', '=', 'User.userId')
+                      ->where('User.companyId', '=', $company_id)->get()
+
+        Helpers::show_data($admins);
+        /*
         return View::of_layout()->partial('contents', 'admin/admin_index_view', Array(
-            'admins' => DB::Table('User', 'master')
-                          ->join('AuthAssignment', 'AuthAssignment.userId', '=', 'User.userId')
-                          ->where('User.companyId', '=', $company_id)->get()
+            'admins' => $admins
         ));
+        */
     }),
 
     'GET /admin/add_admin' => Array('name' => 'admin', 'before' => 's36_auth', 'do' => function() {
