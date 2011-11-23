7d11e85a533bb1a6209e9ab1092bbeedff4dea45
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-11-23 12:10:10 +0800

    whoops knew I forget something

---
+++ application/views/inbox/contacts_inbox_view.php
@@ -0,0 +1,28 @@
+<?=$metrics?>
+<div class="admin-sorter-bar">
+
+<table cellpadding="2" width="100%">
+    <tr>
+        <td width="10"></td>
+        <td width="10"><img src="images/avatar-matthew.png" /></td>
+        <td valign="middle"><strong>Mathew Wong</strong></td>
+        <td align="right"><a href="#">Back to Contacts</a></td>
+        <td width="10"></td>
+    </tr>
+</table>
+<div class="sorter-bar">
+    <div class="left">
+        &nbsp;
+    </div>
+    <div class="right">
+        <div class="g4of5">
+            &nbsp;
+        </div>
+        <div class="g1of5">     
+            &nbsp;
+        </div>
+    </div>
+    <div class="c"></div>
+</div>
+</div>
+<!-- end of top blue bar with filter options -->
---
+++ application/views/partials/contact_metricbar_view.php
@@ -0,0 +1,7 @@
+<div class="block">
+    <p class="small">
+        Total contacts:<span class="count"><?=$contact_count->total_rows?></span>          
+        Total requests sent:<span class="count">0</span>          
+        Total responses:     <span class="count">0</span>
+    </p> 
+</div>
