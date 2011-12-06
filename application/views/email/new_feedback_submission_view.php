f1eb0b8498358934e410793f186de174759d034f
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-12-06 13:30:50 +0800

    fixing email

--- application/views/email/new_feedback_submission_view.php
+++ application/views/email/new_feedback_submission_view.php
@@ -83,7 +83,7 @@
                                 <?endif?>
                                 
                                 <br />
-                                <?if($feedback_data->str_rating != "POOR"):?>
+                                <?if($feedback_data->str_rating != "POOR" && $feedback_data->permission != "PRIVATE"):?>
                                     <a href="<?=URL::to("api/publish?params=".rawurlencode($user->encryptstring)."&feedback_id={$feedback_data->id}&company_id={$user->companyid}")?>" style="text-decoration:none;margin-right:10px;font-size:11px;background:#ccf2cd;padding:7px 20px 7px 2px;color:#464646;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;">
                                     <?=HTML::image('img/ico-check.png', 'Icon Check', array('style' => 'vertical-align:middle'))?>
                                     <span style="vertical-align: middle">Publish this feedback now</span>
