8870227bba8a8c0971a2de2d6ee446b6c09244e5
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-12-21 18:09:03 +0800

    asd

--- application/routes/dashboard.php
+++ application/routes/dashboard.php
@@ -2,7 +2,7 @@
 
 return array(
     'GET /dashboard' => Array('name' => 'dashboard', 'before' => 's36_auth', 'do' => function() {
-        print_r(Config::get('application.deploy_env'));
+        //print_r(Config::get('application.deploy_env'));
         return View::of_layout()->partial('contents', 'dashboard/index');
     }),
 );
