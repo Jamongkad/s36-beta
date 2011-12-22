a0cd8067aae60332c83ca9c077ed3497e3591d8a
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-12-22 19:10:08 +0800

    debugging

--- application/routes/api.php
+++ application/routes/api.php
@@ -55,6 +55,7 @@ return array(
         $key = Config::get('application.name');
         
         //decrypt string use username and password to authenticate into application. 
+        print_r($params);
         if($key != null && S36Auth::login($params[0], $params[1])) {  
 
             $user = new DBUser; 
