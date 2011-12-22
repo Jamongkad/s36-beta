965ec6efd1fa01465885ab3427f04e016182d9de
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-12-22 11:54:52 +0800

    making ryan account

--- application/models/dbaccount.php
+++ application/models/dbaccount.php
@@ -15,17 +15,20 @@ class DBAccount extends S36DataObject {
         
         $encrypt = new Crypter;
         $password_string = "p455w0rd";
-        $password = crypt($password_string);
-        $email = "wrm932@gmail.com";
+
+        $password = $this->escape(crypt($password_string));
+        $email = $this->escape("ryanchua6@gmail.com");
+        $encrypt_string = $this->escape($encrypt->encrypt($email."|".$password));
+        $company = $this->escape("ryan");
 
         $this->dbh->beginTransaction();
-        $this->dbh->query('INSERT INTO Company (`name`, `planid`) VALUES("TheMathewClan", 1)');
+        $this->dbh->query('INSERT INTO Company (`name`, `planid`) VALUES("'.$company.'", 1)');
         $this->dbh->query('SET @company_id = LAST_INSERT_ID()');
         $this->dbh->query('INSERT INTO Metric (`companyId`, `totalRequest`, `totalResponse`) VALUES(@company_id, 0, 0)'); 
         $this->dbh->query('INSERT INTO Site (`companyId`, `domain`, `name`, `defaultFormId`) VALUES(@company_id, "www.mathewklan.com", "mathewklan", 1)');          
         $this->dbh->query('SET @site_id = LAST_INSERT_ID()');
         $this->dbh->query('INSERT INTO User (`companyId`, `username`, `confirmed`, `password`, `encryptString`, `email`, `fullName`, `title`, `imId`)  
-                           VALUES (@company_id, "mathew", 1, "'.$this->escape($password).'", "'.$this->escape($encrypt->encrypt($email."|".$password_string)).'", "'.$this->escape($email).'", "Mathew Wong", "CEO", 1)');
+                           VALUES (@company_id, "mathew", 1, "'.$password.'", "'.$encrypt_string.'", "'.$email.'", "Mathew Wong", "CEO", 1)');
         $this->dbh->query('SET @user_id = LAST_INSERT_ID()');
         $this->dbh->query('INSERT INTO AuthAssignment (`itemname`, `userid`) VALUES ("Admin", @user_id)');
         $this->dbh->query('INSERT INTO FeedbackBlock (`siteId`, `themeId`, `formId`) VALUES(@site_id, 1, 1)');
