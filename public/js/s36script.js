7aa408a9e35ca60dd076f1de7e86b4f18128f610
Mathew Wong <mathew@ubuntu-11-04.(none)>
2011-12-22 16:03:31 +0800

    pwet

--- public/js/s36script.js
+++ public/js/s36script.js
@@ -312,6 +312,7 @@ function fb_connect_success(obj){
                         $(this).selected(true);
                     }
                 });
+                console.log("location success!");
                 console.log(loc);
 			}
 	
