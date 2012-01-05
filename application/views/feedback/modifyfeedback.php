22934b81f37fe6c6b6e0f367d4ed3151b9d0540c
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-05 17:57:05 +0800

    no

--- application/views/feedback/modifyfeedback.php
+++ application/views/feedback/modifyfeedback.php
@@ -99,7 +99,7 @@
             </div>
         </div>
         <div class="g1of3">
-                <table cellpadding="2" class="feedback-data-table">
+                <table cellpadding="2" class="feedback-data-table user-info">
                  <span id="toggle_url" hrefaction="<?=URL::to('/feedback/toggle_feedback_display')?>"></span>
                  <tr><td colspan="2" class="header">Display Information</td><td>display?</td></tr>
                  <tr>
