e3c7105a2b135fcdc575b6fc1fa6ba1ab6cf632e
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-14 14:29:03 +0800

    ok

--- application/routes/dashboard.php
+++ application/routes/dashboard.php
@@ -4,21 +4,15 @@ return array(
     'GET /dashboard' => Array('name' => 'dashboard', 'before' => 's36_auth', 'do' => function() {
         //print_r(Config::get('application.deploy_env'));
 
-        $arr_data = Array(
-            Array('Germany', 100) 
-          , Array('United States', 450)
-          , Array('Philippines', 290)
-          , Array('Singapore', 780)
-          , Array('Thailand', 659)
-          , Array('Vietnam', 500)
-          , Array('China', 300) 
-          , Array('China', 300)
-        );
-
-        $geo_chart_data = json_encode($arr_data);
-
+        $dash = new DBDashboard; 
+        $dash->company_id = S36Auth::user()->companyid;
+        $dashboard_summary = $dash->pull_summary();
+        
+        Helpers::show_data($dashboard_summary);
+        /*
         return View::of_layout()->partial('contents', 'dashboard/dashboard_index_view', Array(
-            'geo_chart_data' => $geo_chart_data
+            'dashboard_summary' => $dashboard_summary
         ));
+        */
     }),
 );
