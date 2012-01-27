30a67f2333d4eaac83f324744b7d185c1bf89ab6
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-27 17:17:35 +0800

    asdas

--- modules/widget/views/widget_embedded_hor_view.php
+++ modules/widget/views/widget_embedded_hor_view.php
@@ -376,18 +376,15 @@
                 </div>
                 <div class="feedbackInfo">
                     <div class="feedbackAuthorInfo">
-                    	<span class="name">Chris Davidson </span>
-                    	<span class="position">Front End Developer, </span>
-                        <span class="company"> 36Stories</span> 
-                        <span class="city"> Manila City </span>
-                        <span class="country"> Philippines</span>
+                    	<span class="name"></span>
+                    	<span class="position"></span>
+                        <span class="company"></span> 
+                        <span class="city"></span>
+                        <span class="country"></span>
                         <span class="flag flag-ph"></span>
-                        <span class="date">13th Sept. 2011</span>
+                        <span class="date"></span>
                     </div>
                     <div class="feedbackText">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                     <div class="feedbackOptions">
                         <div class="feedbackSocial">
