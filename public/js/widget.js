680f6eda946a0fb93a04ddda79e10641f840c183
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-28 14:28:53 +0800

    changed

--- public/js/widget.js
+++ public/js/widget.js
@@ -1,7 +1,8 @@
+/*
 function s36Widget(){
 	this.frameUrl = '',
 	this.init = function(params){
-        console.log(params);
+         
 		//this.generateWidgetUrl();
 	},
 	this.generateWidgetUrl = function(){
@@ -16,3 +17,22 @@ function s36Widget(){
         document.write(this.generateFrameMarkup());
     }	
 }
+*/
+function WidgetLoader(opts) {
+    this.company_id = opts.companyId;
+    this.username = opts.user;
+    this.widget_id = opts.widgetId;
+}
+
+WidgetLoad.prototype.generateFrameMarkup = function() {
+    var that = this;    
+    var frameUrl = 'http://razer.gearfish.com/widget/embedded_proto/'+that.widget_id+'/'+that.username+'/'+that.company_id+'/';
+    var src = '<span style="z-index:100001"><iframe id="s36Widget" allowTransparency="true" height="300" frameborder="0" scrolling="no" style="width:100%;border:none;overflow:visible;" src="'+frameUrl+'"><a href="javascript:;" title="html form">Fill out my Wufoo form!</a></iframe></span>';
+    return src;
+}
+
+WidgetLoader.prototype.display = function() { 
+    document.write(this.generateFrameMarkup());
+}
+
+
