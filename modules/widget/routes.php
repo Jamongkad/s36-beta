c18f68c9fb09eba15d4d6674932a2d26e014f98f
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-28 17:04:54 +0800

    asd

--- modules/widget/routes.php
+++ modules/widget/routes.php
@@ -80,14 +80,17 @@ return array(
         $user_obj->username = $username;
         $user_obj->company_id = $company_id;
         $user_result = $dbu->pull_user($user_obj);
-        print_r($user_result);
-        /*
+
+
         if($user_result) { 
             $dbw = new DBWidget;
             $widget_obj = $dbw->fetch_widget_by_id($widget_id); 
+            print_r($widget_obj);
+            /*
             $obj = base64_decode($widget_obj->widgetobjstring);
             $obj = unserialize($obj); 
-
+            */
+            /*
             $params = Array(
                 'company_id'   => $obj->company_id
               , 'site_id'      => $obj->site_id
@@ -117,12 +120,11 @@ return array(
             }
 
             return View::of_widget_layout()->partial('contents', $widget_view, Array('result' => $data));
-
-
+            */
         } else {
             throw new Exception("Invalid Widget paramaters!");
         }
-        */
+
     },
 
     'GET /widget/js_output' => function() {
