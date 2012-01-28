7395a9438604ac04c64e4d5b6d166859835ddd76
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-28 14:51:58 +0800

    asd

--- public/js/widget.js
+++ public/js/widget.js
@@ -7,7 +7,7 @@ function WidgetLoader(opts) {
 WidgetLoader.prototype.generateFrameMarkup = function() {
     var that = this;    
     var frameUrl = 'http://razer.gearfish.com/widget/widget_loader/'+that.widget_id+'/'+that.username+'/'+that.company_id+'/';
-    var src = '<span style="z-index:100001"><iframe id="s36Widget" allowTransparency="true" height="300" frameborder="0" scrolling="no" style="width:100%;border:none;overflow:visible;" src="'+frameUrl+'"><a href="javascript:;" title="html form">Fill out my Wufoo form!</a></iframe></span>';
+    var src = '<span style="z-index:100001"><iframe id="s36Widget" allowTransparency="true" height="300" frameborder="0" scrolling="no" style="width:100%;border:none;overflow:visible;" src="'+frameUrl+'">Insomnia wooohooooh</iframe></span>';
     return src;
 }
 
