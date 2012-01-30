4c9ed475614d459adf72cd6d2894d56b92c61ef7
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-30 19:10:51 +0800

    asda

--- application/models/widgetloader.php
+++ application/models/widgetloader.php
@@ -40,13 +40,31 @@ class WidgetLoader {
             $widget_view = "widget::widget_modal_popup_view";
         }
 
+        $fixed_data = Array();
+        foreach($data->result as $rows) {
+           if($rows->indlock == 1) { 
+               $feed_rules = $obj->perms;    
+           } else {
+               $feed_rules = new StdClass;
+               $feed_rules->displayname     = $rows->displayname;
+               $feed_rules->displayimg      = $rows->displayimg;
+               $feed_rules->displaycompany  = $rows->displaycompany;
+               $feed_rules->displayposition = $rows->displayposition;
+               $feed_rules->displayurl      = $rows->displayurl;
+               $feed_rules->displaycountry  = $rows->displaycountry;
+               $feed_rules->displaysbmtdate = $rows->displaysbmtdate;
+            }
+           $rows->rules = $feed_rules;
+           $fixed_data[] = $rows;
+        }
+
         $wd = new WidgetDelivery;         
         $wd->widget_view = $widget_view;
-        $wd->data = $data;
+        $wd->widget_data = $fixed_data;
         return $wd;
     }
 }
 
 class WidgetDelivery {
-    public $data, $widget_view;
+    public $widget_data, $widget_view;
 }
