fbf2eebef01034da2b8efc8ca45eef1904ccd523
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-01-30 16:37:37 +0800

    asdas

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -219,12 +219,12 @@ return array(
     'GET /tests/widget_data' => function() {
         $dbw = new DBWidget;
         //$widget_obj_modal = $dbw->fetch_widget_by_id('c2wu9'); 
-        $widget_obj_embed = $dbw->fetch_widget_by_id('iuz0h'); 
+        $widget_obj_embed = $dbw->fetch_widget_by_id('qtg3d'); 
         $obj = base64_decode($widget_obj_embed->widgetobjstring);
         $obj = unserialize($obj);
 
         //global widget rules
-        Helpers::show_data($obj->perms);
+        Helpers::show_data($obj);
 
         $params = Array(
             'company_id'   => $obj->company_id
