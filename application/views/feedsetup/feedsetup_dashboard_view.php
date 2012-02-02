df6693c3bca80c4edba8a19c659809d187e13e17
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-02 14:34:08 +0800

    sa

--- application/views/feedsetup/feedsetup_dashboard_view.php
+++ application/views/feedsetup/feedsetup_dashboard_view.php
@@ -3,49 +3,58 @@
     <div class="widget-options">
         <h2 class="ico-widget widget">Display Widgets <small style="font-weight:normal;">(for displaying feedback on your websites)</small></h2>
         <?=HTML::link('feedsetup/display_widgets', 'Create Display Widget', array('class' => 'widget-create'))?>
-        <?foreach($widgets->display_widgets as $rows):?>
-            <div class="widget-types">
-                <div class="widget-info">
-                    <div class="grids">
-                        <div class="g2of3">
-                        <div class="widget-title"><?=$rows->widget_obj->theme_name?></div>
-                        <table width="100%" cellpadding="0" cellspacing="0">
-                            <tr><td width="90"><strong>Widget Type :</strong></td>
-                                <td><?=$rows->widget_obj->embed_type?></td>
-                            </tr>
-                            <tr><td width="90"><strong>Theme :</strong></td>
-                                <td><?=$rows->widget_obj->theme_type?></td>
-                            </tr>
-                            <tr><td width="90"><strong>URL :</strong></td>
-                                <td><?=$rows->widget_obj->base_url?></td>
-                            </tr>
-                        </table>
-                        </div>
-                        <div class="g1of3">
-                            <div class="right-align">
-                                <ul class="widget-button-list">
-                                    <li><?=HTML::link('feedsetup/edit/'.$rows->widgetstoreid, 'Edit', array('class' => 'button-gray'))?></li>
-                                    <!--
-                                    <li><a href="#" class="button-gray">Stat</a></li>
-                                    <li><a href="#" class="button">More</a></li>
-                                    -->
-                                </ul>
+        <?if($widgets->display_widgets != null):?>
+            <?foreach($widgets->display_widgets as $rows):?>
+                <div class="widget-types">
+                    <div class="widget-info">
+                        <div class="grids">
+                            <div class="g2of3">
+                            <div class="widget-title"><?=$rows->widget_obj->theme_name?></div>
+                            <table width="100%" cellpadding="0" cellspacing="0">
+                                <tr><td width="90"><strong>Widget Type :</strong></td>
+                                    <td><?=$rows->widget_obj->embed_type?></td>
+                                </tr>
+                                <tr><td width="90"><strong>Theme :</strong></td>
+                                    <td><?=$rows->widget_obj->theme_type?></td>
+                                </tr>
+                                <tr><td width="90"><strong>URL :</strong></td>
+                                    <td><?=$rows->widget_obj->base_url?></td>
+                                </tr>
+                            </table>
+                            </div>
+                            <div class="g1of3">
+                                <div class="right-align">
+                                    <ul class="widget-button-list">
+                                        <li><?=HTML::link('feedsetup/edit/'.$rows->widgetstoreid, 'Edit', array('class' => 'button-gray'))?></li>
+                                        <!--
+                                        <li><a href="#" class="button-gray">Stat</a></li>
+                                        <li><a href="#" class="button">More</a></li>
+                                        -->
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div> 
-        <?endforeach?>
-    </div>
-    <div class="widget-opts">
-        <div class="widget-pagination">
-            <a href="#" class="activePage">1</a>
-            <a href="#">2</a>
-            <a href="#">3</a>
-            <a href="#">4</a>
-            <a href="#">5</a>
+                </div> 
+            <?endforeach?>
         </div>
-    </div>
+        <div class="widget-opts">
+            <div class="widget-pagination">
+                <a href="#" class="activePage">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+            </div>
+        </div>
+    <?else:?>
+        <div class="woops">
+            <h2 class="woops-header">Create your feedback submission form</h2>
+            <p class="woops-content">
+                <?=HTML::link('feedsetup/display_widgets', 'Share your customer testimonials on your website', Array('class' => 'woops-a'))?> 
+            </p>
+        </div>
+    <?endif?>
 </div>
 
 <div id="widget-setup-block">
@@ -104,6 +113,6 @@
                 </p>
             </div>
         <?endif?>
-        </div>
+    </div>
 </div>
 </div>
