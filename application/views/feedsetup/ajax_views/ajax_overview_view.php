9f6cd5328a7b936c6c3a6558111f4ce764e9dc1d
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-02 22:17:24 +0800

    cripple cheecks

---
+++ application/views/feedsetup/ajax_views/ajax_overview_view.php
@@ -0,0 +1,55 @@
+<?if($widgets):?>
+    <?foreach($widgets as $rows):?>
+        <div class="widget-types">
+            <div class="widget-info">
+                <div class="grids">
+                    <div class="g2of3">
+                    <div class="widget-title"><?=$rows->widget_obj->theme_name?></div>
+                    <table width="100%" cellpadding="0" cellspacing="0">
+                        <tr><td width="90"><strong>Widget Type :</strong></td>
+                            <td><?=$rows->widget_obj->embed_type?></td>
+                        </tr>
+                        <tr><td width="90"><strong>Theme :</strong></td>
+                            <td><?=$rows->widget_obj->theme_type?></td>
+                        </tr>
+                        <tr><td width="90"><strong>URL :</strong></td>
+                            <td><?=$rows->widget_obj->base_url?></td>
+                        </tr>
+                    </table>
+                    </div>
+                    <div class="g1of3">
+                        <div class="right-align">
+                            <ul class="widget-button-list">
+                                <li><?=HTML::link('feedsetup/edit/'.$rows->widgetstoreid, 'Edit', array('class' => 'button-gray'))?></li>
+                                <!--
+                                <li><a href="#" class="button-gray">Stat</a></li>
+                                <li><a href="#" class="button">More</a></li>
+                                -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    <?endforeach?>
+</div>
+<div class="widget-opts">
+    <div class="widget-pagination">
+
+        <a href="#" class="activePage">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+   
+    </div>
+</div>
+<?else:?>
+<div class="woops">
+    <h2 class="woops-header">No Widgets available</h2>
+    <p class="woops-content">
+       Whoops! Looks like there are no widgets available... 
+    </p>
+</div>
+</div>
+<?endif?>
