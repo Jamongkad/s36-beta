7ebf7f69c7ecbd0b2e5f6e54c96dcd304d35d422
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-02 18:56:44 +0800

    sa

--- application/libraries/zebrapagination/zebrapagination.php
+++ application/libraries/zebrapagination/zebrapagination.php
@@ -617,7 +617,6 @@ class ZebraPagination
             );
 
             $url = preg_replace('/(subdomain=[a-zA-Z-0-9]+&)/', false, $_SERVER['QUERY_STRING']);
-            print_r($url);
             // if the current page is already set in GET
             if (isset($_GET[$this->variable_name])) {
 
