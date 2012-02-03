7a65b13fbd849463f3f5ed9ec26b430eac31043c
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-03 13:21:26 +0800

    sa

--- application/views/feedsetup/feedsetup_overview_view.php
+++ application/views/feedsetup/feedsetup_overview_view.php
@@ -40,11 +40,11 @@
                     </div> 
                 <?endforeach?>
             </div>
-            <div class="widget-opts">
-                <div class="widget-pagination">
-                    <?=$pagination?>
+                <div class="widget-opts">
+                    <div class="widget-pagination">
+                        <?=$pagination?>
+                    </div>
                 </div>
-            </div>
             <?else:?>
             <div class="woops">
                 <h2 class="woops-header">No Widgets available</h2>
