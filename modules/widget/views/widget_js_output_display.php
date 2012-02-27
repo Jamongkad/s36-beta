a39b0e6917dc83c1e3aa5862f526b6c2763b3dbb
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-27 16:41:06 +0800

    wq

--- modules/widget/views/widget_js_output_display.php
+++ modules/widget/views/widget_js_output_display.php
@@ -7,7 +7,7 @@ function WidgetLoader() {
         var src = '<?=$js_load?>' + 
                   '<?=$css_load?>' + 
                   '<div style="position:relative;width:<?=$width?>px;height:<?=$height?>px;">' +
-                  '<div class="s36_<?=$embed_block_type?>"><a href="javascript:;" onclick="s36_openForm('<?=$widget_child_loader_url?>')">Send Feedback</a></div>' +
+                  '<div class="s36_<?=$embed_block_type?>"><a href="javascript:;" onclick="s36_openForm(\'<?=$widget_child_loader_url?>\')">Send Feedback</a></div>' +
                   '<iframe id="s36Widget" allowTransparency="true" height="<?=$height?>" width="<?=$width?>"frameborder="0" scrolling="no" style="width:100%;border:none;overflow:hidden;" src="<?=$widget_loader_url?>">Insomnia wooohooooh</iframe></div>';
         return src;    
     },
