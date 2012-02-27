2a791e3eef4e0beb2bd46a095fc15015018e76e1
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-02-27 16:33:58 +0800

    wq

--- modules/widget/views/widget_js_output_display.php
+++ modules/widget/views/widget_js_output_display.php
@@ -4,8 +4,10 @@ header("Content-type: application/x-javascript; charset=UTF-8");
 ?>
 function WidgetLoader() {
     this.generateFrameMarkup = function() {
-        var src = '<div style="position:relative;width:<?=$width?>px;height:<?=$height?>px;">' +
-                  '<div class="s36_<?=$embed_block_type?>"><a href="javascript:;" onclick="s36_openForm('<?=$widget_child_loader_url?>');">Send Feedback</a></div>' +
+        var src = '<?=$js_load?>' + 
+                  '<?=$css_load?>' + 
+                  '<div style="position:relative;width:<?=$width?>px;height:<?=$height?>px;">' +
+                  '<div class="s36_<?=$embed_block_type?>"><a href="javascript:;" onclick="s36_openForm('<?=$widget_child_loader_url?>')">Send Feedback</a></div>' +
                   '<iframe id="s36Widget" allowTransparency="true" height="<?=$height?>" width="<?=$width?>"frameborder="0" scrolling="no" style="width:100%;border:none;overflow:hidden;" src="<?=$widget_loader_url?>">Insomnia wooohooooh</iframe></div>';
         return src;    
     },
