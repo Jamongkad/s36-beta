a434b1e8cd9ad8bac0446f6aa8c96d1f695a8e27
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-06 12:39:01 +0800

    wq

--- public/js/widget/form.js
+++ public/js/widget/form.js
@@ -59,15 +59,16 @@ jQuery(function($) {
     // added
     // assign crop script to crop btn
     $('#cropbtn').hide();
-    $('#cropbtn').click(function(){        
-        var crop_success = S36Form.save_crop_image();
+    $('#cropbtn').click(function(e){          
         /*
+        var crop_success = S36Form.save_crop_image();
         if ( crop_success.status == 200 ) {
             $steps.cycle(5);
             // hide the crop btn						
             S36Form.hide_crop_buttons();
         } 
         */
+        e.preventDefault();
     });
     //end added
     $('#edit-review-feedback').click(function(){S36Form.edit_feedback()});
