5541f8b61c855bd66ebee94fc865a319a0988314
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-07 12:29:49 +0800

    wq

--- application/views/feedback/modifyfeedback.php
+++ application/views/feedback/modifyfeedback.php
@@ -1,3 +1,10 @@
+<script type="text/javascript">
+    $('.feedback-textarea').tinymce({
+        script_url : '<?=URL::to('/')?>js/tiny_mce.js',
+        mode : "textareas",
+        theme_advanced_font_sizes : "12px,14px,16px,18px,24px"
+    });
+</script>
 <? $id = $feedback->id ?>
 <div class="block noborder">
     <div class="grids">
