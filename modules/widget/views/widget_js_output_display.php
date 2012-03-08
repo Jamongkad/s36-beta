9ddb59b281209787e55cf67a11d56e71d9a86389
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-08 19:16:51 +0800

    wq

--- modules/widget/views/widget_js_output_display.php
+++ modules/widget/views/widget_js_output_display.php
@@ -4,12 +4,20 @@ header("Content-type: application/x-javascript; charset=UTF-8");
 ?>
 function WidgetLoader() {
     this.generateFrameMarkup = function() {
-        var src = '<?=$js_load?>' + 
-                  '<?=$css_load?>' + 
-                  '<?=$class_name?>' + 
-                  '<div style="position:relative;width:<?=$width?>px;height:<?=$height?>px;">' +
-                  '<div class="s36_<?=$embed_block_type?>"><a href="javascript:;" onclick="s36_openForm(\'<?=$widget_child_loader_url?>\');">Send Feedback</a></div>' +
-                  '<iframe id="s36Widget" allowTransparency="true" height="<?=$height?>" width="<?=$width?>"frameborder="0" scrolling="no" style="width:100%;border:none;overflow:hidden;" src="<?=$widget_loader_url?>">Insomnia wooohooooh</iframe></div>';
+        <?if($class_name == "VerticalEmbedWidget" || $class_name == "HorizontalEmbedWidget"):?>
+            var src = '<?=$js_load?>' + 
+                      '<?=$css_load?>' + 
+                      '<div style="position:relative;width:<?=$width?>px;height:<?=$height?>px;">' +
+                      '<div class="s36_<?=$embed_block_type?>"><a href="javascript:;" onclick="s36_openForm(\'<?=$widget_child_loader_url?>\');">Send Feedback</a></div>' +
+                      '<iframe id="s36Widget" allowTransparency="true" height="<?=$height?>" width="<?=$width?>"frameborder="0" scrolling="no" style="width:100%;border:none;overflow:hidden;" src="<?=$widget_loader_url?>">Insomnia wooohooooh</iframe></div>';
+        <?endif?>
+        <?if($class_name == "ModalEmbedWidget"):?>
+            var src = '<div class="s36_embed_block_p"><a href="javascript:;" onclick="s36_open_popup_widget()">Display Feedback</a></div>'+
+                      '<div id="s36PopupWidgetShadow">'+
+                      '<div id="s36PopupWidgetBox">'+
+                      '<div id="s36PopupWidgetSendFeedback" onclick="s36_openForm(\<?=$widget_child_loader_url?>\')">Send Feedback</div><div id="s36_closebtn" onclick="s36_closePopupWidget()"></div>'+
+                      '<iframe id="s36Widget" allowTransparency="true" height="<?=$height?>" width="<?=$width?>" frameborder="0" scrolling="no" style="width:100%;border:none;overflow:visible;" src="'<?=$widget_loader_url?>"></iframe></div></div>';
+        <?endif?>
         return src;    
     },
 
