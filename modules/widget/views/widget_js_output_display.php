d917e4c4a90d4e87f320b60906bb67696bc1a80f
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-08 19:20:10 +0800

    wq

--- modules/widget/views/widget_js_output_display.php
+++ modules/widget/views/widget_js_output_display.php
@@ -15,7 +15,7 @@ function WidgetLoader() {
             var src = '<div class="s36_embed_block_p"><a href="javascript:;" onclick="s36_open_popup_widget()">Display Feedback</a></div>'+
                       '<div id="s36PopupWidgetShadow">'+
                       '<div id="s36PopupWidgetBox">'+
-                      '<div id="s36PopupWidgetSendFeedback" onclick="s36_openForm(\<?=$widget_child_loader_url?>\')">Send Feedback</div><div id="s36_closebtn" onclick="s36_closePopupWidget()"></div>'+
+                      '<div id="s36PopupWidgetSendFeedback" onclick="s36_openForm(\'<?=$widget_child_loader_url?>\')">Send Feedback</div><div id="s36_closebtn" onclick="s36_closePopupWidget()"></div>'+
                       '<iframe id="s36Widget" allowTransparency="true" height="<?=$height?>" width="<?=$width?>" frameborder="0" scrolling="no" style="width:100%;border:none;overflow:visible;" src="<?=$widget_loader_url?>"></iframe></div></div>';
         <?endif?>
         return src;    
