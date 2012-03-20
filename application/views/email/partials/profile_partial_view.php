67c2c43af2621fe9076413f0b3d44eb7f505af0f
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-20 14:53:18 +0800

    fixed email bug

--- application/views/email/partials/profile_partial_view.php
+++ application/views/email/partials/profile_partial_view.php
@@ -1,5 +1,5 @@
 <tr>
-<?if($feedback_data->str_rating != "POOR"):?>
+<?if($feedback_data->rating != "POOR"):?>
     <?if($feedback_data->avatar):?>
         <td><?=HTML::image('uploaded_cropped/150x150/'.$feedback_data->avatar, 'Avatar', array('width' => '91', 'height' => '91'))?></td>
         <td width="10"></td>
@@ -7,7 +7,7 @@
 <?endif?>
     <td style="font-size:12px;">
         <h2 style="color:#0078aa;margin:0px;"><?=$feedback_data->firstname?> <?=$feedback_data->lastname?></h2>
-<?if($feedback_data->str_rating != "POOR"):?>
+<?if($feedback_data->rating != "POOR"):?>
     <strong><?=$feedback_data->position?></strong><br />
     <?=$feedback_data->companyname?><br />
     <a href="#" target="_blank"><?=$feedback_data->url?></a><br />
