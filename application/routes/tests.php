1e631aff16942defef21ce9aaf4303277792a1d2
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-21 22:34:24 +0800

    wq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -188,8 +188,9 @@ return array(
     },
 
     'GET /tests/widget_data/(:any)' => function($widget_id) {
-        $wl = new WidgetLoader($widget_id); 
-        Helpers::show_data($wl);
+        $wl = new Widget\Services\WidgetLoader($widget_id); 
+        $widget = $wl->load();
+        Helpers::dump($widget->render_data());
     },
 
     'GET /tests/pull_feedback' => function() {        
