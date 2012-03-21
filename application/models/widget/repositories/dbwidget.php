941cb77e695a113aac4e9070b61912a5b3a538ea
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-22 00:08:38 +0800

    wq

--- application/models/widget/repositories/dbwidget.php
+++ application/models/widget/repositories/dbwidget.php
@@ -1,6 +1,7 @@
 <?php namespace Widget\Repositories;
 
 use S36DataObject\S36DataObject, PDO, StdClass, Helpers, DB, S36Auth;
+use ZebraPagination\ZebraPagination;
 
 class DBWidget extends S36DataObject {    
 
