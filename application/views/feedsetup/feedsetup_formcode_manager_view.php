45a7ab3aea895efc714fb095829fb9ea25b61f49
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-22 18:55:03 +0800

    wq

--- application/views/feedsetup/feedsetup_formcode_manager_view.php
+++ application/views/feedsetup/feedsetup_formcode_manager_view.php
@@ -81,7 +81,12 @@ function display_codes(i){
                         <!--JS Native Pop code goes here-->
                         <tr><td><strong>JavaScript Version <small>(Recommended)</small></strong></td></tr>
                         <tr><td><textarea class="regular-text jspopup" rows="7">
-                        <?=$link_js_output;?>
+<?=$link_js_output;?>
+                        </textarea></td></tr>
+
+                        <tr><td><strong>HTML Popup Version</strong></td></tr>
+                        <tr><td><textarea class="regular-text jspopup" rows="7">
+<a href="http://bmwsg.wufoo.com/forms/k7x1a3/" onclick="window.open(this.href,  null, 'height=757, width=680, toolbar=0, location=0, status=1, scrollbars=1, resizable=1'); return false">Please fill out my form.</a>
                         </textarea></td></tr>
                         </table>
                         <br />
--- modules/widget/views/widget_link_js_output_form.php
+++ modules/widget/views/widget_link_js_output_form.php
@@ -1,3 +1,3 @@
-<a href="#" onclick="s36_openForm('<?=$widget_loader_url?>');javascript::void(0);">Click to open feedback form</a>
+<a href="#" onclick="s36_openForm('<?=$widget_loader_url?>');javascript::void(0);">Please Fill out my feedback form.</a>
 <?=$js_load?>
 <?=$css_load?>
