8f199a107759bf0fc4a1b7a6bb3ae780f8dc7691
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-22 18:44:57 +0800

    wq

--- modules/widget/views/widget_link_js_output_form.php
+++ modules/widget/views/widget_link_js_output_form.php
@@ -1,16 +1,3 @@
+<a href="#" onclick="s36_openForm(\'<?=$widget_loader_url?>\');javascript::void(0);">Click to open feedback form</a>
 <?=$js_load?>
 <?=$css_load?>
-<script type="text/javascript">
-function s36FormLink() {
-	this.generateTab= function(){
-        var src = '<a href="#" onclick="s36_openForm(\'<?=$widget_loader_url?>\');javascript::void(0);">Click to open feedback form</a>';				
-        return src;
-	},
-	this.display = function () {
-        document.write(this.generateTab());
-    }	
-    
-}
-var link = new s36FormLink();
-link.display();
-</script>
