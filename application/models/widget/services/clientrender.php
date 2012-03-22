5efb870523a3f1146d934f5b46855b56eb0509ca
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-22 19:07:23 +0800

    wq

--- application/models/widget/services/clientrender.php
+++ application/models/widget/services/clientrender.php
@@ -63,8 +63,8 @@ class ClientRender {
 
         if($obj instanceof FormWidgets) {
             return '<a href="'.$this->_widget_loader($obj->widgetkey).'" 
-                       onclick="window.open(this.href,  null, "height=757, width=680, toolbar=0, location=0, status=1, scrollbars=1, resizable=1"); 
-                                javascript::void(0)">Please fill out my form.</a>';
+                       onclick="window.open(this.href,  null, \'height=757, width=680, toolbar=0, location=0, status=1, scrollbars=1, resizable=1\'); 
+                                return false;)">Please fill out my form.</a>';
         }
 
         if($obj instanceof DisplayWidgets) {
