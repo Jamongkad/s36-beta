e34f3e9b15d3cd641f6f5f22f002c14ada899aaf
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-23 18:32:18 +0800

    wq

--- public/js/FeedSetup.js
+++ public/js/FeedSetup.js
@@ -287,7 +287,10 @@ jQuery(function($) {
      
     $('.form-design').selected_theme({set_value: '#selected-form'});
     $('.tab-design').selected_theme({set_value: '#selected-tab'}).click(function(e) { 
-        var url = $("#update-tabtype-url").attr('hrefaction');   
+        var value = $(this).attr('id'); 
+        var widgetkey = $('input[name=submit_widgetkey]').val();
+        var url = $('#update-tabtype-url').attr('hrefaction') + '/' + widgetkey + '/' + value;
+
         console.log(url);
         e.preventDefault();
     });    
