5b8fbe93a7282ed7c134cf1b1484856cb55d2944
Mathew Wong <mathew@ubuntu-11-04.(none)>
2012-03-26 13:21:12 +0800

    wq

--- public/js/s36LightBox.js
+++ public/js/s36LightBox.js
@@ -1,42 +1,39 @@
-jQuery(function($) { 
-    //helper functions ABSTRACT THIS MOTHAFUCKER 
-    function s36Lightbox(width, height, insertContent) {	
-        if($('#lightbox').size() == 0){
-            var theLightbox = $('<div id="lightbox"></div>');
-            var theShadow = $('<div id="lightbox-shadow"/>');
-            $(theShadow).click(function(e){
-                    closeLightbox();
-            });
-            $('body').append(theShadow);
-            $('body').append(theLightbox);
-        }
-        $('#lightbox').empty();
-        if(insertContent != null){
-            //This is just a test
-            $('#lightbox').append(insertContent + "<div id='lightbox-comment' style='color:#fff;position:absolute;top:-20px;right:10px;'>This is a preview only | <span style='cursor:pointer'>close</span></div>");
-        }
-
-        $('#lightbox-comment').click(function(e){
-            closeLightbox();
+function s36Lightbox(width, height, insertContent) {	
+    if(jQuery('#lightbox').size() == 0){
+        var theLightbox = jQuery('<div id="lightbox"></div>');
+        var theShadow = jQuery('<div id="lightbox-shadow"/>');
+        jQuery(theShadow).click(function(e){
+                closeLightbox();
         });
-
-        //set negative margin for dynamic width
-        var margin = Math.round(width / 2);
-
-        // set the css and show the lightbox
-        $('#lightbox').css('top', $(window).scrollTop() + 50 + 'px');
-        $('#lightbox').css({
-                'width':width,
-                'height':height,
-                'margin-left':"-"+margin+"px"
-                });
-
-        $('#lightbox').fadeIn('fast');
-        $('#lightbox-shadow').fadeIn('fast');
+        jQuery('body').append(theShadow);
+        jQuery('body').append(theLightbox);
+    }
+    jQuery('#lightbox').empty();
+    if(insertContent != null){
+        //This is just a test
+        jQuery('#lightbox').append(insertContent + "<div id='lightbox-comment' style='color:#fff;position:absolute;top:-20px;right:10px;'>This is a preview only | <span style='cursor:pointer'>close</span></div>");
     }
 
-    function closeLightbox(){
-        $('#lightbox').fadeOut('fast',function(){$(this).empty();});
-        $('#lightbox-shadow').fadeOut('fast');
-    }    
-});
+    jQuery('#lightbox-comment').click(function(e){
+        closeLightbox();
+    });
+
+    //set negative margin for dynamic width
+    var margin = Math.round(width / 2);
+
+    // set the css and show the lightbox
+    jQuery('#lightbox').css('top', jQuery(window).scrollTop() + 50 + 'px');
+    jQuery('#lightbox').css({
+            'width':width,
+            'height':height,
+            'margin-left':"-"+margin+"px"
+            });
+
+    jQuery('#lightbox').fadeIn('fast');
+    jQuery('#lightbox-shadow').fadeIn('fast');
+}
+
+function closeLightbox(){
+    jQuery('#lightbox').fadeOut('fast',function(){jQuery(this).empty();});
+    jQuery('#lightbox-shadow').fadeOut('fast');
+}    
