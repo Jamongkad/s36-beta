d2727ddae3834929b43749728092d5f8b365f2e5
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-03-28 17:50:12 +0800

    wqe

--- public/js/DropDownChange.js
+++ public/js/DropDownChange.js
@@ -11,7 +11,7 @@ DropDownChange.prototype.enable = function() {
         $(that).children('select').show();
         $(that).undelegate(me.status_selector).delegate('select', me.status_selector, function(e) {
             var select = $(this);
-            console.log($('option[value="'+select.val()+'"]').text());
+            console.log($('option[value="'+select.val()+'"]', this).text());
             var select_val = select.val();
             var feedid = select.attr('feedid');
             var feedurl = select.attr('feedurl');
