0f3a1c9db26e2afd1abaf873b70385416da0927e
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-03-30 11:26:29 +0800

    FIXING IE!!!!!

--- public/js/s36_client_script.js
+++ public/js/s36_client_script.js
@@ -30,7 +30,7 @@ function s36_openLightbox(width,height,src) {
 	var s36_closebtn = document.createElement("div");
 		s36_closebtn.id = "s36_closebtn";
 		//s36_closebtn.setAttribute("onclick","s36_closeLightbox()");
-        s36_closebtn.addEventListener("click", s36_closeLightbox, false);
+        s36_closebtn.attachEvent("onclick", s36_closeLightbox);
 		s36_closebtn.className = "s36_closebtn";
 		s36_modalbox.appendChild(s36_closebtn);
 	// build that awesome iframe
@@ -86,7 +86,7 @@ function s36_openForm(form_url) {
 		s36_closebtn.id 		= "s36_closebtn";
 		s36_closebtn.className 	= "s36_closebtn";
 		//s36_closebtn.setAttribute("onclick","s36_closeLightbox()");		
-        s36_closebtn.addEventListener("click", s36_closeLightbox, false);
+        s36_closebtn.attachEvent("onclick", s36_closeLightbox);
 		s36_modalbox.appendChild (s36_closebtn);
 		
 	var width 	= 447;
