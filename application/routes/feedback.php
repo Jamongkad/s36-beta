f1f2da9cb2dcd5484d7a5507d4da79901d39372b
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-04 17:47:03 +0800

    Gotta stop that plane! Turn it around.

--- application/routes/feedback.php
+++ application/routes/feedback.php
@@ -41,14 +41,14 @@ return array(
         $company_id = S36Auth::user()->companyid;
         $widget = new Widget\Repositories\DBWidget;
         
-        Helpers::dump($widget->fetch_widgets_by_company());
-        /*
+        //Helpers::dump($widget->fetch_widgets_by_company());
+
         return View::of_layout()->partial('contents', 'feedback/requestfeedback_view', Array(
             'sites' => DB::Table('Site', 'master')->where('companyId', '=', $company_id)->get()
           , 'submission_widgets' => $widget->fetch_widgets_by_company() 
           , 'errors' => Array()
           , 'input' => Array('firstname' => null, 'lastname' => null, 'email' => null, 'custom_message' => "")
-        ));*/
+        ));
     }),
 
     'POST /feedback/requestfeedback' => Array('needs' => 'S36ValueObjects', 'do' => function() {
