75a7fe514d6d1d5dfcdbfc96f6b71dbe5f902173
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-04 18:13:36 +0800

    just a little styling

--- application/views/feedback/requestfeedback_thankyou_view.php
+++ application/views/feedback/requestfeedback_thankyou_view.php
@@ -1,2 +1,10 @@
-<h1>Feedback Request Sent</h1>
-
+<div class="block">
+    <div class="woops">
+        <h2>Feedback Request Sent</h2>
+        <br/><br/>
+        <p>
+         Awesome! If you want to send more requests 
+         <strong style="font-size:12px"><?=HTML::link('feedback/requestfeedback', 'click here', Array('class' => 'woops-a'))?> </strong>
+         .</p>
+    </div> 
+</div>
