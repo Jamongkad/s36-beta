7bbaa581cbb93dcdfef043d47c668e455eaa7e2b
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-05 22:00:04 +0800

    experimenting

--- application/models/feedback/services/inboxservice.php
+++ application/models/feedback/services/inboxservice.php
@@ -84,7 +84,8 @@ class InboxService {
     //I am sorry but filters are hard (-_-) TODO GET BACK TO THIS!!
     public function _check_filters(Array $filters) {
 
-        $date_statement = "Feedback.dtAdded DESC";
+        $date_statement = "Feedback.rating = 5, Feedback.rating = 4, Feedback.rating = 3, Feedback.rating = 2, Feedback.rating = 1
+                        , Feedback.dtAdded DESC";
         $grouped_date_statement = "Feedback.dtAdded DESC";
 
         $filters['filed_statement'] = ($filters['filter'] == 'filed') ? 'AND Category.intName != "default"' : 'AND Category.intName = "default"';       
