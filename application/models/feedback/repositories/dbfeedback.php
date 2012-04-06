2ed1ad876d5ae6fc29f5c93c52e9750df512942e
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-06 17:09:41 +0800

    Weird error.

--- application/models/feedback/repositories/dbfeedback.php
+++ application/models/feedback/repositories/dbfeedback.php
@@ -125,8 +125,8 @@ class DBFeedback extends S36DataObject {
         $sth->bindParam(':is_deleted', $opts['deleted'], PDO::PARAM_INT);
         $sth->bindParam(':is_published', $opts['published'], PDO::PARAM_INT);
         $sth->bindParam(':is_featured', $opts['featured'], PDO::PARAM_INT);
-        $sth->bindparam(':limit', $opts['limit'], pdo::param_int);
-        $sth->bindparam(':offset', $opts['offset'], pdo::param_int);
+        $sth->bindparam(':limit', $opts['limit'], PDO::PARAM_INT);
+        $sth->bindparam(':offset', $opts['offset'], PDO::PARAM_INT);
         $sth->execute();       
 
         $row_count = $this->dbh->query("SELECT FOUND_ROWS()");
@@ -206,8 +206,8 @@ class DBFeedback extends S36DataObject {
         $sth->bindParam(':is_deleted', $opts['deleted'], PDO::PARAM_INT);
         $sth->bindParam(':is_published', $opts['published'], PDO::PARAM_INT);
         $sth->bindParam(':is_featured', $opts['featured'], PDO::PARAM_INT);
-        $sth->bindparam(':limit', $opts['limit'], pdo::param_int);
-        $sth->bindparam(':offset', $opts['offset'], pdo::param_int);
+        $sth->bindparam(':limit', $opts['limit'], PDO::PARAM_INT);
+        $sth->bindparam(':offset', $opts['offset'], PDO::PARAM_INT);
         $sth->execute();
         $date_result = $sth->fetchAll(PDO::FETCH_CLASS); 
         return $date_result;
