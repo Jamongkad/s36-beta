42916c955ae0dcc59fcc0652d125a46533f85264
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-09 13:24:40 +0800

    generalizing thankyou page

--- application/views/feedback/requestfeedback_thankyou_view.php
+++ application/views/feedback/requestfeedback_thankyou_view.php
@@ -1,9 +1,9 @@
 <div class="block">
     <div class="woops">
-        <h2>Feedback Request Sent</h2>
+        <h2>Email Successfully Sent</h2>
         <br/><br/>
         <p>
-         Awesome! If you want to send more requests 
+         Awesome! If you want to send more emails
          <strong style="font-size:12px"><?=HTML::link('feedback/'.$linkback, 'click here', Array('class' => 'woops-a'))?> </strong>
          .</p>
     </div> 
