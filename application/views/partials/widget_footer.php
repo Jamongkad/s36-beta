aff6508253200a19be6b398280da9bf2dfe7e554
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-10 11:24:32 +0800

    returning

--- application/views/partials/widget_footer.php
+++ application/views/partials/widget_footer.php
@@ -1,20 +1,22 @@
 
-<?//=HTML::script('js/jquery.cycle.all.min.js')?>
-<?//=HTML::script('js/jquery.mousewheel.js')?>
-<?//=HTML::script('js/jquery.scroll.js')?> 
+<?=HTML::script('js/jquery.cycle.all.min.js')?>
+<?=HTML::script('js/jquery.mousewheel.js')?>
+<?=HTML::script('js/jquery.scroll.js')?> 
 
 <script text="text/javascript">
 <?
+/*
     $js_scripts = Array(
-         '/js/jquery.cycle.all.min.js' 
-       , '/js/jquery.mousewheel.js'
-       , '/js/jquery.scroll.js'
+         '/js/jquery.jcrop.js' 
+       , '/js/jquery.ajaxfileupload.js'
+       , '/js/s36FormModule.js'
     );
 
     $string = '"' . implode('","', $js_scripts) . '"';
+*/
 ?>
 
-head.js(<?=$string?>);
+//head.js(<?=$string?>);
 </script>
 </body>
 </html>
