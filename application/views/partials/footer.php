0c35169eceff3c0fd7792ff1b226ee8db80840c7
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-12 15:59:02 +0800

    integrating live leader stuff

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -8,6 +8,12 @@
         <h4>36Stories © 2011. </h4>
         <p>Keep in touch by following us on <a href="#">Facebook</a>, <a href="#">Twitter</a>, subscribing to our <a href="#">blog's feed</a> and joining our <a href="#">email newsletter</a>.</p>
     </div>
+
+<!--Live Leader Chat app-->
+<script type="text/javascript" src="http://www.liveleader.com/a/Hello?cid=69d46946-034a-498a-911b-e4508d14a78c;v=2"></script>
+<a class="ChatPoweredLink" href="http://www.liveleader.com">Live chat, live help, live support</a>
+<!--Live Leader Chat app-->
+
 <?endif?>
 
 </div>
