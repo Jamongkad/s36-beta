a94e92ffb43dc6817957d70a6c90f20355f67a77
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-12 16:26:31 +0800

    ad

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -9,10 +9,10 @@
         <p>Keep in touch by following us on <a href="#">Facebook</a>, <a href="#">Twitter</a>, subscribing to our <a href="#">blog's feed</a> and joining our <a href="#">email newsletter</a>.</p>
     </div>
     <?if(Config::get('application.env_name') == 'prod'):?>
-        <!--Live Leader Chat app-->
+        <!--Live Leader Chat app
         <script type="text/javascript" src="http://www.liveleader.com/a/Hello?cid=69d46946-034a-498a-911b-e4508d14a78c;v=2"></script>
         <a class="ChatPoweredLink" href="http://www.liveleader.com">Live chat, live help, live support</a>
-        <!--Live Leader Chat app-->
+        -->
     <?endif?>
 
 <?endif?>
