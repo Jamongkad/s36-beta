c46d58dd91f1c97dd467dec003a2171fbc9bbe82
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-12 15:38:27 +0800

    ewqwq

--- application/views/admin/admin_index_view.php
+++ application/views/admin/admin_index_view.php
@@ -18,9 +18,9 @@
                             <?if($role == 'Admin'):?>
                                 <small class="edit"><?=HTML::link('admin/edit_admin/'.$admin->userid, 'edit')?></small>
                                 <?if($user_id != $admin->userid || $admin->account_owner == 0):?>
-
-                                    <?=HTML::link('admin/delete_admin/'.$admin->userid, 'delete', Array('class' => 'admin-delete'))?>
-
+                                    <small class="edit">
+                                        <?=HTML::link('admin/delete_admin/'.$admin->userid, 'delete', Array('class' => 'admin-delete'))?>
+                                    </small>
                                 <?endif?>
                             <?endif?>
                         </div>
