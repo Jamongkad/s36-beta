fd41cbc1ef909d74ca1e95a9f815441659ea6da8
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-17 18:44:20 +0800

    malayo

--- application/models/halcyonic/services/halcyonicservice.php
+++ application/models/halcyonic/services/halcyonicservice.php
@@ -1,12 +1,10 @@
 <?php namespace Halcyonic\Services;
 
-use redisent;
-
 class HalcyonicService {
 
     private $redis;
 
     public function __construct()  {
-        $this->redis = new Redis; 
+        $this->redis = new \redisent\Redis; 
     }
 }
