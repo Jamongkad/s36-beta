2c0028727f6baeaccad9614fd26b8d9d12bef860
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-17 21:00:55 +0800

    stop that plane!!

--- application/models/halcyonic/services/halcyonicservice.php
+++ application/models/halcyonic/services/halcyonicservice.php
@@ -4,10 +4,11 @@ use S36Auth;
 
 class HalcyonicService {
 
-    private $redis, $auth;
+    private $redis, $auth, $feedback;
 
     public function __construct()  {
         $this->redis = new \redisent\Redis; 
+        $this->feedback = new Feedback\Repositories\DBFeedback;
         $this->auth = S36Auth::user();
     } 
 }
