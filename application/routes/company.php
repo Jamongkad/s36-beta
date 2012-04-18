b82a0d5e7bcfbf4a8fa252c859494dc31439d2d2
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-18 23:08:22 +0800

    step

--- application/routes/company.php
+++ application/routes/company.php
@@ -2,7 +2,7 @@
 
 return array(
     'GET /company/hosted_form/(:any)' => function($widget_id) { 
-        $wl = new Widget\Services\WidgetLoader($id); 
+        $wl = new Widget\Services\WidgetLoader($widget_id); 
         $widget = $wl->load();
 
         Helpers::dump($widget);
