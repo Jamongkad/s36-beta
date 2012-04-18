476e8a9795378d1347de78fd8b5554933075c6dc
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-18 23:12:10 +0800

    oh yes

--- application/routes/company.php
+++ application/routes/company.php
@@ -5,7 +5,7 @@ return array(
         $wl = new Widget\Services\WidgetLoader($widget_id); 
         $widget = $wl->load();
 
-        Helpers::dump($widget);
+        Helpers::dump($widget->render_data());
 
         return View::of_company_layout()->partial('contents', 'company/hosted_feedback_form_view', Array(
             'subdomain' => Input::get('subdomain')
