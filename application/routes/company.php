2e4873202c37b279acfe27ebe7bfc8fd96149ce1
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-18 23:45:40 +0800

    ok

--- application/routes/company.php
+++ application/routes/company.php
@@ -5,10 +5,8 @@ return array(
         $wl = new Widget\Services\WidgetLoader($widget_id); 
         $widget = $wl->load();
 
-        Helpers::dump($widget->render_hosted());
-
         return View::of_company_layout()->partial('contents', 'company/hosted_feedback_form_view', Array(
-            'subdomain' => Input::get('subdomain')
+            'widget' => $widget->render_hosted()
         ));
     },
 );
--- application/views/company/hosted_feedback_form_view.php
+++ application/views/company/hosted_feedback_form_view.php
@@ -1,1 +1,12 @@
-<?=$subdomain?>
+<div id="headerWrapper">
+	<div id="headerContent">
+    	<div id="headerTitle">
+        	<strong>Acme Inc</strong> 
+            
+            <span><a href="#">View all feedback</a> | <a href="#">Send in feedback</a></span>
+            
+            <span class="right padfix"><a href="#">Visit Acme's Website</a></span>
+        </div>
+    </div>
+</div>
+<?=$widget?>
