99ee2724009d42fffd35255367c92b79e3f538de
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-19 14:59:16 +0800

    fixing

--- modules/widget/views/widget_hostedform_view.php
+++ modules/widget/views/widget_hostedform_view.php
@@ -1,7 +1,7 @@
+<?=HTML::script('/js/s36FormModule.js')?>
 <?
     $js_scripts = Array(
-         '/js/jquery.jcrop.js' 
-       , '/js/s36FormModule.js'
+         '/js/jquery.jcrop.js'  
        , '/js/jquery.ajaxfileupload.js'
        , '/js/cycle.function.js'
        , '/js/widget/form.js'
