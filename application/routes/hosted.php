f99454c098add1c3a0bcfdf8c43ecd5ca209580a
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-19 17:53:00 +0800

    you blow my mind

--- application/routes/hosted.php
+++ application/routes/hosted.php
@@ -4,7 +4,7 @@ return array(
     'GET /hosted/form/(:any)' => function($widget_id) { 
         $wl = new Widget\Services\WidgetLoader($widget_id); 
         $widget = $wl->load();
-        return View::of_company_layout()->partial('contents', 'company/hosted_feedback_form_view', Array(
+        return View::of_company_layout()->partial('contents', 'hosted/hosted_feedback_form_view', Array(
             'widget' => $widget->render_hosted()
         ));
     },
