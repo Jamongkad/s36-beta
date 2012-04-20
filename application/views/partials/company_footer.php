d10350af37d37436bb2c4ec7a0ebb625ebec8d91
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-20 18:54:39 +0800

    don't you know you blow my mind

--- application/views/partials/company_footer.php
+++ application/views/partials/company_footer.php
@@ -1,4 +1,29 @@
 
+        <div class="block" style="height:40px;"></div>
+        <div id="companyDetails" class="block">
+        	<div class="companyLogo">
+                <?=HTML::image('img/company-logo-filler.jpg')?>
+            </div>
+            <div class="companyDetails">
+            	<h2>Company Profile</h2>
+                <p>Acme in specializes in creating widgets for everyday use. Thousands of 
+customers worldwideuse Acme products and get better each and everyday. 
+Visit Acme's website today for more information. </p>
+				<br />
+                <div class="companyLinks">
+                	<ul>
+                    	<li><a href="#" class="website">Visit Our Website</a></li>
+                        <li><a href="#" class="facebook">Join us on Facebook</a></li>
+                        <li><a href="#" class="twitter">Follow us on Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="block" style="height:20px;"></div>
+        <div class="block" style="text-align:center;font-size:11px;color:#c2c3c4;">Powered by 36Stories</div>
+    </div>
+</div>
+
 <?=HTML::script('js/jquery.cycle.all.min.js')?>
 <?=HTML::script('js/jquery.mousewheel.js')?>
 <?=HTML::script('js/jquery.scroll.js')?> 
--- modules/widget/views/widget_hostedform_view.php
+++ modules/widget/views/widget_hostedform_view.php
@@ -441,27 +441,3 @@
             </div>
         </div>
         <!-- form box ends here -->
-        <div class="block" style="height:40px;"></div>
-        <div id="companyDetails" class="block">
-        	<div class="companyLogo">
-                <?=HTML::image('img/company-logo-filler.jpg')?>
-            </div>
-            <div class="companyDetails">
-            	<h2>Company Profile</h2>
-                <p>Acme in specializes in creating widgets for everyday use. Thousands of 
-customers worldwideuse Acme products and get better each and everyday. 
-Visit Acme's website today for more information. </p>
-				<br />
-                <div class="companyLinks">
-                	<ul>
-                    	<li><a href="#" class="website">Visit Our Website</a></li>
-                        <li><a href="#" class="facebook">Join us on Facebook</a></li>
-                        <li><a href="#" class="twitter">Follow us on Twitter</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="block" style="height:20px;"></div>
-        <div class="block" style="text-align:center;font-size:11px;color:#c2c3c4;">Powered by 36Stories</div>
-    </div>
-</div>
