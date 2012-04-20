68eae12c885c0a0ad40b1e7a219355864442eed8
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-20 18:48:32 +0800

    oh year

--- application/views/hosted/hosted_feedback_form_view.php
+++ application/views/hosted/hosted_feedback_form_view.php
@@ -2,9 +2,9 @@
 	<div id="headerContent">
     	<div id="headerTitle">
             <?$company_name = ucfirst($company->name);?>
-        	<strong><?=$company_name?></strong> 
-            
-            <span><a href="#">View all feedback</a> | <a href="#">Send in feedback</a></span>
+
+        	<strong><?=$company_name?></strong>  
+            <span><?=HTML::link('/', 'View all feedback')?></span>
             
             <?if($company->domain):?>
                 <span class="right padfix">
