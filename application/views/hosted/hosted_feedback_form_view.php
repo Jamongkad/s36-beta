4ef3c0a51687f10e8a041cad66310ebaf99c2d58
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-20 19:00:26 +0800

    everything is good and round

--- application/views/hosted/hosted_feedback_form_view.php
+++ application/views/hosted/hosted_feedback_form_view.php
@@ -16,27 +16,43 @@
 </div>
 <?=$widget?>
 
-
 <div class="block" style="height:40px;"></div>
         <div id="companyDetails" class="block">
         	<div class="companyLogo">
-                <?=HTML::image('img/company-logo-filler.jpg')?>
+                <?if($company->logo):?>
+                    <?=HTML::image('img/company_logos/'.$company->logo)?>
+                <?else:?>
+                    <?=HTML::image('img/company-logo-filler.jpg')?>
+                <?endif?>
             </div>
             <div class="companyDetails">
             	<h2>Company Profile</h2>
-                <p>Acme in specializes in creating widgets for everyday use. Thousands of 
-customers worldwideuse Acme products and get better each and everyday. 
-Visit Acme's website today for more information. </p>
+                <p>
+                    <?if($company->description):?>
+                        <?=$company->description?> 
+                    <?else:?>
+                        Acme in specializes in creating widgets for everyday use. Thousands of 
+                        customers worldwideuse Acme products and get better each and everyday. 
+                        Visit Acme's website today for more information. 
+                    <?endif?>
+                </p>
+
 				<br />
                 <div class="companyLinks">
-                <!--decompress social_links object and iterate here
-                	<ul>
-                    	<li><a href="#" class="website">Visit Our Website</a></li>
-                        <li><a href="#" class="facebook">Join us on Facebook</a></li>
-                        <li><a href="#" class="twitter">Follow us on Twitter</a></li>
-                    </ul>
+                    <?if($company->social_links):?> 
+                        <ul>
+                            <li><a href="#" class="website">Visit Our Website</a></li>
+                            <li><a href="#" class="facebook">Join us on Facebook</a></li>
+                            <li><a href="#" class="twitter">Follow us on Twitter</a></li>
+                        </ul>
+                    <?else:?>
+                        <ul>
+                            <li><a href="#" class="website">Visit Our Website</a></li>
+                            <li><a href="#" class="facebook">Join us on Facebook</a></li>
+                            <li><a href="#" class="twitter">Follow us on Twitter</a></li>
+                        </ul>
+                    <?endif?>
                 </div>
-                -->
             </div>
         </div>
         <div class="block" style="height:20px;"></div>
