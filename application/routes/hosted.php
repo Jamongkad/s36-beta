7604b83bcdf86dba25a8a306d785cc4c89363c20
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-20 18:42:30 +0800

    the speed of chiba

--- application/routes/hosted.php
+++ application/routes/hosted.php
@@ -6,10 +6,8 @@ return array(
         $company = new DBCompany;
         $widget = $wl->load();
         Helpers::dump($widget); 
-        Helpers::dump($company->get_company_info($widget->company_id)); 
-
         return View::of_company_layout()->partial('contents', 'hosted/hosted_feedback_form_view', Array(
-            'widget' => $widget->render_hosted(), 'company' => $company
+            'widget' => $widget->render_hosted(), 'company' => $company->get_company_info($widget->company_id)
         ));
     },
 );
