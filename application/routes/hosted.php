2bb4a8d548c8f0fe8e800a307c1207cafe666207
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-20 17:41:20 +0800

    Claire

--- application/routes/hosted.php
+++ application/routes/hosted.php
@@ -4,6 +4,7 @@ return array(
     'GET /hosted/form/(:any)' => function($widget_id) { 
         $wl = new Widget\Services\WidgetLoader($widget_id); 
         $widget = $wl->load();
+        Helpers::dump($widget);
         return View::of_company_layout()->partial('contents', 'hosted/hosted_feedback_form_view', Array(
             'widget' => $widget->render_hosted()
         ));
