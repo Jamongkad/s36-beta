b448b5255555b4f3cf498d8e902d755276f0d9f3
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-23 15:16:20 +0800

    confidence

---
+++ application/views/hosted/hosted_feedback_single_view.php
@@ -0,0 +1,61 @@
+<div id="headerWrapper">
+	<div id="headerContent">
+    	<div id="headerTitle">
+            <?$company_name = ucfirst($feedback->company_name);?>
+
+        	<strong><?=$company_name?></strong>  
+            <span><?=HTML::link('/', 'View all feedback')?></span>
+            
+            <?if($company->domain):?>
+                <span class="right padfix">
+                    <a href="http://<?=$feedback->sitedomain?>" target="_blank"><?="Visit $company_name's Website"?></a>
+                </span>
+            <?endif?>
+        </div>
+    </div>
+</div>
+
+
+<div class="block" style="height:40px;"></div>
+        <div id="companyDetails" class="block">
+        	<div class="companyLogo">
+                <?if($feedback->company_logo):?>
+                    <?=HTML::image('img/company_logos/'.$feedback->company_logo)?>
+                <?else:?>
+                    <?=HTML::image('img/company-logo-filler.jpg')?>
+                <?endif?>
+            </div>
+            <div class="companyDetails">
+            	<h2>Company Profile</h2>
+                <p>
+                    <?if($feedback->company_description):?>
+                        <?=$feedback->company_description?> 
+                    <?else:?>
+                        Acme in specializes in creating widgets for everyday use. Thousands of 
+                        customers worldwideuse Acme products and get better each and everyday. 
+                        Visit Acme's website today for more information. 
+                    <?endif?>
+                </p>
+
+				<br />
+                <div class="companyLinks">
+                    <?if($feedback->company_social_links):?> 
+                        <ul>
+                            <li><a href="#" class="website">Visit Our Website</a></li>
+                            <li><a href="#" class="facebook">Join us on Facebook</a></li>
+                            <li><a href="#" class="twitter">Follow us on Twitter</a></li>
+                        </ul>
+                    <?else:?>
+                        <ul>
+                            <li><a href="#" class="website">Visit Our Website</a></li>
+                            <li><a href="#" class="facebook">Join us on Facebook</a></li>
+                            <li><a href="#" class="twitter">Follow us on Twitter</a></li>
+                        </ul>
+                    <?endif?>
+                </div>
+            </div>
+        </div>
+        <div class="block" style="height:20px;"></div>
+        <div class="block" style="text-align:center;font-size:11px;color:#c2c3c4;">Powered by 36Stories</div>
+    </div>
+</div>
