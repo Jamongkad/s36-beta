023675d80e3c2b9c646a19bb8119c5c1c7b77b2c
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-23 16:00:11 +0800

    da

--- application/views/partials/company_header.php
+++ application/views/partials/company_header.php
@@ -11,13 +11,13 @@
         
         <?  
             preg_match_all('~hosted/(form|single|full)~', Request::uri(), $matches);
- 
-            $hosted_type = array_shift(array_slice($matches, 1));
-            if($hosted_type[0] == 'single') {
+            //YUCK!! 
+            $hosted_type = array_slice($matches, 1);
+            if($hosted_type[0][0] == 'single') {
                 echo HTML::style('css/widget_master/hosted-single.css');
             }
 
-            if($hosted_type[0] == 'form') {
+            if($hosted_type[0][0] == 'form') {
                 echo HTML::style('css/widget_master/hosted-form.css');
             }
 
