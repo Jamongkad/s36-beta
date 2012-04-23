5f72ae05723bb33441719393fb865ecdeb475f3d
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-23 14:00:04 +0800

    fixing

--- application/views/partials/company_header.php
+++ application/views/partials/company_header.php
@@ -4,6 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7/jquery.min.js'></script>  
+        <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
         <?=HTML::script('js/head.min.js')?>
         <?=HTML::style('css/widget_master/flags_widget.css')?>
         <?=HTML::style('css/widget_master/grids.css')?>
