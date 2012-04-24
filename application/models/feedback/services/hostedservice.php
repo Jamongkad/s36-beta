1798946fa5a7940d396ccd3fe4487739b66156d0
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-24 16:34:04 +0800

    asd

--- application/models/feedback/services/hostedservice.php
+++ application/models/feedback/services/hostedservice.php
@@ -16,8 +16,7 @@ class HostedService {
         $ctr = 0;
         $units = 4;
         $max = count($feeds);
-        $main = new StdClass;
-        $child = Array();
+        $node = new StdClass;
         foreach($feeds as $feed) {            
             $end = 0;
             /*
@@ -35,21 +34,25 @@ class HostedService {
             
             $collection[] = $main; 
             */  
-            $slice_coords = Array();
-            //echo "feedid: ".$feed->id." num: ".$ctr." mod: ".(($ctr % $units) == 0)."<br/>";        
-            if(($ctr % $units) == 0) { 
-                //echo "multiple: ".$ctr."<br/>";
-                $start = $ctr;   
-                $end = $ctr + $units;
-                $slice_coords['start'] = $start;
-                $slice_coords['end'] = $end;
 
-                Helpers::dump($slice_coords);
-            }             
+            if($feed->isfeatured == 1)  {
+                $node->head = $feed->id;
+            }  else {
+                $slice_coords = Array();
+                //echo "feedid: ".$feed->id." num: ".$ctr." mod: ".(($ctr % $units) == 0)."<br/>";        
+                if(($ctr % $units) == 0) { 
+                    //echo "multiple: ".$ctr."<br/>";
+                    $start = $ctr;   
+                    $end = $ctr + $units;
+                    $slice_coords['start'] = $start;
+                    $slice_coords['end'] = $end;
+                }             
 
-            //echo "end: $end<br/>";
+                $node->slice = $slice_coords;
+            }
+            
             $ctr += 1;
-            //Helpers::dump($d);
+            Helpers::dump($node);
         }
 
         //return $child;
