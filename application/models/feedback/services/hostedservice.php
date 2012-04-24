6f14ab80bd24b56b17de09c231e737890c7396ad
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-24 14:43:17 +0800

    Wasd

--- application/models/feedback/services/hostedservice.php
+++ application/models/feedback/services/hostedservice.php
@@ -11,10 +11,17 @@ class HostedService {
     public function fetch_hosted_feedback($company_id) {
         $feeds = $this->feedback->televised_feedback($company_id);
         $collection = Array();
+
+        $ctr = 0;
+        $units = 3;
+        $max = count($feeds);
         foreach($feeds as $feed) {
+            $end = 1;
             if($feed->isfeatured == 1)  {
                 $collection[] = $feed;
             }
+            echo $ctr++."<br/>";
+            echo $end++."<br/>";
         }
 
         return $collection;
