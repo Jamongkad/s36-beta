8af53f3d5c02f5ab90c0dd92fc175554f23f7434
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-24 17:26:37 +0800

    asdas

--- application/models/feedback/services/hostedservice.php
+++ application/models/feedback/services/hostedservice.php
@@ -1,6 +1,6 @@
 <?php namespace Feedback\Services;
 
-use Feedback\Repositories\DBFeedback, Input, Exception, Helpers, DB, StdClass;
+use Feedback\Repositories\DBFeedback, Input, Exception, Helpers, DB, StdClass, ArrayIterator, LimitIterator;
 
 class HostedService {
     
@@ -16,29 +16,32 @@ class HostedService {
         $ctr = 0;
         $units = 4;
         $max = count($feeds);
-        $node = new StdClass;
 
-        foreach($feeds as $feed) {            
-            $end = 0;
+        $collection = Array();
+        foreach($feeds as $feed) {           
+            $node = new StdClass;
             if(($ctr % $units) == 0) { 
-                //echo "multiple: ".$ctr."<br/>";
-                $f = new \ArrayIterator($feeds);
-                $i = new \LimitIterator($f, $ctr, $units);
+                $f = new ArrayIterator($feeds);
+                $i = new LimitIterator($f, $ctr, $units);
                 $coll = Array();
                 foreach($i as $fr) {
                     if($fr->ispublished == 1 and $fr->isfeatured == 0) {
                         $coll[] = $fr->id;      
                     } else {
                         echo $fr->id."<br/>";     
+                        $node->head = $fr->id;
                     }
-                  
+
+                    if($coll) {
+                        $node->children = $coll;     
+                    }
+
+                    $collection[] = $node;
                 }
-                Helpers::dump($coll);
-            }             
- 
+            }              
             $ctr += 1;
         }
 
-        //return $child;
+        return $collection;
     }
 }
