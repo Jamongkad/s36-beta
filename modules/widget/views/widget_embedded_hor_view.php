1cf328c27eff93bf74c7f89ff566091dd2e33df7
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-25 16:03:08 +0800

    fixing display widgets

--- modules/widget/views/widget_embedded_hor_view.php
+++ modules/widget/views/widget_embedded_hor_view.php
@@ -171,12 +171,18 @@
                                 }
                                  
                                 $comp = '&nbsp;';								
+
+                                $companyname = $r->companyname;
+                                if($r->rules->displayurl == 1) {
+                                    $companyname = "<a href='".$r->url."'>".$r->companyname."</a>";    
+                                }
+
                                 if(($r->rules->displayposition == 1) && ($r->rules->displaycompany == 1)){								
-                                    $comp = $r->position.', '.$r->companyname;								
+                                    $comp = $r->position.', '.$companyname;								
                                 }
                                 
                                 if(($r->rules->displaycompany == 1) && ($r->rules->displayposition != 1)){								
-                                    $comp = $r->companyname;								
+                                    $comp = $companyname;								
                                 }
                                 
                                 if(($r->rules->displaycompany != 1) && ($r->rules->displayposition == 1)){								
