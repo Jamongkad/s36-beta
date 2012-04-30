774be5ca1f290ecabf6a3f3ed916a3630cdf857a
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-04-30 15:46:57 +0800

    ewq

--- application/routes/inbox.php
+++ application/routes/inbox.php
@@ -22,7 +22,7 @@ return array(
         );
 
         $inbox->set_filters($filters);
-        $feedback = $inbox->present_feedback(True);
+        $feedback = $inbox->present_feedback();
 
         $admin_check = S36Auth::user();
 
