d95808d65878b86e8e286feea1d4afeb731d5fb0
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-03 18:35:39 +0800

    ewq

--- application/views/hosted/partials/hosted_feedback_partial_view.php
+++ application/views/hosted/partials/hosted_feedback_partial_view.php
@@ -1,6 +1,6 @@
 <?foreach($collection as $coll):?>
     <h2><?=$coll->head?></h2>
     <?foreach($coll->children as $child):?>
-        <?=$child-?><br/>
+        <?=$child?><br/>
     <?endforeach?>
 <?endforeach?>
