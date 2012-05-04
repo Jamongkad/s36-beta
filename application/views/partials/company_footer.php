7686eb7efbf58f5c2d4382dc7808389b85974fb0
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-04 15:28:58 +0800

    The fuck

--- application/views/partials/company_footer.php
+++ application/views/partials/company_footer.php
@@ -2,5 +2,7 @@
 <?=HTML::script('js/jquery.cycle.all.min.js')?>
 <?=HTML::script('js/jquery.mousewheel.js')?>
 <?=HTML::script('js/jquery.scroll.js')?> 
+<?=HTML::script('js/masonry.js')?> 
+<?=HTML::script('js/modernizr.js')?> 
 </body>
 </html>
