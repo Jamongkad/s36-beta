b51b8f1b9aa5abc0cf83830eeee037d9282287e5
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-04 14:37:51 +0800

    Paulson

--- application/views/hosted/partials/hosted_feedback_partial_view.php
+++ application/views/hosted/partials/hosted_feedback_partial_view.php
@@ -34,9 +34,41 @@
             </div>
         </div>
     <?endif?>
-    <ul>
-    <?//foreach($coll->children as $child):?>
-        <li><?//=$child?></li>
-    <?//endforeach?>
-    </ul>
+
+    <?foreach($coll->children as $child):?> 
+        <div class="feedback normal">
+            <div class="feedbackContents">
+                <div class="feedbackBlock">
+                    <div class="feedbackAuthor">
+                        <div class="feedbackAuthorAvatar">
+                            <img src="/uploaded_cropped/150x150/<?=$child->avatar?>" width="150" height="150" />
+                        </div>
+                        <div class="feedbackAuthorDetails">
+                            <h2><?=$child->firstname?> <?=$child->lastname?></h2>
+                            <h4><?=$child->position?>, <span><?=$child->companyname?></span></h4>
+                            <p><span style="float:left"><?=$child->countryname?>, <?=$child->city?></span>
+                               <span class="flag flag-<?=strtolower($child->countrycode)?>"></span></p>
+                            <p> <span class="feedbackDate"><?=$child->date?></span></p>
+                        </div>
+                    </div>
+                    <div class="feedbackText">
+                        <div class="feedbackTextTail"></div>
+                        <div class="feedbackTextBubble">
+                            <p><?=$child->text?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="feedbackBlock">
+                    <div class="feedbackMeta">
+                        <div class="feedbackTimestamp">21 minutes ago via <span><a href="#">36Stories</a></span></div> 
+                        <div class="feedbackSocial">
+                            <div class="feedbackSocialTwitter"><a href="http://webmumu.com" class="twitter-share-button">Tweet</a></div>
+                            <div class="feedbackSocialFacebook"><iframe src="//www.facebook.com/plugins/like.php?href=http://webmumu.com&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=154673521284687" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?endforeach?>
+
 <?endforeach?>
