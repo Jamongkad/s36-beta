5f997d9a6115c959bea3f6b975b442f6be6d58d3
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-07 17:16:20 +0800

    ewq

--- application/views/hosted/partials/hosted_feedback_unit_view.php
+++ application/views/hosted/partials/hosted_feedback_unit_view.php
@@ -40,7 +40,7 @@
                 <?
                 $date = $feed->date;
                 $unix = strtotime($date);
-                echo date('F j, Y', $unix)." ".date('h:i:m a', $unix);?>
+                echo date('F j, Y', $unix)?>
                 via <span><a href="#">36Stories</a></span></div> 
                 <div class="feedbackSocial">
 
