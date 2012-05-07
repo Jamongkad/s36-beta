1462c43ad067cfa8046493412322cb1b3f55f92a
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-07 17:18:43 +0800

    DOTA

--- application/views/hosted/partials/hosted_feedback_unit_view.php
+++ application/views/hosted/partials/hosted_feedback_unit_view.php
@@ -40,7 +40,7 @@
                 <?
                 $date = $feed->date;
                 $unix = strtotime($date);
-                echo date('F j, Y', $unix)?>
+                echo date('M j, Y', $unix)?>
                 via <span><a href="#">36Stories</a></span></div> 
                 <div class="feedbackSocial">
 
