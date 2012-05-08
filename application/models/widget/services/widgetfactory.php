ae00dee832f041e6cae74ee5d889d39e4e3d981d
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-08 13:29:01 +0800

    runaway baby

--- application/models/widget/services/widgetfactory.php
+++ application/models/widget/services/widgetfactory.php
@@ -17,7 +17,7 @@ class WidgetFactory {
 
        if ($option->widget == 'embedded') {
            echo "embedded";
-           echo $option->embed_block_type;
+           print_r($option->embed_block_type);
            if ($option->embed_block_type == 'embed_block_x') {
                $widget = new HorizontalEmbedWidget($option);
                return $widget;
