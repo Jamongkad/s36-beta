d4194a03738acbc618d80f8cf5ba1471123eaffe
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-08 18:36:10 +0800

    asd

--- modules/widget/views/widget_embedded_ver_view.php
+++ modules/widget/views/widget_embedded_ver_view.php
@@ -249,9 +249,7 @@
                                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
                             </div>
                             
-                            <div class="facebook-button">
-                                <iframe src="//www.facebook.com/plugins/like.php?href&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=154673521284687" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
-                            </div>                                                        
+                            <div class="facebook-button"></div>                                                        
                         </div>                                               
 
                     </div>                    
