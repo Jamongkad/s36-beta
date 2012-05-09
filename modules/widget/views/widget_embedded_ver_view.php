8985c6abadf2d926d254ae54e2655cb867662bb8
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-09 14:49:52 +0800

    fixing

--- modules/widget/views/widget_embedded_ver_view.php
+++ modules/widget/views/widget_embedded_ver_view.php
@@ -213,11 +213,7 @@
                         <div class="soloFeedbackText"></div>                        
 
                         <div class="soloFeedbackSocialButtons block">                        	
-                            <div class="twitter-button">
-                                <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
-                                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                            </div>
-                            
+                            <div class="twitter-button"></div> 
                             <div class="facebook-button"></div>                                                        
                         </div>                                               
 
