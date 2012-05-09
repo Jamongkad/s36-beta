455e9385a6a1faef21c46d0fb11ff9b83896f50e
mathew <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-09 17:31:33 +0800

    eqwewq

--- public/js/widget/display/master.js
+++ public/js/widget/display/master.js
@@ -6,9 +6,8 @@ var S36Display = new function() {
 		if(target.find('.twitter-button').length == 0){
 
             var twitter = '<a class="twitter-share-button" href="'+link+'" data-url="'+link+'"></a>';
-            target.html(twitter);
 			target.html(
-                $('<div />').addClass('facebook-button')
+                twitter + $('<div />').addClass('facebook-button')
                             .append('<fb:like href="'+link+'" send="false" layout="button_count" width="100" show_faces="false"></fb:like>')
 			);
 			
