c1b8ac4adfddb0f258d657bbbb2b555ba6784750
Mathew Wong <mathew@mathew-Parallels-Virtual-Platform.(none)>
2012-05-11 19:05:36 +0800

    yikes

--- application/models/company/services/companysettings.php
+++ application/models/company/services/companysettings.php
@@ -9,5 +9,17 @@ class CompanySettings {
 
     public function upload_companylogo($files)  {
         $this->files = $files; 
+        $filename = $this->files['your_photo']['name'];
+        $upload_dir = "/var/www/s36-upload-images/uploaded_tmp/";
+
+        if($this->files['your_photo']['error'] > 0) {
+            echo "Return Code: " . $this->files['your_photo']['error'] . "<br/>";
+        } else if(file_exists($upload_dir.$filename)) {
+            echo $filename . " already exists.";
+        } else {
+            $move = move_uploaded_file($this->files['your_photo']['tmp_name'], $upload_dir.$filename);           
+            Helpers::dump($move);
+        }
+
     }
 }
