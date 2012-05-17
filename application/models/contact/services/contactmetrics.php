2fe4bdb5edb98facd32fbd366e4bf14e4e7d25b1
mathew <mathew@ubuntu.(none)>
2012-05-17 12:19:17 +0800

    ewq

--- application/models/contact/services/contactmetrics.php
+++ application/models/contact/services/contactmetrics.php
@@ -1,13 +1,13 @@
 <?php namespace Contact\Services;
 
 use View, Helpers;
-use Contact\Repositories\DBContact;
+use Contact\Repositories\DBContact, S36Auth, DBMetric;
 
 class ContactMetrics {
 
     private $contacts_model;
 
-    public function __construct(DBContact $dbcontact, \DBMetric $metric, S36Auth $auth) {
+    public function __construct(DBContact $dbcontact, DBMetric $metric, S36Auth $auth) {
         $this->contacts_model = $dbcontact;
         $this->auth = $auth;
         $this->metric = $metric;
