8f03a4f7b28d1eba5b312aef9e73e2caf4603475
mathew <mathew@ubuntu.(none)>
2012-05-19 00:49:46 +0800

    ewq

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -67,10 +67,10 @@
 	<div id="headerContent">
     	<div id="headerTitle">
             <?$company_name = ucfirst($company->name);?>
-
         	<strong><?=$company_name?></strong>              
             <?if($company->domain):?>
                 <span class="right padfix">
+                    <a href="http://<?=$company->domain?>" target="_blank"><?="Visit $company_name's Website"?></a>
                     <a href="http://<?=$company->domain?>" target="_blank"><?="Visit $company_name's Website"?></a>
                 </span>
             <?endif?>
