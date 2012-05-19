087e1f3f314baf95a563323f9ad285673e4f0c98
mathew <mathew@ubuntu.(none)>
2012-05-19 13:04:42 +0800

    adding in additional checks

--- application/models/dbaccount.php
+++ application/models/dbaccount.php
@@ -117,27 +117,29 @@ class DBAccount extends S36DataObject {
     public function activate_defaultwidgets() {
         $accounts = $this->companies_wo_defaultwidgets();
         foreach($accounts as $account) {
-             
-            $form = new Widget\Entities\FormWidget;
-            $form->make_default = True;
+            
+            if(!$this->company($account->name)) { 
+                $form = new Widget\Entities\FormWidget;
+                $form->make_default = True;
 
-            $form_data = (object) Array(
-                'widgetkey'   => False
-              , 'widget_type' => 'submit'
-              , 'site_id'     => $account->siteid 
-              , 'company_id' => $account->companyid
-              , 'theme_type' => 'form-aglow'
-              , 'theme_name' => "$account->name Default"
-              , 'embed_type' => 'form'
-              , 'submit_form_text'     => 'Please gives us your feedback'
-              , 'submit_form_question' => 'What are your thoughts about our product/services?'
-              , 'tab_pos'  => 'side'
-              , 'tab_type' => 'tab-l-aglow'
-            );
-           
-            $form->set_widgetdata($form_data);
-            Helpers::dump($form);
-            //$form->save();
+                $form_data = (object) Array(
+                    'widgetkey'   => False
+                  , 'widget_type' => 'submit'
+                  , 'site_id'     => $account->siteid 
+                  , 'company_id' => $account->companyid
+                  , 'theme_type' => 'form-aglow'
+                  , 'theme_name' => "$account->name Default"
+                  , 'embed_type' => 'form'
+                  , 'submit_form_text'     => 'Please gives us your feedback'
+                  , 'submit_form_question' => 'What are your thoughts about our product/services?'
+                  , 'tab_pos'  => 'side'
+                  , 'tab_type' => 'tab-l-aglow'
+                );
+               
+                $form->set_widgetdata($form_data);
+                Helpers::dump($form);
+                //$form->save();
+            }
         }
     }
 }
