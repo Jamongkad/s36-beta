2f367a22d24f253e45f1b55cb909fff0434b5116
mathew <mathew@ubuntu.(none)>
2012-05-22 12:58:28 +0800

    Funky monkey

--- application/models/dbbadwords.php
+++ application/models/dbbadwords.php
@@ -2,6 +2,9 @@
 
 class DBBadWords extends S36DataObject { 
     public function profanity_detection($bad_words) {
+
+        $bad_words = strip_tags($bad_words);
+
         $sth = $this->dbh->prepare(" 
             SELECT  
                 BadWords.word
@@ -11,7 +14,7 @@ class DBBadWords extends S36DataObject {
                 AND :bad_words LIKE CONCAT('%', BadWords.word, '%')
         ");
 
-        $sth->bindParam(':bad_words', strip_tags($bad_words), PDO::PARAM_STR);
+        $sth->bindParam(':bad_words', $bad_words, PDO::PARAM_STR);
         $sth->execute();
         $is_profane = $sth->fetchAll(PDO::FETCH_CLASS);
         return $is_profane;
