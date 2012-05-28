885a19bd080878f46405d04c1b47ed30facd5ed1
mathew <mathew@ubuntu.(none)>
2012-05-28 16:49:19 +0800

    pwer

--- public/js/inbox/Checky.js
+++ public/js/inbox/Checky.js
@@ -65,7 +65,7 @@ Checky.prototype.init = function() {
 
                             var feed_unit = '#' + $(this).val();
                             $(feed_unit).fadeOut(300, function() { $(this).hide(); });      
-                            var my_parent = $(feed_unit).parents('div.feedback-group').children('div.feedback :hidden');
+                            var my_parent = $(feed_unit).parents('div.feedback-group > :hidden');
                             console.log(my_parent);
 
                             /*
