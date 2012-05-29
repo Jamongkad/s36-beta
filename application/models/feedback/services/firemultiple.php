d6437015a7a6545f42cfb5a76bbd7446e74895ce
mathew <mathew@ubuntu.(none)>
2012-05-29 14:13:08 +0800

    ocean blue

--- application/models/feedback/services/firemultiple.php
+++ application/models/feedback/services/firemultiple.php
@@ -73,8 +73,7 @@ class FireMultiple {
         $count = 0;
         foreach($group as $key => $val) {
             $total_units = $this->underscore->first($val);
-            $total_units = $this->underscore->pluck($total_units, 'total_units');
-            Helpers::dump($total_units);
+            Helpers::dump($total_units->total_rows);
             /*
             $count += 1;
             $this->redis->hsetnx("inbox:check-action:".$key, $this->mode.$count, json_encode($val));     
