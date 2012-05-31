2625717b8dbd5173f9f14be47d7e1e85b7eec2fa
mathew <mathew@ubuntu.(none)>
2012-05-31 15:42:03 +0800

    ewq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -315,7 +315,7 @@ return array(
         $is_mobile = (bool)preg_match('#\b(ip(hone|od|ad)|android|opera m(ob|in)i|windows (phone|ce)|blackberry|tablet'.
                             '|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp|laystation portable)|nokia|fennec|htc[\-_]'.
                             '|mobile|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $_SERVER['HTTP_USER_AGENT'] );
-        Helpers::dump($is_mobile);
+        Helpers::dump("Mobile Browser Detected");
     },
 
     //reserved route for Leica and Ryan testing
