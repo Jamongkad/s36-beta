57e6bb3c0de2616098fa506dc73b987af2da581d
mathew <mathew@ubuntu.(none)>
2012-05-31 12:43:26 +0800

    stupido

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -313,7 +313,7 @@ return array(
 
     'GET /tests/manipulate_feedback/(:any)' => function($feedback_id) {
         $feedback = new Feedback\Repositories\DBFeedback;
-        //$feedback->_change_feedback('isDeleted', $feedback_id, 0);      
+        $feedback->_change_feedback('isDeleted', $feedback_id, 0);      
         //$feedback->permanently_remove_feedback($feedback_id) 
         Helpers::dump($feedback_id);
     },
