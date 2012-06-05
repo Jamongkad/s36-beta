039b64ce585a743bd148a0bce4e675cf070e23df
mathew <mathew@ubuntu.(none)>
2012-06-05 18:56:06 +0800

    wq

--- application/models/feedback/services/hostedservice.php
+++ application/models/feedback/services/hostedservice.php
@@ -113,12 +113,15 @@ class HostedService {
         foreach($children_collection as $key => $val) {
 
             $final_node = new StdClass;  
-            //if(isset($featured_feeds[$key])) {
+            /*
+            if(isset($featured_feeds[$key])) {
+                $final_node->head = $featured_feeds[$key];     
+            } 
+            */
+            print_r(isset($featured_feeds[$key])."-".$featured_feeds[$key]);
             $final_node->head = $featured_feeds[$key];     
-            //} 
             $final_node->children = $val;
             $collection[] = $final_node;
-
         }     
 
         return $collection;
