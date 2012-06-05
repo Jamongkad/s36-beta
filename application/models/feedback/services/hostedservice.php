3ecd925c2f507b23013acb6b3116ca8b6c935653
mathew <mathew@ubuntu.(none)>
2012-06-05 19:12:14 +0800

    ewq

--- application/models/feedback/services/hostedservice.php
+++ application/models/feedback/services/hostedservice.php
@@ -117,6 +117,7 @@ class HostedService {
             if(isset($featured_feeds[$key])) {
                 $final_node->head = $featured_feeds[$key];      
                 print_r($featured_feeds[$key]);     
+
                 $final_node->children = $val;
             } 
             
