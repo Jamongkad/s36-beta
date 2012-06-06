f32c6c654e1a1837192036045dad4e8e5a644356
mathew <mathew@ubuntu.(none)>
2012-06-06 15:14:14 +0800

    eqw

--- application/views/hosted/hosted_feedback_single_view.php
+++ application/views/hosted/hosted_feedback_single_view.php
@@ -97,14 +97,14 @@
             <div class="block" style="height:20px"></div>
 
             <div class="feedbackSocial"> 
-                <div class=""> 
+                <div style="float:left"> 
                     <a href="<?=URL::to('single/'.$feedback->id)?>" 
                        data-url="<?=URL::to('single/'.$feedback->id)?>" 
                        data-text="<?=$feedback->text?>"
                        class="twitter-share-button">Tweet</a>
                 </div>
              
-                <div class=""> 
+                <div style="float:left "> 
                     <fb:like href="<?=URL::to('single/'.$feedback->id)?>" send="false" layout="button_count" width="100" show_faces="false"></fb:like> 
                 </div>
 
