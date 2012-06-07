0df8145dc2c354285a41e19f0457294534828ad3
mathew <mathew@ubuntu.(none)>
2012-06-07 12:43:40 +0800

    ewq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -197,7 +197,9 @@ return array(
         $inbox_service->ignore_cache = True;
         $inbox_service->set_filters($filters);
         $feedback = $inbox_service->present_feedback();
-        Helpers::dump($feedback);
+        //Helpers::dump($feedback);
+        echo json_encode($feedback);
+        
         $time_end = microtime(True);
         $time = $time_end - $time_start;
         Helpers::dump("New Algorithm: ".$time." seconds");
