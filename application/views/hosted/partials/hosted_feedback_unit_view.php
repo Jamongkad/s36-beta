123a0070f644fabc48e96bcbcc252c3a8187f237
mathew <mathew@ubuntu.(none)>
2012-06-11 17:00:15 +0800

    fuck

--- application/views/hosted/partials/hosted_feedback_unit_view.php
+++ application/views/hosted/partials/hosted_feedback_unit_view.php
@@ -5,13 +5,13 @@
                 <div class="feedbackAuthorAvatar">
                     <?if($type == 'featured'):?>
                         <?if($feed->avatar):?>
-                            <img src="/uploaded_cropped/150x150/<?=$feed->avatar?>" width="150" height="150" />
+                            <img src="/uploaded_cropped/150x150/<?=$feed->avatar?>"  />
                         <?else:?>
                             <img src="/img/blank-avatar.png" width="150" height="150" />
                         <?endif?>
                     <?else:?>
                         <?if($feed->avatar):?>
-                            <img src="/uploaded_cropped/48x48/<?=$feed->avatar?>" width="48" height="48" class="small-avatar"/>
+                            <img src="/uploaded_cropped/48x48/<?=$feed->avatar?>"  class="small-avatar"/>
                             <?=HTML::image('uploaded_cropped/150x150/'.$feed->avatar, false, array('class' => 'large-avatar'))?>
                         <?else:?>
                             <img src="/img/48x48-blank-avatar.jpg" width="48" height="48" />
