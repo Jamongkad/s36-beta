be667692bb272c3aa911cf7c0bef1a23a89fbb4c
mathew <mathew@ubuntu.(none)>
2012-06-11 16:07:36 +0800

    eqw

--- application/models/feedback/entities/contactdetails.php
+++ application/models/feedback/entities/contactdetails.php
@@ -5,25 +5,53 @@ use Input, DB, UserInfo;
 class ContactDetails { 
 
     private $country_id = 895;
-    private $avatar
-          , $position
+    private $position
           , $city
           , $company
           , $website
-          , $profilelink
-          , $ipaddress
-          , $browser
-          , $firstname
-          , $lastname
-          , $company_name
-          , $login
-          , $email
-          , $site_id;
+          , $profilelink;
+
+    private $contact_data = Array();
+
+    public function __construct() {
+         $this->userinfo = new UserInfo;
+    }
 
     public function insert_contact() { 
+
+        $avatar = Input::get('cropped_image_nm');
+
         if ($country_input = Input::get('country')) {
             $country = DB::table('Country', 'master')->where('code', '=', $country_input)->first();           
-            $this->country_id = $country->countryid;
+            $country_id = $country->countryid;
         }
+
+        if ($avatar == '0' and Input::get('rating') > 2) {
+            //$avatar = $profile_img->auto_resize(Input::get('orig_image_dir'), Input::get('login_type'));
+            $this->position = Input::get('position');
+            $this->city    = Input::get('city');
+            $this->company = Input::get('company');
+            $this->website = Input::get('website');
+            $this->profilelink = Input::get('profile_link');
+        }  
+
+        $login_type = (Input::get('login_type')) ? Input::get('login_type') : '36';
+
+        $this->contact_data = Array(
+            'siteId'    => Input::get('site_id')
+          , 'firstName' => Input::get('first_name')
+          , 'lastName'  => Input::get('last_name')
+          , 'email'     => Input::get('email')
+          , 'countryId' => $country_id
+          , 'avatar'    => $this->avatar
+          , 'position'  => $this->position
+          , 'city'      => $this->city
+          , 'companyName' => $this->company
+          , 'website'     => $this->website
+          , 'profileLink' => $this->profilelink
+          , 'loginType'   => $login_type 
+          , 'ipaddress'   => $this->userinfo->get_ip_long()
+          , 'browser' => $this->userinfo->browser()->getBrowser()
+        );
     }
 }
