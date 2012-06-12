9018f8a1c2fb6f5c61d88b2044bffcff514280fa
mathew <mathew@ubuntu.(none)>
2012-06-12 16:27:49 +0800

    fixing

--- application/models/feedback/entities/contactdetails.php
+++ application/models/feedback/entities/contactdetails.php
@@ -28,7 +28,7 @@ class ContactDetails {
         }
 
         if ($avatar == '0' and Input::get('rating') > 2) {
-            $avatar = $profile_img->auto_resize(Input::get('orig_image_dir'), Input::get('login_type'));
+            $avatar = $this->profile_img->auto_resize(Input::get('orig_image_dir'), Input::get('login_type'));
             $this->position = Input::get('position');
             $this->city    = Input::get('city');
             $this->company = Input::get('company');
