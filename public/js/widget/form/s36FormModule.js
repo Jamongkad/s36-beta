eb7cbee0cc1eaa349ff81f4205c442aab4194968
mathew <mathew@ubuntu.(none)>
2012-06-15 15:32:24 +0800

    eqw

--- public/js/widget/form/s36FormModule.js
+++ public/js/widget/form/s36FormModule.js
@@ -656,7 +656,7 @@ var S36Form = new function() {
                  $("#submission-loader-header").hide();
                  $("#submission-success").show();
                  $("#submission-success-header").show();
-                 $("#feedback_text").val("Mathew");
+                 $("#feedback_text").val("");
                  $("#next").hide();
             }
 		}); 
