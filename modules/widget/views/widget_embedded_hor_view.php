2bc00bfb2840cead0153ec91112036e1651b32bc
mathew <mathew@ubuntu.(none)>
2012-06-15 19:10:19 +0800

    eqw

--- modules/widget/views/widget_embedded_hor_view.php
+++ modules/widget/views/widget_embedded_hor_view.php
@@ -197,7 +197,7 @@
 
                                echo '<div class="g1of3">
                                         <div class="'.$feedback_class.'" id="feedbackid-'.$r->id.'">
-                                            <input type="hidden" class="theFullFeedbackText" data-flag="'.$cc.'" value=\''.strip_tags($r->text).'\' />                        	
+                                            <input type="hidden" class="theFullFeedbackText" data-flag="'.$cc.'" value=\''.trim($text).'\' />                        	
                                             <div class="block">
                                                 <div class="theFeedbackAvatar">
                                                     <img src="'.$avatar.'" />
