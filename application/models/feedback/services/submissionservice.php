217777cbf10209f18b9c836235fbf6a57c7a486d
mathew <mathew@ubuntu.(none)>
2012-06-15 14:04:13 +0800

    oh yeah

--- application/models/feedback/services/submissionservice.php
+++ application/models/feedback/services/submissionservice.php
@@ -13,15 +13,13 @@ class SubmissionService {
         $this->company_id = Input::get('company_id');
         $this->contact_details = $contact;     
         $this->feedback_details = $feedback_details;
-
-        $this->dbh = DB::connection('master')->pdo;
     }
 
-    public function perform() {
-        $this->dbh->beginTransaction();
-        
+    public function perform() {        
+
         $this->contact_details->read_data(); 
         $this->contact_details->write_new_contact();
+
         $this->feedback_details->set_contact_id($this->contact_details->get_contact_id());
         $this->feedback_details->set_company_id($this->company_id);
         $this->feedback_details->read_data();
@@ -31,12 +29,9 @@ class SubmissionService {
         $dash = new DBDashboard; 
         $dash->company_id = $this->company_id;
         $dash->write_summary();
-
         //Upon new feedback always invalidate cache       
         $halcyon = new HalcyonicService($this->company_id);
         $halcyon->save_latest_feedid();
-
-        $this->dbh->commit();
     }
 
     public function metric_response() {
