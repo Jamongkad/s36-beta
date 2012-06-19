40ad594fb19fdf8b66843eadd25733a1ba05cf13
mathew <mathew@ubuntu.(none)>
2012-06-19 16:41:19 +0800

    ewq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -254,7 +254,7 @@ return array(
         $test->fetch_hosted_feedback(); 
         $test->build_data();
         $test->fetch_data_by_set();
-        $test->view_fragment();
+        Helpers::dump($test->view_fragment());
     
         $time_end = microtime(True);
         $time = $time_end - $time_start;
