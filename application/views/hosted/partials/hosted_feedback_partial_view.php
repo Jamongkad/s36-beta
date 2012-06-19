1c45cecc1bfc78e5cf36820f3fa81339e8fadc4a
mathew <mathew@ubuntu.(none)>
2012-06-19 17:03:43 +0800

    wq

--- application/views/hosted/partials/hosted_feedback_partial_view.php
+++ application/views/hosted/partials/hosted_feedback_partial_view.php
@@ -1,11 +1,11 @@
-<?foreach($collection as $coll):?>
-    <?if(property_exists($coll, 'head')):?>
-        <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $coll->head, 'type' => 'featured'))?>
-    <?endif?>
-    
-    <?if(property_exists($coll, 'children')):?>
-        <?foreach($coll->children as $child):?> 
-            <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $child, 'type' => 'normal'))?>
-        <?endforeach?>
-    <?endif?>
-<?endforeach?>
+<?//foreach($collection as $coll):?>
+<?if(property_exists($collection, 'head')):?>
+    <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $collection->head, 'type' => 'featured'))?>
+<?endif?>
+
+<?if(property_exists($collection, 'children')):?>
+    <?foreach($collection->children as $child):?> 
+        <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $child, 'type' => 'normal'))?>
+    <?endforeach?>
+<?endif?>
+<?//endforeach?>
