f9d76f4e1ed91bce8b6fa9a5bae1413c01bc4d57
mathew <mathew@ubuntu.(none)>
2012-06-20 12:07:36 +0800

    dassd

--- application/routes/inbox.php
+++ application/routes/inbox.php
@@ -25,6 +25,8 @@ return array(
         $inbox->ignore_cache = True;
         $feedback = $inbox->present_feedback();
 
+        Helpers::dump($feedback->pagination);
+
         $admin_check = S36Auth::user();
 
         $user_id = S36Auth::user()->userid;
