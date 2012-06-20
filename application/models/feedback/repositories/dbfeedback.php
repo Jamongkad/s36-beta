a8b9ff88adc8f31a0803060138227c2cdafbf321
mathew <mathew@ubuntu.(none)>
2012-06-20 12:31:44 +0800

    ugh I hate doing this

--- application/models/feedback/repositories/dbfeedback.php
+++ application/models/feedback/repositories/dbfeedback.php
@@ -155,6 +155,7 @@ class DBFeedback extends S36DataObject {
         $result_obj = new StdClass;
         $result_obj->result = $date_result;
         $result_obj->total_rows = $row_count->fetchColumn();
+        Helpers::dump($date_sql);
         Helpers::dump($result_obj);
         return $result_obj; 
     }
