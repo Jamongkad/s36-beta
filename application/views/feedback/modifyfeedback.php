cbc6ea3642edb80bbe6af7dfafc3fc71225f0366
mathew <mathew@ubuntu.(none)>
2012-06-21 11:59:52 +0800

    whoooo

--- application/views/feedback/modifyfeedback.php
+++ application/views/feedback/modifyfeedback.php
@@ -198,7 +198,7 @@ $(function() {
                  </tr>
                  <tr>
                      <td class="title">Company:</td>
-                     <td><?=($feedback->companyname == true) ? 'pwet' : null?></td>
+                     <td><?=($feedback->companyname == true) ? 'pwet' : 'choo'?></td>
                      <?if(!$feedback->companyname):?>
                          <?$null_options = Array('disabled' => 'disabled')?>
                      <?endif?>
