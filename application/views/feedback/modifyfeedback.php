650238530c7f694b88c7307537609767efecb0e6
mathew <mathew@ubuntu.(none)>
2012-06-21 12:01:26 +0800

    is it safe here?

--- application/views/feedback/modifyfeedback.php
+++ application/views/feedback/modifyfeedback.php
@@ -198,7 +198,7 @@ $(function() {
                  </tr>
                  <tr>
                      <td class="title">Company:</td>
-                     <td><?=($feedback->companyname == true) ? 'pwet' : 'choo'?></td>
+                     <td><?=$feedback->companyname?></td>
                      <?if($feedback->companyname == false):?>
                          <?$null_options = Array('disabled' => 'disabled')?>
                      <?endif?>
