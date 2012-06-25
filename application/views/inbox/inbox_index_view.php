64f2986c9bca2cb1ec4f97b1f8b8d5243304f396
mathew <mathew@ubuntu.(none)>
2012-06-25 15:14:52 +0800

    fixing

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -133,17 +133,13 @@
                             <!-- end of category picker-->
 
                             <div class="options">
-                                <?if($feed->rating != "POOR"):?>
+                                <?if($feed->rating != "POOR" || $feed->permission_css != 'private-permission'):?>
                                     <?if($admin_check->inbox_approve == 0):?>
                                         <input type="button" class="check" tooltip="Option Disabled" tt_width="75" style="background-position: 0px -51px"/>
                                     <?else:?>
-                                        <?if($feed->permission_css != 'private-permission'):?>  
-                                            <input type="button" class="check" tooltip="<?=($feed->ispublished) ? "Return to Inbox" : "Publish Feedback"?>"  tt_width="85"
-                                            <?=Helpers::switchable($feed->ispublished, $id, $feed->categoryid, URL::to('/feedback/change_feedback_state'), ' style="background-position: 0px -34px"') ?>/>
-                                        <?else:?>
-                                            <input type="button" class="check" tooltip="This feedback cannot be published" tt_width="165" 
-                                                                                                                           style="background-position: 0px -51px !important;"/>
-                                        <?endif?>
+
+                                        <input type="button" class="check" tooltip="<?=($feed->ispublished) ? "Return to Inbox" : "Publish Feedback"?>"  tt_width="85"
+                                        <?=Helpers::switchable($feed->ispublished, $id, $feed->categoryid, URL::to('/feedback/change_feedback_state'), ' style="background-position: 0px -34px"') ?>/>
 
                                     <?endif?>
                                 <?else:?>
