32d7f5c3c7bd311a4d7e0054b1b61dcf96c52cdf
mathew <mathew@ubuntu.(none)>
2012-06-28 00:13:56 +0800

    sheesh

--- application/views/hosted/partials/hosted_feedback_partial_view.php
+++ application/views/hosted/partials/hosted_feedback_partial_view.php
@@ -1,15 +1,9 @@
 <?if($collection):?>
     <?if(is_array($collection)):?>
         <?foreach($collection as $collect):?>
-            <?if($collect->head):?>
-                <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $collect->head, 'type' => 'featured'))?>
-            <?endif?>
-
-            <?if($collect->children):?>
-                <?foreach($collect->children as $child):?> 
-                    <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $child, 'type' => 'normal'))?>
-                <?endforeach?>
-            <?endif?>
+            <?=View::make('hosted/partials/hosted_feedback_iter_view', Array('collection' => $collect))?>
         <?endforeach?>
+    <?else:?> 
+        <?=View::make('hosted/partials/hosted_feedback_iter_view', Array('collection' => $collection))?>
     <?endif?>
 <?endif?>
