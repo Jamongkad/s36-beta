fbfc82103112b868673fcb76aa75bc8c90cdb4a7
mathew <mathew@ubuntu.(none)>
2012-06-27 23:31:58 +0800

    eqwew

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -247,7 +247,7 @@ return array(
 
         $test = new Feedback\Services\HostedService($company_name);
         $test->page_number = $page;
-        $test->starting_units_onload = 0;
+        //$test->starting_units_onload = 0;
         $test->debug = True;
         $test->fetch_hosted_feedback(); 
         $test->scale_feeds();
