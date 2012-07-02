772bf660ccaa562e9ce857ffec9ac5b78a1eb35a
mathew <mathew@ubuntu.(none)>
2012-07-02 13:33:20 +0800

    faster than my buller

--- application/views/feedsetup/feedsetup_hosted_wizard_view.php
+++ application/views/feedsetup/feedsetup_hosted_wizard_view.php
@@ -501,6 +501,7 @@ a.button-gray:hover{background:#dce9f5;}
 
         function check_current_hosted_wizard_step(){
             var cur_step = $('#hosted-wizard').find('.current').attr('id');
+            console.log(cur_step);
             if(cur_step != 'hosed-wizard-step-1'){
                 $('#hosted-wizard-back').fadeIn();		
             }else{
