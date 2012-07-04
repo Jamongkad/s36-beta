9f9b7e3d0f61ac22622e008c4ea500de0337e978
mathew <mathew@ubuntu.(none)>
2012-07-04 13:48:24 +0800

    ewq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -288,7 +288,7 @@ return array(
         Helpers::dump($under);
         foreach($ref->corporate->children as $value) {
             //Helpers::dump($value->default);
-            Helpers::dump($under->last($value->default));
+            Helpers::dump($under->first($value->default));
         }
     },
 
