8c0bf57408875296a0b65d81d45ef31607a3b93b
mathew <mathew@ubuntu.(none)>
2012-07-04 14:50:52 +0800

    oaaahn

--- application/views/feedsetup/feedsetup_hosted_wizard_view.php
+++ application/views/feedsetup/feedsetup_hosted_wizard_view.php
@@ -1,6 +1,6 @@
 <?=Form::open('feedsetup/update_hosted_settings', 'POST', Array('id' => 'update-hosted'))?>
 <?=Form::hidden('company_id', $company_id)?>
-<?=Form::hidden('theme_type', 'hosted-aglow', Array('id' => 'selected-form'))?>
+<?=Form::hidden('theme_type', 'hosted-matte', Array('id' => 'selected-form'))?>
 <div>
     <div class="block graybg" style="margin-top:10px;border-top:1px solid #dedede;">
         <h3>HOSTED FEEDBACK DISPLAY SETUP</h3>
