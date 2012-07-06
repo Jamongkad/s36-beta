7bf9088084670b8bdb3a02ec08d22b2296d9db3c
mathew <mathew@ubuntu.(none)>
2012-07-06 17:33:26 +0800

    Some kind of secret I want to share with you

--- application/views/feedsetup/feedsetup_dashboard_view.php
+++ application/views/feedsetup/feedsetup_dashboard_view.php
@@ -1,5 +1,6 @@
 <div class="block">
     <?if($hosted_full_page):?>
+        <?Helpers::dump($hosted_full_page)?>
         <div id="widget-setup-block">
             <div class="widget-options">
                 <h2 class="ico-widget widget">
