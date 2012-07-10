4c013d64a00cd92c853d224d5fa4f7127022de43
mathew <mathew@ubuntu.(none)>
2012-07-10 18:26:30 +0800

    wq

--- application/views/feedsetup/feedsetup_wizard_view.php
+++ application/views/feedsetup/feedsetup_wizard_view.php
@@ -109,39 +109,6 @@
             <br />
             <div class="form-design-slide" style="margin-left:-10px;">
                 <div class="form-design-prev"></div>
-                    <div class="form-designs grids">  
-                    <?php
-                        $form_slides = '';
-                        $units = 5;
-                        $ctr = 0;
-                        $max = count($form_themes);
-                        
-                        foreach($form_themes as $form_colors => $val){
-                            if (($ctr % $units) == 0) {
-                                $form_slides .= '<div class="form-design-group grids">';
-                                $end = 1;
-                            }
-
-                                $color_name = 'form-'.$form_colors;
-                                $form_slides .= '<div class="form-design" id="'.$color_name.'">
-                                                    <img src="/img/display-thumb.png "/>
-                                                    <span>'.$val.'</span>
-                                                    <!--
-                                                    <div id="preview" class="preview button-gray">
-                                                        Preview
-                                                    </div>
-                                                    -->
-                                                </div>';
-                            
-                             if(($end == $units) || $ctr == ($max - 1)){
-                                $form_slides .= '</div>';
-                            }
-                            $end++;
-                            $ctr++;
-                        }
-                        echo $form_slides
-                    ?>  
-                    </div>
                 <div class="form-design-next"></div>
             </div>
 
