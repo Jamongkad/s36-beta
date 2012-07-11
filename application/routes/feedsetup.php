d6f7292aa95044e45541f68ce3d7dfd6892e476c
mathew <mathew@ubuntu.(none)>
2012-07-11 15:16:46 +0800

    oh shit

--- application/routes/feedsetup.php
+++ application/routes/feedsetup.php
@@ -61,17 +61,19 @@ return array(
 
         $wl = new Widget\Services\WidgetLoader($widget_id); 
         $widget = $wl->widget_obj;
-        Helpers::dump($widget);
-        /* 
+
         $widget_themes->build_menu_structure();
 
         if($widget->widget_type == 'display') {
             $edit_view = 'feedsetup/feedsetup_editdisplay_view';
+            $theme_type = $widget->theme_type;
         } else { 
             $edit_view = 'feedsetup/feedsetup_editform_view';
+            $theme_type = explode("-", $widget->theme_type);
+            $theme_type = $theme_type[1];
         }
 
-        $theme_type = explode("-", $widget->theme_type);
+
         
         return View::of_layout()->partial('contents', $edit_view, Array( 
             'site'            => DB::table('Site', 'master')->where('companyId', '=', S36Auth::user()->companyid)->get()
@@ -81,10 +83,10 @@ return array(
           , 'iframe_code'     => $wl->load_iframe_code()
           , 'js_code'         => $wl->load_widget_init_js_code()
           , 'themes' =>  $widget_themes->perform()
-          , 'themes_parent' => $widget_themes->get_parent($theme_type[1])
+          , 'themes_parent' => $widget_themes->get_parent($theme_type)
           , 'main_themes'   => $widget_themes->main_themes()
         ));
-        */
+
     }),
 
     'GET /feedsetup/wizard/(:any)' => Array(  'name' => 'feedsetup', 'before' => 's36_auth'
