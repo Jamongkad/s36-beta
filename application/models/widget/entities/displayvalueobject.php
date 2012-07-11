24cbcaee78231bf4fa5ab40e3a28208b40d48d7f
mathew <mathew@ubuntu.(none)>
2012-07-11 16:12:28 +0800

    ok

--- application/models/widget/entities/displayvalueobject.php
+++ application/models/widget/entities/displayvalueobject.php
@@ -3,5 +3,17 @@
 use Input;
 use \Widget\Entities\Types\WidgetValueObject;
 
-class DisplayValueObject extends WidgetValueObject {
+class DisplayValueObject extends WidgetValueObject { 
+    $widgetKey;
+    $site_id;
+    $company_id;
+    $theme_type;
+    $theme_name
+    $form_text;
+    $embed_type;
+    $embed_block_type;
+    $embed_effects;
+    $modal_effects;
+    $perms;
+    $widget_type = 'display';
 }
--- application/models/widget/entities/formvalueobject.php
+++ application/models/widget/entities/formvalueobject.php
@@ -3,5 +3,16 @@
 use Input;
 use \Widget\Entities\Types\WidgetValueObject;
 
-class FormValueObject extends WidgetValueObject { 
+class FormValueObject extends WidgetValueObject {  
+    $widgetKey; 
+    $site_id;
+    $company_id;
+    $theme_type;
+    $theme_name;
+    $submit_form_question;
+    $submit_form_text;
+    $tab_pos;
+    $tab_type;
+    $widget_type = 'submit';
+    $embed_type = 'form';
 }
