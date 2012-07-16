a8e0e0ccd759f8d0a58ed0183a211872b75b7ae5
mathew <mathew@ubuntu.(none)>
2012-07-16 11:55:36 +0800

    fixing bug

--- modules/widget/views/widget_embedded_hor_view.php
+++ modules/widget/views/widget_embedded_hor_view.php
@@ -112,7 +112,7 @@
                                 //avatar
                                 $pic = $r->avatar;
                                 if ($r->rules->displayimg == 1) {
-                                    if ($pic == '') {
+                                    if ($pic == false) {
                                         $avatar = "/img/48x48-blank-avatar.jpg";
                                     }else{
                                         $avatar = "/uploaded_cropped/48x48/".$pic;
