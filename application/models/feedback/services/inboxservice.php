edd0a324fb9318ceebccd987b0fd434068ac9bd0
mathew <mathew@ubuntu.(none)>
2012-07-16 18:14:23 +0800

    fuck

--- application/models/feedback/services/inboxservice.php
+++ application/models/feedback/services/inboxservice.php
@@ -81,7 +81,7 @@ class InboxService {
                 $data_obj->num_rows = $date_result->total_rows;
                 $data_obj->pagination = $this->pagination->render();
                 */ 
-                //Helpers::dump($this->filters);
+                Helpers::dump($this->filters);
 
                 if(!$this->ignore_cache) {
                     $this->cache->set_cache($data_obj);     
