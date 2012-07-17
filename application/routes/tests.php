284a4d67fe4cdb2f416f706b1d1b25e0148e39f1
mathew <mathew@ubuntu.(none)>
2012-07-17 15:10:15 +0800

    pwet si boss

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -180,8 +180,8 @@ return array(
         $filters = array(
               'limit'=> 10
             , 'site_id'=> false 
-            , 'filter'=> 'published' //(new arrivals) all (show only) featured published
-            , 'choice'=> 'all' //positive negative neutral profanity flagged mostcontent
+            , 'filter'=> 'all' //(new arrivals) all (show only) featured published
+            , 'choice'=> false //positive negative neutral profanity flagged mostcontent
             , 'date'  => false //date_new date_old
             , 'rating' => false //5 4 3 2 1
             , 'category' => false 
