238a1f044d43344b2cfa7c20437764c7e81f2015
mathew <mathew@ubuntu.(none)>
2012-07-17 18:17:29 +0800

    wq

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -12,7 +12,7 @@
         <?foreach($feeds->children->result as $feed):?>
             <p></p>
             <? $id = $feed->id ?>
-            <div class="feedback" id="<?=$id?>" <?=($feed->isfeatured) ? 'style="background-color: #FFFFAA"' : null?>>
+            <div class="feedback" id="<?=$id?>" <?=($feed->isfeatured) ? 'style="background-color: #FFFFE0"' : null?>>
                 <div class="left">      
                     <input type="checkbox" name="id" value="<?=$id?>" class="check-feed-id"/>
                     <input type="hidden" name="rating" value="<?=$feed->rating?>" class="feed-ratings" />
