4bfb6ac57c4b0580957e4b4fab4387885608e0db
mathew <mathew@ubuntu.(none)>
2012-07-18 18:03:02 +0800

    rew

--- application/models/widget/repositories/dbwidgetthemes.php
+++ application/models/widget/repositories/dbwidgetthemes.php
@@ -69,7 +69,7 @@ class DBWidgetThemes {
                 $child_data->heart   = array($heart_key, $heart);
                 $child_data->like    = array($like_key, $like);
 
-                Helpers::dump(count($child_data));
+                Helpers::dump(count((array)$child_data));
 
                 $data->children[] = $child_data;
             }
