3a909f0956b92fc4464e85ed16606c19a1c1a390
mathew <mathew@ubuntu.(none)>
2012-07-18 17:55:47 +0800

    wq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -290,7 +290,7 @@ return array(
             foreach($main_themes as $key => $value) {
                 if(is_array($value)) {
                     foreach($value as $v) {
-                        Helpers::dump($value);
+                        Helpers::dump($v);
                         /*
                         Helpers::dump($v->default[0]." ".$v->default[1]);               
                         Helpers::dump($v->heart[0]." ".$v->heart[1]);               
