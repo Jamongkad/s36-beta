d1398e4aa3b584c2682b6629a2ae94e50b3a709b
mathew <mathew@ubuntu.(none)>
2012-07-19 14:36:55 +0800

    change the rules

--- application/models/widget/repositories/dbwidgetthemes.php
+++ application/models/widget/repositories/dbwidgetthemes.php
@@ -96,9 +96,9 @@ class DBWidgetThemes {
                     foreach($value as $v) { 
                         foreach($v as $prop => $vl) {
                             if($prop != 'parent') {
-                                print_r($prop);
+                                print_r($prop."\n");
                                 foreach($vl as $theme) {
-                                    print_r($theme);
+                                    print_r($theme."\n");
                                     $ctr += 1;
                                 }
                             }             
