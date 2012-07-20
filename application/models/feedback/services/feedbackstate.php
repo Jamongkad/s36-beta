83cb4ead9abd20cc15457d8ed22862ecb76e56a4
mathew <mathew@ubuntu.(none)>
2012-07-21 00:39:07 +0800

    ok prototype

--- application/models/feedback/services/feedbackstate.php
+++ application/models/feedback/services/feedbackstate.php
@@ -1,5 +1,30 @@
 <?php namespace Feedback\Services;
 
-class FeedbackState {
+use DB;
 
+class FeedbackState {
+    public function process_data($mode, $block_id, $company_id, $extra=False) {
+        $category = DB::Table('Category')->where('companyId', '=', $company_id)
+                                         ->where('intName', '=', 'default')->first(Array('categoryId'));
+
+        $categoryId = $category->categoryid;
+
+        $lookup = Array(
+            'inbox'   => 'SET isDeleted = 0, isPublished = 0, isFeatured = 0, isFlagged = 0, isArchived = 0, indLock = 1, categoryId = '.$categoryId.''
+          , 'restore' => 'SET isDeleted = 0, isPublished = 0, isFeatured = 0, isFlagged = 0, isArchived = 0, indLock = 1, categoryId = '.$categoryId.''
+          , 'publish' => 'SET isDeleted = 0, isPublished = 1, isFeatured = 0, isArchived = 0, categoryId = '.$categoryId.''
+          , 'feature' => 'SET isDeleted = 0, isPublished = 0, isFeatured = 1, isArchived = 0, categoryId = '.$categoryId.''
+          , 'delete'  => 'SET isDeleted = 1, isPublished = 0, isFeatured = 0, isFlagged = 0
+                                           , isSticked = 0, isArchived = 0, indLock = 0, categoryId = '.$categoryId.''
+          , 'fileas'  => 'SET isDeleted = 0, isPublished = 0, isFeatured = 0'.$extra
+          , 'flag'    => 'SET isFlagged = 1'
+        );
+        
+        $column = null;
+        if(array_key_exists($mode, $lookup)) { $column = $lookup[$mode]; }
+
+        $in_query = implode(',', array_fill(0, count($block_id), '?'));
+         
+        print_r($column);
+    }
 }
