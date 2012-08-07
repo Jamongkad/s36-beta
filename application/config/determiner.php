0828c87817489b7cf914a8debec80f01dcf17937
mathew <mathew@ubuntu.(none)>
2012-08-07 12:54:25 +0800

    holy sheeeet

--- application/config/determiner.php
+++ application/config/determiner.php
@@ -49,8 +49,7 @@ class Determiner {
        
 
        if($this->host_host == 'mathew-staging.gearfish.com') {
-           print_r($my_url);
-           $obj->hostname = $my_url[1];
+           $obj->hostname = $subdomain; 
            $obj->host = 'https://'.$subdomain.'.gearfish.com';
            $obj->db   = Array(
                'host' => 'localhost'
