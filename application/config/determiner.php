4a15a9d71e508fbed2963eb8c718533d78d2d7ca
mathew <mathew@ubuntu.(none)>
2012-08-07 12:49:19 +0800

    testing hostname

--- application/config/determiner.php
+++ application/config/determiner.php
@@ -33,6 +33,7 @@ class Determiner {
        
        //Staging
        if($my_url[1] == 'gearfish') {
+           print_r($my_url);
            $obj->hostname = $my_url[1];
            $obj->host = 'https://'.$subdomain.'.gearfish.com';
            $obj->db   = Array(
