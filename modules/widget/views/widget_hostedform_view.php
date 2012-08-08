f3f6df030dda6a3589304ab113fd311fdd5b284b
mathew <mathew@ubuntu.(none)>
2012-08-09 06:39:33 +0800

    I had a way

--- modules/widget/views/widget_hostedform_view.php
+++ modules/widget/views/widget_hostedform_view.php
@@ -435,8 +435,6 @@
                                 <p id="submission-success">
                                     Thank you for taking the time to send in your feedback, and we will get back to you very shortly. 
                                     Feedback submitted to our team typically takes about 24-48 working hours to be reviewed and processed.
-
-                                    <br/>
                                     Click <a href="submit">here</a> to submit more feedback.
 
                                     <div id="share-panel">
