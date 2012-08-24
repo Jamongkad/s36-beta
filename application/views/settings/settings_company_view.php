fbfdb44435ddf26a7e417d238ae3702cb2bde8c7
root <root@li221-107.members.linode.com>
2012-08-24 16:36:03 +0800

    Updating Settings Company View

--- application/views/settings/settings_company_view.php
+++ application/views/settings/settings_company_view.php
@@ -6,76 +6,94 @@
 <div class="block graybg" style="margin-top:10px;border-top:1px solid #dedede;">
     <h3>COMPANY PROFILE SETTINGS</h3>
 </div>
-<div class="block">
-    <p><strong>Company Social Links</strong><br/>
-    <span class="light-blue">socials links are your company homepages on social networking sites. (e.g.) Facebook, Twitter etc.</span>
-    </p>
-
-    <div class="label"><label>Facebook Company Link: </label></div>
-    <div class="input-field">
-        <input type="text" name="fb_link" class="regular-text" value="<?=$company->fb_link?>"/> 
-    </div>
-
-    <div class="label"><label>Twitter Company Link: </label></div>
-    <div class="input-field">
-        <input type="text" name="twit_link" class="regular-text" value="<?=$company->twit_link?>"/> 
-    </div>
-
-    <? $social_links = json_decode($company->social_links); ?>
-    <div class="label"><label>Social Link 1: </label></div>
-    <div class="input-field">
-        <input type="text" name="social_links[]" class="regular-text" 
-               value="<?=($social_links and array_key_exists(0, $social_links)) ? $social_links[0] : null?>"/> 
-    </div>
-
-    <div class="label"><label>Social Link 2: </label></div>
-    <div class="input-field">
-        <input type="text" name="social_links[]" class="regular-text" 
-               value="<?=($social_links and array_key_exists(1, $social_links)) ? $social_links[1] : null?>"/> 
-    </div>
-
-    <div class="label"><label>Social Link 3: </label></div>
-    <div class="input-field">
-        <input type="text" name="social_links[]" class="regular-text" 
-               value="<?=($social_links and array_key_exists(2, $social_links)) ? $social_links[2] : null?>"/> 
-    </div>
-
-    <br/><br/><br/><br/><br/><br/><br/>
-
-    <p><strong>Company Description</strong><br />
-    <span class="light-blue">a short description about your company</span></p>
-
-<textarea class="regular-text" rows="20" name="company_desc">
-<?=$company->description?>
-</textarea> 
-
-    <br/>
-
-    <div class="grids">
-
-        <?if($error):?>
-            <p class="error-msg"><?=$error?></p>
-        <?endif?>
-        <div class="g1of3">
-            <span id="ajax-upload-url" hrefaction="<?=URL::to('/settings/upload')?>"></span>
-            <p><strong>Add Company Logo</strong><br /> 
-               <span class="light-blue">logo size should be atleast 250x180</span></p>
-            <?if($company->logo):?>
-                <?=HTML::image('company_logos/'.$company->logo)?>
-            <?else:?>
-                <?=HTML::image('img/company-logo-filler.jpg')?>
-            <?endif?>
-
-            <div style="padding-left:10px;font-weight:bold;">
-                <div style="margin:5px 0px;">
-                    <input type="file" id="your_photo" class="fileupload" name="your_photo"/> 
-                </div>
+<div class="block">    
+    <p class="small"><strong></strong></p>
+    <div class="grids border-bottom">
+        <div class="g3of4">
+            <div class="grids">
+                <div class="g1of3"><strong>Name</strong> <br />This name will appear <br />on your hosted page</div>
+                <div class="g2of3"><input type="text" class="regular-text" /></div>
             </div>
+            <br />
+            <div class="grids">
+                <div class="g1of3"><strong>Add Company Logo</strong> <br />Select your company's logo and upload it here, you can  adjust it once it's been uploaded.</div>
+                <div class="g2of3"><br /><input type="file" id="your_photo" class="fileupload regular-text" name="your_photo" /> </div>
+            </div>
+            
+            <br />
+            <div class="grids">
+            	<?if($error):?>
+                    <p class="error-msg"><?=$error?></p>
+                <?endif?>
+            </div>
+            <div class="grids">
+                <div id="image-container">
+                <span id="ajax-upload-url" hrefaction="<?=URL::to('/settings/upload')?>"></span>
+                <?if($company->logo):?>
+            	    <?=HTML::image('company_logos/'.$company->logo)?>
+	            <?else:?>
+    	            <?=HTML::image('img/company-logo-filler.jpg')?>
+        	    <?endif?></div>
+            </div>
+            <br />
+            <div class="grids">
+                <div class="g1of3"><strong>Company Description</strong> <br />A short description about<br /> your company 500 char<br /> limit.</div>
+                <div class="g2of3">
+                <textarea class="regular-text" rows="20" name="company_desc"><?=$company->description?></textarea>
+				</div>
+            </div>
+            <br />
+            <div class="grids">
+                <div class="g1of3"><strong class="facebook-icon">Facebook URL : </strong></div>
+                <div class="g2of3"><input type="text" name="fb_link" class="regular-text" value="<?=$company->fb_link?>"/></div>
+            </div>
+            <br />
+            <div class="grids">
+                <div class="g1of3"><strong class="twitter-icon">Twitter URL : </strong></div>
+                <div class="g2of3"><input type="text" name="twit_link" class="regular-text" value="<?=$company->twit_link?>"/></div>
+            </div>
+            <br />
+            <? $social_links = json_decode($company->social_links); ?>
+            <div class="grids">
+                <div class="g1of3"><strong class="website-icon">Social Link 1 : </strong></div>
+                <div class="g2of3"><input type="text" name="social_links[]" class="regular-text" 
+                value="<?=($social_links and array_key_exists(0, $social_links)) ? $social_links[0] : null?>"/> 
+                </div>
+            </div> 
+            <br />
+            <div class="grids">
+                <div class="g1of3"><strong class="website-icon">Social Link 1 : </strong></div>
+                <div class="g2of3"><input type="text" name="social_links[]" class="regular-text" 
+                value="<?=($social_links and array_key_exists(1, $social_links)) ? $social_links[1] : null?>"/> 
+                </div>
+            </div> 
+            <br /> 
+            <div class="grids">
+                <div class="g1of3"><strong class="website-icon">Social Link 1 : </strong></div>
+                <div class="g2of3"><input type="text" name="social_links[]" class="regular-text" 
+                value="<?=($social_links and array_key_exists(2, $social_links)) ? $social_links[2] : null?>"/>
+                </div>
+            </div> 
+            <br /> 
+        </div>
+        <div class="g1of4">
+            &nbsp;
         </div>
     </div>
-
-    <div style="padding-top:20px; padding-bottom:30px; padding-left:5px">
-        <input type="submit" class="large-btn" value="Save Settings" />
+    <br /> 
+    <div class="grids">
+        <div class="g3of4">
+            <div class="grids">
+                <strong><a href="#" style="text-decoration:underline">Preview this on your public feedback page</a></strong>
+                <p>If you leave a field empty, it will not appear on your <a href="#" style="text-decoration:underline">public feedback page</a></p>
+                <br />
+                <input type="submit" class="large-btn" value="Save Settings" />
+            </div>
+        </div>
+        <div class="g1of4">
+            &nbsp;
+        </div>
     </div>
 </div>
+
 <?=Form::close()?>
