39357ddfb4233dbcb2ecc42b360f893e2d5530c7
mathew <mathew@ubuntu.(none)>
2012-08-28 16:22:55 +0800

    making corrections Thank You -> Thank you

--- modules/widget/views/widget_hostedform_view.php
+++ modules/widget/views/widget_hostedform_view.php
@@ -475,7 +475,7 @@
                                     							
                                     <div id="share-panel">
                                         <div id="success-message-excellent">
-                                            <h3>Thank You! We'd greatly appreciate it if you would share your feedback with others!</h3> 
+                                            <h3>Thank you! We'd greatly appreciate it if you would share your feedback with others!</h3> 
                                         </div>
                                         <div class="all-done-feedback-box">
                                             <p></p>
