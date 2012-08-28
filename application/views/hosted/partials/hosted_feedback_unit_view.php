a82b473f0dc334d13094f863f0e55aa3b33983d4
mathew <mathew@ubuntu.(none)>
2012-08-28 18:22:52 +0800

    adding

--- application/views/hosted/partials/hosted_feedback_unit_view.php
+++ application/views/hosted/partials/hosted_feedback_unit_view.php
@@ -108,7 +108,7 @@
                     </div>
                 <?endif?>
                 <?if($feed->displaysbmtdate):?>
-                    <?=$feed->daysago;//=date('M j, Y', strtotime($feed->date))?>
+                    <?="Posted " . $feed->daysago;//=date('M j, Y', strtotime($feed->date))?>
                 <?endif?>
             </div>
         </div>
