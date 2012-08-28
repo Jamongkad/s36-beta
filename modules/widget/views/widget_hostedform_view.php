a8961a0b274562305fcb51fe2e0e6a19dd7925f6
mathew <mathew@ubuntu.(none)>
2012-08-28 16:00:07 +0800

    Fixing

--- modules/widget/views/widget_hostedform_view.php
+++ modules/widget/views/widget_hostedform_view.php
@@ -471,13 +471,11 @@
                                     <h3 id="success-message-ord">
                                         Thank you for taking the time to send in your feedback, and we will get back to you very shortly. 
                                         Feedback submitted to our team typically takes about 24-48 working hours to be reviewed and processed.
-                                        
                                     </h3>
                                     							
                                     <div id="share-panel">
                                         <div id="success-message-excellent">
-                                            <h3>Thank You! We'd greatly appreciate it if you would share your feedback with others!</h3>
-                                       
+                                            <h3>Thank You! We'd greatly appreciate it if you would share your feedback with others!</h3> 
                                         </div>
                                         <div class="all-done-feedback-box">
                                             <p></p>
