f4037d863dcb91fdb75d1bf916733252f7c7c020
mathew <mathew@ubuntu.(none)>
2012-08-29 10:40:31 +0800

    using Helpers relative_time function

--- application/views/hosted/partials/hosted_feedback_unit_view.php
+++ application/views/hosted/partials/hosted_feedback_unit_view.php
@@ -108,7 +108,7 @@
                     </div>
                 <?endif?>
                 <?if($feed->displaysbmtdate):?>
-                    <?="Posted " . strtotime($feed->date);//$feed->daysago;//=date('M j, Y', strtotime($feed->date))?>
+                    <?="Posted " . Helpers::relative_time(strtotime($feed->date));//$feed->daysago;//=date('M j, Y', strtotime($feed->date))?>
                 <?endif?>
             </div>
         </div>
