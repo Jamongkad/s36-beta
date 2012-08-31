8e943f6710d6e595f8944000b5b056828b09d300
mathew <mathew@ubuntu.(none)>
2012-08-31 16:11:58 +0800

    ewq

--- application/views/hosted/partials/hosted_feedback_header_view.php
+++ application/views/hosted/partials/hosted_feedback_header_view.php
@@ -2,10 +2,14 @@
 	<div id="headerContent">
     	<div id="headerTitle">
             <?$company_name = ucfirst($company_name);?>
-            <?if($fullpage_company_name):?>
-        	    <strong><?=$fullpage_company_name?></strong>              
+            <?if(!$sample_name):?>
+                <?if($fullpage_company_name):?>
+                    <strong><?=$fullpage_company_name?></strong>              
+                <?else:?>
+                    <strong><?=$company_name?></strong>              
+                <?endif?>
             <?else:?>
-        	    <strong><?=$company_name?></strong>              
+                <strong><?=$sample_name?></strong>              
             <?endif?>
 
             <span><a href="https://<?=strtolower($company_name).".".$hostname?>.com">View all feedback</a></span>
