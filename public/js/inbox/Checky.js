67948f9c089fbe2644d33eed92bd9b331ed2eebe
mathew <mathew@ubuntu.(none)>
2012-09-06 17:20:46 +0800

    oh shit

--- public/js/inbox/Checky.js
+++ public/js/inbox/Checky.js
@@ -94,32 +94,35 @@ Checky.prototype.init = function() {
                 var poor_feeds = exam_collection.filter(function(el) {
                     return el.rating == 'POOR' && (el.mode == 'publish' || el.mode == 'feature');
                 });
-
+                
+                /*
                 var restricted_feeds = exam_collection.filter(function(el) {
                     return (el.perm == 3 || el.perm == 2 || el.rating == 'POOR') && (el.mode == 'publish' || el.mode == 'feature'); 
                 });
-
+                */
+               
                 //console.log(limited_perm_feeds);
                 //console.log(poor_feeds);
-                console.log(restricted_feeds);
-                
-                /*
-                if(limited_perm_feeds.length > 0) {
+                //console.log(restricted_feeds); 
+                if(limited_perm_feeds.length > 0 and poor_feeds.length > 0) {   
+                    confirm("Warning: There are feedback that have been marked as private/limited and poor and will not be processed.");     
+                }
+
+                if(limited_perm_feeds.length > 0 and poor_feeds.length == 0) {  
                     if(limited_perm_feeds.length == 1) {
-                        confirm("Warning: This feedback has been set as private and will not be processed.");     
-                    } else {
-                        confirm("Warning: There is feedback that has been set as private and will not be processed.");    
+                        confirm("Warning: This feedback has been marked as private/limited and will not be processed.");    
+                    } else { 
+                        confirm("Warning: There are feedback that has been marked as private/limited and will not be processed.");    
                     }    
                 }
 
-                if(poor_feeds.length > 0) {
+                if(poor_feeds.length > 0 and limited_perm_feeds.length == 0) { 
                     if(poor_feeds.length == 1) { 
                         confirm("Warning: This feedback has been rated as poor and will not be processed.");     
                     } else {
                         confirm("Warning: There is feedback that has been rated as poor and will not be processed.");     
                     } 
                 }
-                */
  
                 $("option:first", this).prop("selected", true);
                 if(collection.length > 0) { 
