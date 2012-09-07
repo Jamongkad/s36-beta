9ba83f1b657abf6f18b476dd910897f451e862a0
mathew <mathew@ubuntu.(none)>
2012-09-08 05:33:43 +0800

    wq

--- application/views/feedback/modifyfeedback.php
+++ application/views/feedback/modifyfeedback.php
@@ -60,7 +60,9 @@ $(function() {
                                                                       ($feedback->indlock ? True : Null))?></td></tr>
                         <!--<tr><td>License:		</td><td>Full license</td>-->
                         <tr><td>Submission Date:</td><td>
-                            <input type="text" name="date_change" value="<?=$feedback->unix_timestamp?>" class="regular-text datepicker" id="date" />
+                            <input type="text" name="date_change" 
+                                   value="<?=date("Y-m-d H:i:s", strtotime($feedback->unix_timestamp))?>" 
+                                   class="regular-text datepicker" id="date" />
                         </td>
                     </table>
                 </div>
