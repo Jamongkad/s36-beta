cfa80dc548855bd369afb7814829a62e629c3f2e
mathew <mathew@ubuntu.(none)>
2012-09-11 14:45:05 +0800

    keep

---
+++ application/routes/testify.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'GET /' => function() { 
+        $tf = new Testify("Mathew Test");
+        $tf->beforeEach(function($tf) {
+            $tf->data->calc = 3;  
+        });
+        
+        $tf->test("Testing the add method", function($tf) {
+            $num = $tf->data->calc;     
+            $tf->assert($num);
+            $tf->assert(True);
+            $tf->assert(True);
+        });
+
+        $tf->test("Testing enequality", function($tf) {
+            $num = $tf->data->calc;     
+            $tf->assertEqual($num, 1);
+            $tf->assertEqual($num, 3);
+        });
+
+        $tf->run();
+    }
+);
