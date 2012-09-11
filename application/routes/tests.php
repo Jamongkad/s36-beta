c5b6166772a4b89c6e97660acb56ac7411fe4cae
mathew <mathew@ubuntu.(none)>
2012-09-11 13:28:26 +0800

    eq

--- application/routes/tests.php
+++ application/routes/tests.php
@@ -24,6 +24,7 @@ return array(
         $tf->test("Testing enequality", function($tf) {
             $num = $tf->data->calc;     
             $tf->assertEqual($num, 1);
+            $tf->assertEqual($num, 3);
         });
 
         $tf->run();
