f51b7f0e0ff9fab025781811d78ee95feec0c51a
mathew <mathew@ubuntu.(none)>
2012-09-12 21:22:54 +0800

    wq wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -40,7 +40,7 @@ return array(
         $encrypt = new Encryption\Encryption;
 
         $obj = new StdClass;
-        $obj->username = 'jamongkad';
+        $obj->username = 'mathew-staging';
         $obj->company_id = 6;
         $me = $user->pull_user($obj);
 
