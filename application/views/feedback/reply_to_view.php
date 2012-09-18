b613d227eaa322d7d63f1d74a24099bbdbd50926
mathew <mathew@ubuntu.(none)>
2012-09-19 01:00:10 +0800

    wq

--- application/views/feedback/reply_to_view.php
+++ application/views/feedback/reply_to_view.php
@@ -33,7 +33,7 @@
             </td>
             <td id="bcc-target">
                 <!--<input id="first-bcc" class="regular-text" type="text" name="bcc[]" value="" />-->
-                <textarea class="regular-text" name="bcc" rows="3"></textarea>
+                <textarea class="regular-text" name="bcc" rows="2"></textarea>
             </td>
             <td class="small" valign="top">
                 <span class="reply-text">Click on email addresses below to add to the bcc (<?=HTML::link('/settings', 'configure fastforward', array('class' => 'linky'))?>) </span>
