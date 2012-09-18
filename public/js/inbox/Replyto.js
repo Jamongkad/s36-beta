ed6aa6d552c8f40cb0c8140181105d1fdf1eb933
mathew <mathew@ubuntu.(none)>
2012-09-18 23:40:10 +0800

    look into my eyes

---
+++ public/js/inbox/Replyto.js
@@ -0,0 +1,30 @@
+jQuery(function($) {
+    $('.dialog-form').dialog({
+        autoOpen: false  
+      , height: 500 
+      , width: 700
+      , modal: true
+      , buttons: { 
+            "Send Reply": function() { 
+                 alert("Reply Successful!");
+                 $(this).dialog('close');
+            }
+          , Cancel: function() { $(this).dialog('close'); }
+        }
+    });
+
+    $(document).delegate('.reply', 'click', function(e) {
+        var feedid = $(this).attr('feedid');
+        $('.dialog-form[feedid='+feedid+']').dialog('open');
+        e.preventDefault();
+    });
+
+    /* Old reply implementation
+    $('.reply').bind("click", function(e) {     
+        var href = $(this).attr('hrefaction');
+        window.location = href;
+        e.preventDefault();
+    })
+    */
+
+});
