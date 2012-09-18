f0f12af91228d123a89fca3af2b39ab077173d9d
mathew <mathew@ubuntu.(none)>
2012-09-18 23:49:24 +0800

    wq

--- public/js/inbox/s36application.js
+++ public/js/inbox/s36application.js
@@ -23,34 +23,6 @@ jQuery(function($) {
         e.preventDefault();
     });
     */
-    $('.dialog-form').dialog({
-        autoOpen: false  
-      , height: 500 
-      , width: 700
-      , modal: true
-      , buttons: { 
-            "Send Reply": function() { 
-                 alert("Reply Successful!");
-                 $(this).dialog('close');
-            }
-          , Cancel: function() { $(this).dialog('close'); }
-        }
-    });
-
-    $(document).delegate('.reply', 'click', function(e) {
-        var feedid = $(this).attr('feedid');
-        $('.dialog-form[feedid='+feedid+']').dialog('open');
-        e.preventDefault();
-    });
-
-    /* Old reply implementation
-    $('.reply').bind("click", function(e) {     
-        var href = $(this).attr('hrefaction');
-        window.location = href;
-        e.preventDefault();
-    })
-    */
-
     $(document).delegate(".feedback-avatar", "hover", function(e) {
         if (e.type === "mouseenter")  {
             $('.large-avatar', this).show();
