fb99eca9fcf0b6268e0be4d1b7c6b4dcbe587654
mathew <mathew@ubuntu.(none)>
2012-09-19 09:13:10 +0800

    wq

--- public/js/inbox/Replyto.js
+++ public/js/inbox/Replyto.js
@@ -21,7 +21,7 @@ jQuery(function($) {
         }
       , close: function(e, ui) {    
             var form = $(this).children('form');
-            console.log($(form).children(".block > .regular-text").val(""));
+            console.log($(form).children(".block").chilren());
         }
     });
 
