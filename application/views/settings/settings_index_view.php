bd5d3e8b30365ef02a014b34561adc76e5bf2daa
mathew <mathew@ubuntu.(none)>
2012-09-20 00:48:07 +0800

    wq

--- application/views/settings/settings_index_view.php
+++ application/views/settings/settings_index_view.php
@@ -62,7 +62,7 @@
                 <div class="grids">
                     <div class="g1of3">
                         <span>{{msg.text}}</span>
-                        <input type="text" style="display:hidden" class="regular-text" name="reply_message[]" value="{{msg.text}}"/> 
+                        <input type="text" style="display:none" class="regular-text" name="reply_message[]" value="{{msg.text}}"/> 
                     </div>
                      <div class="g1of3" style="padding-top:8px;">
                          <a href="#" class="gray-btn" ng-click="edit_msg(msg.id, $event)">+ Edit Message</a>
