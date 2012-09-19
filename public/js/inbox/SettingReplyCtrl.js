7b339fb9f116accff1b9149f7ff7ae2130cc52f7
mathew <mathew@ubuntu.(none)>
2012-09-20 07:37:09 +0800

    wq

--- public/js/inbox/SettingReplyCtrl.js
+++ public/js/inbox/SettingReplyCtrl.js
@@ -46,7 +46,7 @@ function SettingReplyCtrl($scope) {
     $scope.delete_msg = function(data, $event) {
         $.ajax({
             url: 'settings/delete_msg/' + data
-          , success: function() { $("div#" + id + ".grids").remove(); }
+          , success: function() { $("div#" + data + ".grids").remove(); }
         }); 
         $event.preventDefault();
     };
