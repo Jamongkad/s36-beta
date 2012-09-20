54a25e58f02a4f961838ff62d1954784ea678ba1
mathew <mathew@ubuntu.(none)>
2012-09-20 16:31:30 +0800

    wq

--- application/models/message/services/settingmessage.php
+++ application/models/message/services/settingmessage.php
@@ -6,8 +6,10 @@ use Input, Config, Helpers;
 class SettingMessage {
 
     private $redis;
+    private $rand_key; 
 
     public function __construct() {
         $this->redis = new redisent\Redis; 
+        $this->rand_key = "msg-".Helpers::randid();
     }
 }
