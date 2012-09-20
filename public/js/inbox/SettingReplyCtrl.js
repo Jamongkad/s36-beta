223f32a4fefe8d13e4c17fce105c35cb9a23b5f0
mathew <mathew@ubuntu.(none)>
2012-09-20 10:12:03 +0800

    wq

--- public/js/inbox/SettingReplyCtrl.js
+++ public/js/inbox/SettingReplyCtrl.js
@@ -18,7 +18,7 @@ myModule.factory('mySettingsService', function($rootScope) {
 function SettingReplyCtrl($scope, mySettingsService) {
 
     console.log(mySettingsService);
-    $scope.msgs = mySettingsService.msg;
+    $scope.msgs = mySettingsService.msg.message;
     /* 
     function my_msg() { 
         $.ajax({
