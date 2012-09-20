e2fe94e2ba275c92600474d856abff1a50610b8b
mathew <mathew@ubuntu.(none)>
2012-09-21 01:48:25 +0800

    wq

--- public/js/inbox/directives/Components.js
+++ public/js/inbox/directives/Components.js
@@ -9,7 +9,9 @@ angular.module('Components', [])
     var msgsel_fn;
 
     msgsel_fn = function(scope, element, attrs) {
-        console.log(element);
+        $(element).bind('click', function(e) {
+            alert("mathew");
+        });
     }
 
     return {
