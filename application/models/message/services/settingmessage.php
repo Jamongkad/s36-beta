50b1b6769a6e11e7cb30d2acd5ad6b81bd2b4f4f
mathew <mathew@ubuntu.(none)>
2012-09-20 17:50:57 +0800

    wq

--- application/models/message/services/settingmessage.php
+++ application/models/message/services/settingmessage.php
@@ -9,15 +9,17 @@ class SettingMessage {
     private $redis;
     private $hash_key; 
     private $company;
-    private $msg_type;
     private $hash_nm;
 
     public function __construct($msg_type) {
-        $this->redis    = new redisent\Redis; 
-        $this->company  = Config::get('application.subdomain');
+        $this->redis    = new redisent\Redis;  
         $this->hash_key = "msg-".Helpers::randid();
-        $this->msg_type = $msg_type;
-        $this->hash_nm  = $this->company.':settings:'.$this->msg_type;
+        $this->company = Config::get('application.subdomain'); 
+        $this->hash_nm  = $this->company.':settings:'.$msg_type;
+    }
+
+    public function save_message($msg) { 
+        $this->redis->hset($this->hash_nm, $this->hash_key, $msg);
     }
 
     public function get_messages() { 
@@ -30,5 +32,13 @@ class SettingMessage {
         }
 
         echo json_encode($collection);
+    }
+
+    public function last_insert() { 
+        $final_data = Array();
+        $final_data['text'] = $redis->hget($this->hash_nm, $this->hash_key);
+        $final_data['id'] = $this->hash_key;
+
+        echo json_encode($final_data);
     }
 }
