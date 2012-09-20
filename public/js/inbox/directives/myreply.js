926aaf7684f2f79a3b08c328f9baf50a91a17de1
mathew <mathew@ubuntu.(none)>
2012-09-21 05:23:40 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -4,6 +4,7 @@ angular.module('Components.reply', [])
         $(element).bind('click', function(e) {
             var feedid = $(this).attr('feedid'); 
             $('.dialog-form[feedid='+feedid+']').dialog('open'); 
+            console.log(feedid); 
             e.preventDefault();
         });
     }
