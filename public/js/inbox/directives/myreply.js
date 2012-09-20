e10b062e9903a48ac02465f13aceffffdf665985
mathew <mathew@ubuntu.(none)>
2012-09-21 05:29:18 +0800

    Music Never Sleep

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -1,11 +1,39 @@
 angular.module('reply', [])
 .directive('myReply', function() {
     return function(scope, element, attrs) {
+
+        $('.dialog-form').dialog({
+            autoOpen: false  
+          , height: 600
+          , width: 700
+          , modal: true
+          , buttons: { 
+                "Send Reply": function() { 
+                     //alert("Reply Successful!"); 
+                     var form = $(this).children('form');
+                     var me = $(this);
+                     form.ajaxSubmit({
+                         dataType: 'json'     
+                       /*
+                       , success: function(data, textStatus, jqXHR) {
+                             console.log(textStatus);
+                             console.log(data);
+                             console.log(me);
+                         }
+                       */
+                     });
+                     me.dialog('close');
+                }
+              , Cancel: function() { $(this).dialog('close'); }
+            }
+          , close: function(e, ui) {    
+                $(".regular-text[name=bcc], .regular-text[name=message]").val("");
+            }
+        });
+
         $(element).bind('click', function(e) {
             var feedid = $(this).attr('feedid'); 
             $('.dialog-form[feedid='+feedid+']').dialog('open'); 
-            console.log(feedid); 
-            console.log($('.dialog-form[feedid='+feedid+']'));
             e.preventDefault();
         });
     }
