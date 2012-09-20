f38bea214b05547367280e6688bf7f81c067bb4a
mathew <mathew@ubuntu.(none)>
2012-09-21 05:54:08 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -1,32 +1,3 @@
-
-        $('.dialog-form').dialog({
-            autoOpen: false  
-          , height: 600
-          , width: 700
-          , modal: true
-          , buttons: { 
-                "Send Reply": function() { 
-                     //alert("Reply Successful!"); 
-                     var form = $(this).children('form');
-                     var me = $(this);
-                     form.ajaxSubmit({
-                         dataType: 'json'     
-                       /*
-                       , success: function(data, textStatus, jqXHR) {
-                             console.log(textStatus);
-                             console.log(data);
-                             console.log(me);
-                         }
-                       */
-                     });
-                     me.dialog('close');
-                }
-              , Cancel: function() { $(this).dialog('close'); }
-            }
-          , close: function(e, ui) {    
-                $(".regular-text[name=bcc], .regular-text[name=message]").val("");
-            }
-        });
 angular.module('reply', [])
 .directive('myReply', function() {
 
@@ -45,3 +16,32 @@ angular.module('reply', [])
     }
 
 })
+
+$('.dialog-form').dialog({
+    autoOpen: false  
+  , height: 600
+  , width: 700
+  , modal: true
+  , buttons: { 
+        "Send Reply": function() { 
+             //alert("Reply Successful!"); 
+             var form = $(this).children('form');
+             var me = $(this);
+             form.ajaxSubmit({
+                 dataType: 'json'     
+               /*
+               , success: function(data, textStatus, jqXHR) {
+                     console.log(textStatus);
+                     console.log(data);
+                     console.log(me);
+                 }
+               */
+             });
+             me.dialog('close');
+        }
+      , Cancel: function() { $(this).dialog('close'); }
+    }
+  , close: function(e, ui) {    
+        $(".regular-text[name=bcc], .regular-text[name=message]").val("");
+    }
+});
