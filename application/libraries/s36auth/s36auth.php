623d62adbc30cdc7c99438007bfda308f8f053b5
mathew <mathew@ubuntu.(none)>
2012-09-20 10:52:09 +0800

    fixing Aryann bug [xpost- Kennwel fix]

--- application/libraries/s36auth/s36auth.php
+++ application/libraries/s36auth/s36auth.php
@@ -26,8 +26,11 @@ class S36Auth {
     }
     
     public static function user_site() { 
-        return DB::table('User', static::$db_name)->join('Site', 'Site.companyId', '=', 'User.companyId')
-                                ->where('User.userId', '=', Session::get(static::$user_id))->get(Array('Site.siteId'));
+        return DB::table('User', static::$db_name)
+                                ->join('Site', 'Site.companyId', '=', 'User.companyId')
+                                ->join('Company', 'User.companyId', '=', 'Company.companyId')
+                                ->where('User.userId', '=', Session::get(static::$user_id))
+                                ->first(Array('Site.siteId', 'Company.companyid'));
     }
 
     public static function login($username, $password, $options=Array()) {
