f384ae8a99d2289364c018819817635a086cd8c9
mathew <mathew@ubuntu.(none)>
2012-09-21 09:56:01 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -31,7 +31,7 @@ angular.module('reply', [])
 })
 .directive('replyBcc', function() {
     return function(scope, element, attrs){
-        console.log(element.children('li > a'));
+        console.log(element.children('li'));
         $(element).children('li a').bind('click', function(e) {
             e.preventDefault();
         });
