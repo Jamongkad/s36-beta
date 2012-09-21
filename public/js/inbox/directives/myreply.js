8aeef8e862a27856aa5c16652320c99e4884f9d7
mathew <mathew@ubuntu.(none)>
2012-09-21 09:38:16 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -11,7 +11,7 @@ angular.module('reply', [])
 .directive('replyCancel', function(){
     return function(scope, element, attrs){
         $(element).bind('click', function(e) {
-            $(this).parents('form textarea[name=bcc]').clearFields();
+            $(this).parents('form textarea[name=bcc] textarea[name=message]').clearFields();
             $(this).parents('.dialog-form').dialog('close');
             e.preventDefault();
         });
