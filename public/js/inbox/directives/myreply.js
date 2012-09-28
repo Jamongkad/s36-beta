f0784ecb2fb3233cf080b714289467831b8f1ac9
mathew <mathew@ubuntu.(none)>
2012-09-28 19:42:46 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -72,7 +72,7 @@ angular.module('reply', [])
 
 $('.dialog-form').dialog({
     autoOpen: false  
-  , height: 550
+  , height: 580
   , width: 700 
   , modal: true
   , close: function(e, ui) {    
