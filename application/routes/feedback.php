2b6dada380992e2f4e3931c2bba655e9f592b52c
mathew <mathew@ubuntu.(none)>
2012-09-29 08:17:57 +0800

    wq

--- application/routes/feedback.php
+++ application/routes/feedback.php
@@ -52,7 +52,6 @@ return array(
     'POST /feedback/requestfeedback' => Array('needs' => 'S36ValueObjects', 'do' => function() use ($dbwidget) {
         $data = Input::get();
         $underscore = new Underscore\Underscore;
-
         $company_id = S36Auth::user()->companyid;
         $widgets = $underscore->first($dbwidget->fetch_widgets_by_company()->form_widgets->widget->widgets);
         $site = DB::Table('Site', 'master')->where('companyId', '=', $company_id)->first();
