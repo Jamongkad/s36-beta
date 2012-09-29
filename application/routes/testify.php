eac01a499f316d789faacaa363e4aee2b0a45966
mathew <mathew@ubuntu.(none)>
2012-09-30 02:33:21 +0800

    wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -75,7 +75,7 @@ return array(
                 "replyto" => "mathew@36stories.com"
             );
 
-            $email_data->bcc = null;//"leicaaah18@gmail.com";
+            $email_data->bcc = "leicaaah18@gmail.com";
             $tf->data->slug->gather($email_data);
             $tf->dump($tf->data->slug);
             $tf->assert($tf->data->slug->send());
