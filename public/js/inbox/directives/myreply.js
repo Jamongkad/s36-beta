cd05a2d02a65aa62208e1466247a5b7920705bd9
mathew <mathew@ubuntu.(none)>
2012-09-29 13:46:46 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -32,7 +32,7 @@ angular.module('reply', [])
 
         //var error_msg = $(".reply-box-error");
 
-        $(element).bind('submit', function(e) {
+        $(element).click('submit', function(e) {
             var form = $(this).parents('form');
             var myparent = $(this).parents('.dialog-form')
             console.log(form);
