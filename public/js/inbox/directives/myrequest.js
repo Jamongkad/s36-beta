dba3e635cd07805ccc195128416e46da75942760
mathew <mathew@ubuntu.(none)>
2012-09-29 10:26:53 +0800

    wq

--- public/js/inbox/directives/myrequest.js
+++ public/js/inbox/directives/myrequest.js
@@ -33,6 +33,10 @@ angular.module('request', [])
 
             $.each($("input[type=text], textarea[name=message]", me), function(index, value) {
                 var elem = $(value);
+
+                if(index == 2) {
+                    console.log(elem);
+                }
                   
                 if( elem.val() == 0 || (index == 2 && !validate_email(elem.val()) && elem.val() == 0) ) {
                      elem.css({'border': '1px solid red'}); 
