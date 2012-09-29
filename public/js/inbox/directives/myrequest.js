0fe1f0b0b28ce22dd9abe230ebd67d79691ac2ef
mathew <mathew@ubuntu.(none)>
2012-09-29 10:16:52 +0800

    wq

--- public/js/inbox/directives/myrequest.js
+++ public/js/inbox/directives/myrequest.js
@@ -33,7 +33,7 @@ angular.module('request', [])
             $.each($("input[type=text], textarea[name=message]", me), function(index, value) {
                 var elem = $(value);
 
-                console.log($('#recipient-email', value));
+                console.log(elem);
 
                 if(elem.val() == 0) {
                      elem.css({'border': '1px solid red'}); 
