a80915f1b1d07e2c813220416f2e689bed6e9dc8
mathew <mathew@ubuntu.(none)>
2012-10-04 20:41:32 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -120,6 +120,8 @@ angular.module('reply', [])
       , link: function(scope, element, attr, ctrl) {
             element.bind("click", function(e) {
                 var text = $("input.regular-text", ctrl.me());
+                console.log(text.val().length);
+                /*
                 if(text.val().length < 0) {
                     alert("This field cannot be blank.");
                 } else { 
@@ -132,6 +134,7 @@ angular.module('reply', [])
                         }
                     });
                 }
+                */
             })
         }
     }    
