728479b18c5c384f74208f20ae67ad226d0c801a
mathew <mathew@ubuntu.(none)>
2012-10-05 01:14:45 +0800

    wq

--- public/js/inbox/services/SettingsService.js
+++ public/js/inbox/services/SettingsService.js
@@ -27,8 +27,8 @@ angular.module('Services', [])
                        + "<div id='${id}' class='msg-reply-text'>"
                            + "<h4>Edit</h4><input type='text' class='regular-text' name='msg' value='${text}' /><br/>"
                                + "<div class='add-msg-box-buttons'>"
-                                   + "<input type='button' class='small-btn' value='Cancel' />&nbsp;&nbsp;"
-                                   + "<input type='submit' class='small-btn' value='Add Item' />"
+                                + "<input type='button' class='small-btn' value='Cancel' />&nbsp;&nbsp;"
+                                + "<input type='submit' class='small-btn' value='Add Item' />"
                                + "</div>"
                        + "</div>"
                    + "</li>"; 
