0aeb698a929a70221288137694d480a30090ffbd
root <root@li221-107.members.linode.com>
2012-10-17 13:39:24 +0800

    ok

---
+++ application/views/settings/settings_downgrade_account_view.php
@@ -0,0 +1,21 @@
+<?php
+$companyInfo		=	$accountInfo->companyInfo;
+$currentPlanInfo  = 	$accountInfo->companyPlanInfo;
+$newPlanInfo		=	$newPlanInfo;
+?>
+<div class="block noborder">
+            	<h3>Please confirm the downgrade from <?=$currentPlanInfo->name?> to <?=$newPlanInfo->name?></h3>
+                <p>You're downgrading your "<?=$companyInfo->company_name?>" account located at <?=$companyInfo->domain?>.</p>
+                <p>Once you click the button below you'll be downgraded to the <?=$newPlanInfo->name?> plan. You'll be charged the new rate of $<?=$newPlanInfo->price?>/month starting on your next bill.</p>
+                <p>
+                	<strong>
+                		<?=HTML::link("settings/upgrade/?planId=$newPlanInfo->planid&action=success",'Downgrade my Account',array('id' => 'downgrade-account','class'=>'gray-btn'))?>
+                		or 
+                		<?=HTML::link('settings/upgrade','Cancel')?>
+                	</strong>
+                </p>
+                <!--
+                <h4>Any questions? Just <a href="#">contact us</a>, we'll help. <a href="#">36Stories support</a></h4>
+                -->
+</div>
+<div style="height:300px;" class="block noborder"></div>
