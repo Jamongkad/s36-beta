0c28eb57dd22715c0fad8e5ddaeee507a98c039c
Robert <robert.mordido@microsourcing.ph>
2012-11-13 18:54:51 +0800

    fullpage update

---
+++ application/models/themes/repositories/dbthemes.php
@@ -0,0 +1,23 @@
+<?php namespace Themes\Repositories;
+
+use S36DataObject\S36DataObject, PDO, StdClass, Helpers, DB, S36Auth;
+
+class DBThemes extends S36DataObject {
+
+var $table_name = 'Themes';
+var $dir_css 	= 'themes/hosted/fullpage';
+var $dir_thumb 	= 'themes/hosted/fullpage/thumb';
+
+
+	public function get_themes(){
+		return DB::table($this->table_name)->get();
+	}
+
+	public function get_theme_by_name($name){
+		return DB::table($this->table_name)
+				->where('theme_name','=',$name)
+				->first();
+	}
+
+
+}
