4adfc9f8b34f284995ae46e9473c3310f17f136a
jamongkad <wrm932@gmail.com>
2012-11-16 18:18:08 +0800

    whoops

--- application/views/partials/fullpage_footer.php
+++ application/views/partials/fullpage_footer.php
@@ -3,4 +3,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js"></script>
 
-<?=HTML::script('js/inbox/S36FullPageModule.js')?>
+<?=HTML::script('js/fullpage/S36FullPageModule.js')?>
