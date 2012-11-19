6b65b7aeea307aa4046f2ce9d18fe59a721436a4
jamongkad <wrm932@gmail.com>
2012-11-19 14:49:30 +0800

    refactoring imageprocessing route

--- application/routes/imageprocessing.php
+++ application/routes/imageprocessing.php
@@ -7,81 +7,79 @@ return array(
         var_dump($imagine);
     }),
 
-    'POST /imageprocessing/upload_coverphoto'=>array('name'=>'upload_coverphoto', 'do' => function() {
-        print_r(Input::all());
-        /*
-        $file       = 'clientLogoImg';
-        $targetpath = "uploaded_images/coverphoto/";
+    'POST /imageprocessing/upload_coverphoto' => array('name'=>'upload_coverphoto', 'do' => function() {
+        $file = Input::all();
         $options    = array(
-            'width'      => 800,
-            'height'     => 500
+            'width'      => 800
+          , 'height'     => 500
+          , 'targetpath' => 'uploaded_images/coverphoto/'
         );
-        upload($file, $targetpath, $options);
-        */
+        upload($file['clientLogoImg'], $options);
     }),
 );
 
-function upload($file, $targetpath="uploaded_images/coverphoto/", $options=null){
+function upload($file, $options=null){
 
-        $error      = Null;
-        $msg        = Null;
-        $filedir    = Null;
-        $width      = Null;
+    $error      = Null;
+    $msg        = Null;
+    $filedir    = Null;
+    $width      = Null;
 
-        if(empty($file))        { die("Please provide a file to be uploaded");}
-        if(empty($targetpath))  { die("Please set the target path for the uploaded file");}
+    if(empty($file))        { die("Please provide a file to be uploaded");}
+    if(empty($options['targetpath']))  { die("Please set the target path for the uploaded file");}
 
-        if(!empty($_FILES[$file]['error']))
-        {
-          switch($_FILES[$file]['error'])
-          {
+    if(!empty($file['error']))
+    {
+      switch($file['error'])
+      {
 
-            case '1':
-              $error = 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
-              break;
-            case '2':
-              $error = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
-              break;
-            case '3':
-              $error = 'The uploaded file was only partially uploaded';
-              break;
-            case '4':
-              $error = 'No file was uploaded.';
-              break;
-            case '6':
-              $error = 'Missing a temporary folder';
-              break;
-            case '7':
-              $error = 'Failed to write file to disk';
-              break;
-            case '8':
-              $error = 'File upload stopped by extension';
-              break;
-            case '999':
-            default:
-              $error = 'No error code avaiable';
-          }
-        }elseif(empty($_FILES[$file]['tmp_name']) || $_FILES[$file]['tmp_name'] == 'none'){
-          $error = 'No file was uploaded..';
-        }elseif(($_FILES[$file]['type'] != "image/jpeg") && 
-            ($_FILES[$file]['type']     != "image/gif")  && 
-            ($_FILES[$file]['type']     != "image/pjpeg")&& 
-            ($_FILES[$file]['type']     != "image/x-png")&& 
-            ($_FILES[$file]['type']     != "image/png")){
-            $error = 'Please Upload Image Files Only';
-        }else{
-            $imagine = new \Imagine\Gd\Imagine();
-            $filename     = date("Ydmhis").$_FILES[$file]['name'];
-            $filedir      = $targetpath.$filename;
-            $image = $imagine->open($_FILES[$file]['tmp_name']);
-            if(isset($options['width']) && isset($options['height'])){
-                $image->resize(new Box($options['width'], $options['height']));
-            }
-            $image->save($filedir, array('quality'=>100));
+        case '1':
+          $error = 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
+          break;
+        case '2':
+          $error = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
+          break;
+        case '3':
+          $error = 'The uploaded file was only partially uploaded';
+          break;
+        case '4':
+          $error = 'No file was uploaded.';
+          break;
+        case '6':
+          $error = 'Missing a temporary folder';
+          break;
+        case '7':
+          $error = 'Failed to write file to disk';
+          break;
+        case '8':
+          $error = 'File upload stopped by extension';
+          break;
+        case '999':
+        default:
+          $error = 'No error code avaiable';
+      }
+    } elseif(empty($file['tmp_name']) || $file['tmp_name'] == 'none'){
+      $error = 'No file was uploaded..';
+    } elseif(($file['type'] != "image/jpeg") ||
+             ($file['type'] != "image/gif") ||
+             ($file['type'] != "image/pjpeg") ||
+             ($file['type'] != "image/x-png") ||
+             ($file['type'] != "image/png")) {
+        $error = 'Please Upload Image Files Only';
+    } else {
+        $imagine = new \Imagine\Gd\Imagine();
+        $filename     = date("Ydmhis").$file['name'];
+        $filedir      = $options['targetpath'].$filename;
+        $image = $imagine->open($file['tmp_name']);
+        if(isset($options['width']) && isset($options['height'])){
+            $image->resize(new Box($options['width'], $options['height']));
         }
-         echo json_encode(Array(
-            "error" => $error
-          , "msg"   => $filedir
-          , "wid"   => $width
-        )); 
+        $image->save($filedir, array('quality'=>100));
+    }
+
+    echo json_encode(Array(
+        "error" => $error
+      , "msg"   => $filedir
+      , "wid"   => $width
+    )); 
 }
