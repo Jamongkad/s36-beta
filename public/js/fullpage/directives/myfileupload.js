7ddd5bacd991753b9b0f6cdaaab3abfac07bd10a
jamongkad <wrm932@gmail.com>
2012-11-22 13:13:44 +0800

    bist

--- public/js/fullpage/directives/myfileupload.js
+++ public/js/fullpage/directives/myfileupload.js
@@ -28,6 +28,7 @@ angular.module('fileupload', [])
                                     $('#dragPhoto').fadeIn('fast');
                                     $('#saveCoverButton').fadeIn('fast');
                                 });
+                                console.log("Image Loading done")
                             });
                         activate_drag(true);
                     }
