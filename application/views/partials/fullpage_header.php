ed68f61f8b976a35b503a0e5d49c9a49c6f54860
jamongkad <wrm932@gmail.com>
2012-11-23 16:15:33 +0800

    wq wq

--- application/views/partials/fullpage_header.php
+++ application/views/partials/fullpage_header.php
@@ -57,7 +57,17 @@
                   , success: function(msg) { 
                         var boxes = $(msg);
                         //container.append( boxes ).masonry( 'appended', boxes ); 
-                        container.append( boxes ).masonry('appended', boxes);
+                        container.append( boxes );
+
+                        $('.feedback').each(function(){
+                            var leftOffset = $(this).css('left');
+                            
+                            if(leftOffset == '400px'){
+                                $(this).css('left','418px');
+                                $(this).find('.feedback-branch').css({'left':'-23px','top':'40px'});
+                            }
+                            
+                        });
                         /*
                         FB.XFBML.parse() ;
 
