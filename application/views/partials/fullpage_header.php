625685349bfe1fe387f0e35d20c9bc8be639f768
jamongkad <wrm932@gmail.com>
2012-11-23 18:46:56 +0800

    fixing multiple events fired

--- application/views/partials/fullpage_header.php
+++ application/views/partials/fullpage_header.php
@@ -47,39 +47,47 @@
 		});
     
 	    var counter = 0;	
-		$(window).bind('scroll.fullpage', function() {
-		   if($(window).scrollTop() + $(window).height() == $(document).height()) {
-                counter += 1;
-                //var page_counter = counter + 1;
-		        var container = $('#feedback-landing');
-                console.log( '/hosted/fullpage_partial/' + counter );
-                /*
-                $.ajax({ 
-                    url: '/hosted/fullpage_partial/' + page_counter
-                  , success: function(msg) { 
-                        var boxes = $(msg);
-                        container.append(boxes);
-               
-                        $('.the-feedbacks').masonry({
-                            itemSelector: '.feedback'
-                          , columnWidth: 100    
-                        }); 
-              
-                 
-                        FB.XFBML.parse() ;
-                        $('.feedback').hover(function(){
-                            $(this).find('.feedbackSocialTwitter').fadeIn();
-                            $(this).find('.feedbackSocialFacebook').fadeIn();
-                        },function(){
-                            $(this).find('.feedbackSocialTwitter').fadeOut();
-                            $(this).find('.feedbackSocialFacebook').fadeOut();
-                        });
-                
-                    }
-                });
-                */
+        function load_more() { 
+            counter += 1;
+            //var page_counter = counter + 1;
+            var container = $('#feedback-landing');
+            console.log( '/hosted/fullpage_partial/' + counter );
+            $(window).bind('scroll', bind_scroll());
+            /*
+            $.ajax({ 
+                url: '/hosted/fullpage_partial/' + page_counter
+              , success: function(msg) { 
+                    var boxes = $(msg);
+                    container.append(boxes);
+           
+                    $('.the-feedbacks').masonry({
+                        itemSelector: '.feedback'
+                      , columnWidth: 100    
+                    }); 
+          
+             
+                    FB.XFBML.parse() ;
+                    $('.feedback').hover(function(){
+                        $(this).find('.feedbackSocialTwitter').fadeIn();
+                        $(this).find('.feedbackSocialFacebook').fadeIn();
+                    },function(){
+                        $(this).find('.feedbackSocialTwitter').fadeOut();
+                        $(this).find('.feedbackSocialFacebook').fadeOut();
+                    });
+            
+                }
+            });
+            */
+        }
+
+        function bind_scroll() {  
+		   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+               $(window).unbind('scroll');
+               load_more();
 		   }
-		});
+        }
+
+		$(window).scroll(bind_scroll());
 		
 		$('.feedback').hover(function(){
 		    $(this).find('.feedbackSocialTwitter').fadeIn();
