7732cf5d6d1f4e0bb6c2439234340add60afec09
jamongkad <wrm932@gmail.com>
2012-11-25 10:10:43 +0800

    wq

--- application/views/partials/fullpage_header.php
+++ application/views/partials/fullpage_header.php
@@ -20,11 +20,20 @@
 <script type="text/javascript" src="js/jquery.fileupload.js"></script>
 <!--new ajax file upload plugin -->
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 <script type="text/javascript">
 		
 	$(document).ready(function(){
 
-        twttr.widgets.load(); // parse the twitter widgets
+        //twttr.widgets.load(); // parse the twitter widgets
         FB.XFBML.parse();	  // parse the facebook widgets
 		$('.the-feedbacks').masonry({
 			itemSelector: '.feedback',
@@ -126,16 +135,6 @@
 		target.slideToggle('fast');
 	}
 */
-</script>
-
-<script>
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
 </script>
 </head>
 <body>
