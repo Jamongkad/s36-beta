f4dea158fa6414da991c0b951fe80a1f299d713c
jamongkad <wrm932@gmail.com>
2012-11-25 10:16:30 +0800

    wq

--- application/views/hosted/partials/hosted_feedback_partial_view.php
+++ application/views/hosted/partials/hosted_feedback_partial_view.php
@@ -72,18 +72,7 @@
                                         </div>
                                     </div>
                                 </div>
-
-                            <?php
-<script>
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-                            break;
+                        <?php
                         case 'tw':
                             $twfeedback = 'twt-feedback';
                             if($feed->feed_data->isfeatured == 1){
