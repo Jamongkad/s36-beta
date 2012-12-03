1e3dc48190b4157c3fd5a67e7117789c31224139
root <root@li221-107.members.linode.com>
2012-12-03 08:42:16 +0800

    before merging with master branch

--- application/models/themes/repositories/dbthemes.php
+++ application/models/themes/repositories/dbthemes.php
@@ -4,21 +4,13 @@ use S36DataObject\S36DataObject, PDO, StdClass, Helpers, DB, S36Auth;
 
 class DBThemes extends S36DataObject {
 
-var $table_name = 'Themes';
-var $dir_css 	= 'themes/hosted/fullpage';
-var $dir_thumb 	= 'themes/hosted/fullpage/thumb';
+    private $table_name  = 'Themes';
 
-
-
-	public function get_themes(){
+	public function get_themes() {
 		return DB::table($this->table_name)->get();
 	}
 
-	public function get_theme_by_name($name){
-		return DB::table($this->table_name)
-				->where('theme_name','=',$name)
-				->first();
+	public function get_theme_by_name($name) {
+		return DB::table($this->table_name)->where('theme_name', '=', $name)->first();
 	}
-
-
 }
