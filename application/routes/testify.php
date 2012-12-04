60f3f661c725952ef4c208ca5ea67de843daff3a
jamongkad <wrm932@gmail.com>
2012-12-04 10:24:24 +0800

    wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -235,7 +235,7 @@ return array(
         });
 
         $tf->test('Twitter', function($tf) {
-            $callback_url = Config::get('application.url').'/testify/twitter_login';
+            $callback_url = urlencode(Config::get('application.url').'/testify/twitter_login');
             $token = $tf->data->twitoauth->getRequestToken($callback_url);
             $tf->dump($token);
             /*
