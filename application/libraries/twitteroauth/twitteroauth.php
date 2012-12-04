2e8ad12da5a0d90daeb02c4206f7d3ff36b3e3f5
jamongkad <wrm932@gmail.com>
2012-12-04 10:35:09 +0800

    wq w

--- application/libraries/twitteroauth/twitteroauth.php
+++ application/libraries/twitteroauth/twitteroauth.php
@@ -181,6 +181,7 @@ class TwitterOAuth {
     if (strrpos($url, 'https://') !== 0 && strrpos($url, 'http://') !== 0) {
       $url = "{$this->host}{$url}.{$this->format}";
     }
+    print_r("Steve");
     print_r($parameters);
     $request = OAuth\OAuthRequest::from_consumer_and_token($this->consumer, $this->token, $method, $url, $parameters);
     $request->sign_request($this->sha1_method, $this->consumer, $this->token);
