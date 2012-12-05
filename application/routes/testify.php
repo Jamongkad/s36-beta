429cd8064c0f7cab8c78dc26856f92c74d19ae8c
jamongkad <wrm932@gmail.com>
2012-12-05 19:23:05 +0800

    wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -161,6 +161,7 @@ return array(
             );
             $social = new Feedback\Services\SocialFeedback($social_services, new Feedback\Repositories\DBSocialFeedback);
             $tf->assert($social->save_social_feeds('tw'));
+            $tf->assertFalse($social->save_social_feeds('tw'));
             $tf->dump($social->save_social_feeds('tw'));
             /*
             $tf->dump($social->save_social_feeds('facebook'));
