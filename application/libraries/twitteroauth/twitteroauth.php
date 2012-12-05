2afe9d8fc45abff2b58e6985149dfc0b0d644978
jamongkad <wrm932@gmail.com>
2012-12-05 19:50:47 +0800

    wq

--- application/libraries/twitteroauth/twitteroauth.php
+++ application/libraries/twitteroauth/twitteroauth.php
@@ -93,15 +93,11 @@ class TwitterOAuth {
     if (is_array($token)) {
       $token = $token['oauth_token'];
     }
-
-    return $this->authorizeURL() . "?oauth_token={$token}";
-    /*
     if (empty($sign_in_with_twitter)) {
       return $this->authorizeURL() . "?oauth_token={$token}";
     } else {
       return $this->authenticateURL() . "?oauth_token={$token}";
     }
-    */
   }
 
   /**
--- application/routes/settings.php
+++ application/routes/settings.php
@@ -84,10 +84,9 @@ return array (
                 $redis->hsetnx($redis_oauth_key, 'oauth_token', $token['oauth_token']);
                 $redis->hsetnx($redis_oauth_key, 'oauth_token_secret', $token['oauth_token_secret']);
 
-                $login_url = $twitoauth->getAuthorizeURL($token['oauth_token']);    
-                Helpers::dump($login_url);
-                //header('Location:'.$login_url);
-                //exit;
+                $login_url = $twitoauth->getAuthorizeURL($token['oauth_token'], $sign_in_with_twitter=False);     
+                header('Location:'.$login_url);
+                exit;
             } else {
                 $twitoauth = new TwitterOAuth($twitter_key, $twitter_secret, 
                                               $redis->hget($redis_oauth_key, 'oauth_token'), $redis->hget($redis_oauth_key, 'oauth_token_secret'));
@@ -113,7 +112,7 @@ return array (
             }                
         }
 
-        //return Redirect::to('settings/social');           
+        return Redirect::to('settings/social');           
     }),
 
     'GET /settings/disconnect/(:any)' => Array('name' => 'settings', 'before' => 's36_auth', 'do' => function($social) use ($redis, $redis_oauth_key
