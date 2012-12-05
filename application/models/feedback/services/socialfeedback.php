d538ac7f9df31812ca241cd87a1fb3ade0cadb5e
jamongkad <wrm932@gmail.com>
2012-12-05 19:22:26 +0800

    wq

--- application/models/feedback/services/socialfeedback.php
+++ application/models/feedback/services/socialfeedback.php
@@ -15,16 +15,13 @@ class SocialFeedback {
 
     public function save_social_feeds($social) {
         $social_fd = $this->socialfeeds[$social];
-        return $social_fd;
-        /*
+
         if($social_feed = $social_fd->result) { 
             foreach($social_feed as $feed) {
                 $this->dbsocial->convert($feed);    
             }
-            return true;
-        } else {     
-            throw new Exception("Social Feed is missing!");
-        }
-        */
+        } else {
+            return false;     
+        }  
     }
 }
