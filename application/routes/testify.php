f4892267d7e547b68d2cb4487afb5b8c1d91d322
jamongkad <wrm932@gmail.com>
2012-12-05 18:42:01 +0800

    qw

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -160,7 +160,7 @@ return array(
                 'tw' => $tf->data->twitter->pull_tweets()
             );
             $tf->dump($social_services['tw']);
-            $social = new Feedback\Services\SocialFeedback($social_services, new Feedback\Repositories\DBSocialFeedback);
+            //$social = new Feedback\Services\SocialFeedback($social_services, new Feedback\Repositories\DBSocialFeedback);
             //$tf->dump($social->save_social_feeds('tw'));
             /*
             $tf->dump($social->save_social_feeds('facebook'));
