e032c39b0f765f2bddb3e7e1f6bf9dcead1c8529
jamongkad <wrm932@gmail.com>
2012-12-05 20:45:45 +0800

    wq

--- application/models/company/repositories/dbcompanysocialaccount.php
+++ application/models/company/repositories/dbcompanysocialaccount.php
@@ -5,19 +5,36 @@ use S36DataObject\S36DataObject, PDO, StdClass, Helpers, DB, S36Auth;
 class DBCompanySocialAccount extends S36DataObject {
 
     public function fetch_social_account($social) { 
+        //if user is logged in
+        if($this->company_id) {
+            $sql = "SELECT * FROM CompanySocialAccount 
+                    WHERE 1=1 
+                        AND CompanySocialAccount.companyId = :company_id
+                        AND CompanySocialAccount.socialAccountOrigin = :origin
+                    LIMIT 1";
 
-        $sql = "SELECT * FROM CompanySocialAccount 
-                WHERE 1=1 
-                    AND CompanySocialAccount.companyId = :company_id
-                    AND CompanySocialAccount.socialAccountOrigin = :origin
-                LIMIT 1";
-
-        $sth = $this->dbh->prepare($sql);
-        $sth->bindParam(':company_id', $this->company_id);
-        $sth->bindParam(':origin', $social);
-        $sth->execute();
-        $result = $sth->fetch(PDO::FETCH_OBJ);
-        return $result;
+            $sth = $this->dbh->prepare($sql);
+            $sth->bindParam(':company_id', $this->company_id);
+            $sth->bindParam(':origin', $social);
+            $sth->execute();
+            $result = $sth->fetch(PDO::FETCH_OBJ);
+            return $result; 
+        } else {
+            $sql = "SELECT * FROM CompanySocialAccount 
+                    INNER JOIN
+                        Company
+                        ON CompanySocialAccount.companyId = Company.companyId
+                    WHERE 1=1 
+                        AND Company.name = :company_name
+                        AND CompanySocialAccount.socialAccountOrigin = :origin
+                    LIMIT 1";
+            
+            $sth->bindParam(':company_name', $this->company_name);
+            $sth->bindParam(':origin', $social);
+            $sth->execute();
+            $result = $sth->fetch(PDO::FETCH_OBJ);
+            return $result; 
+        }
     }
 
     public function save_social_account($data) { 
