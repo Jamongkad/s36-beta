83946c1bf8620ed37f982122c88e43670c7dcb50
jamongkad <wrm932@gmail.com>
2012-12-05 17:30:53 +0800

    wq

--- application/routes/settings.php
+++ application/routes/settings.php
@@ -84,8 +84,6 @@ return array (
 
             if($redis->hgetall($redis_oauth_key) == false) {   
                 //redirects back to /settings/connect/twitter
-                echo "Mathew";
-                /*
                 $callback_url = Config::get('application.url').'/settings/connect/twitter';
                 $token = $twitoauth->getRequestToken($callback_url);
 
@@ -95,11 +93,7 @@ return array (
                 $login_url = $twitoauth->getAuthorizeURL($token['oauth_token']);    
                 header('Location:'.$login_url);
                 exit;
-                */
-
             } else {
-                echo "Irene";
-                /*
                 $twitoauth = new TwitterOAuth($twitter_key, $twitter_secret, $redis->hget($redis_oauth_key, 'oauth_token'), $redis->hget($redis_oauth_key, 'oauth_token_secret'));
                 $token_credentials = $twitoauth->getAccessToken();
 
@@ -121,7 +115,6 @@ return array (
                 }
                 //place redirect code here...should go back to /settings/social 
                 return Redirect::to('settings/social');           
-                */
             }                
         }
     }),
