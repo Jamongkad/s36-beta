69ff409f2305042aea006d3f99e92115a9a19ce6
jamongkad <wrm932@gmail.com>
2012-12-06 20:22:32 +0800

    wq

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -3,33 +3,7 @@ angular.module('formbuilder', [])
     return {
         restrict: 'A'     
       , link: function(scope, element, attrs) { 
-            $(element).formbuilder({
-              , 'useJson': true
-              , messages: { 
-                    save				: "Save",
-                    add_new_field		: "Add New Field...",
-                    text				: "Text Field",
-                    title				: "Title",
-                    paragraph			: "Paragraph",
-                    checkboxes			: "Checkboxes",
-                    radio				: "Radio",
-                    select				: "Select List",
-                    text_field			: "Text Field",
-                    label				: "Label",
-                    paragraph_field		: "Paragraph Field",
-                    select_options		: "Select Options",
-                    add					: "Add",
-                    checkbox_group		: "Checkbox Group",
-                    remove_message		: "Are you sure you want to remove this element?",
-                    remove				: "Remove",
-                    radio_group			: "Radio Group",
-                    selections_message	: "Allow Multiple Selections",
-                    hide				: "Hide",
-                    required			: "Required",
-                    show				: "Show"
-                }
-            });
-
+            $(element).formbuilder({'useJson': true});
             $(element).children('ul').sortable({ opacity: 0.6, cursor: 'move'});
         }
     }    
