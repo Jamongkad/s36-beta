23c9cac9672bfcedb9c370c34216bbffc0f38f5f
jamongkad <wrm932@gmail.com>
2012-12-06 19:25:33 +0800

    wq

--- application/views/feedsetup/feedsetup_create_form_widget_view.php
+++ application/views/feedsetup/feedsetup_create_form_widget_view.php
@@ -59,7 +59,7 @@
         </div>
         <div class="widget-options">
             <h2><span>Step 3 :</span> Create custom fields for your form (optional)</h2>
-            <div my-formbuilder style="padding: 10px 20px 0"></div>
+            <div my-formbuilder style="padding: 25px"></div>
         </div>
         <!--
         <div class="widget-options"> 
