1a881f01f39aaa69ccd716d2af307b3a1a6570af
jamongkad <wrm932@gmail.com>
2012-12-06 16:40:32 +0800

    wq

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -2,8 +2,14 @@ angular.module('formbuilder', [])
 .directive('myFormbuilder', function() {
     return {
         restrict: 'A'     
-      , link: function(scope, element, attrs) {
-            console.log($(element));
+      , link: function(scope, element, attrs) { 
+            $(element).formbuilder({
+                'save_url': 'pwet'
+              , 'load_url': 'irene'
+              , 'useJson': true
+            });
+
+            $(element).children('ul').sortable({ opacity: 0.6, cursor: 'move'});
         }
     }    
 })
