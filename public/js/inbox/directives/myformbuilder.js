b6887545d14003807397569ea20b146c3f40afc6
jamongkad <wrm932@gmail.com>
2012-12-06 18:07:33 +0800

    wq

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -7,6 +7,10 @@ angular.module('formbuilder', [])
                 'save_url': 'pwet'
               , 'load_url': 'irene'
               , 'useJson': true
+              , messages { 
+                    save				: "Pwet",
+                    add_new_field		: "Add New Pwet...",
+                }
             });
 
             $(element).children('ul').sortable({ opacity: 0.6, cursor: 'move'});
