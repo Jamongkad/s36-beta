552050f594d49be58d6657066b66a0443eadf066
jamongkad <wrm932@gmail.com>
2012-12-07 12:52:18 +0800

    wq

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -4,7 +4,7 @@ angular.module('formbuilder', [])
         restrict: 'A'     
       , link: function(scope, element, attrs) { 
             $(element).formbuilder({'useJson': true});
-            $(element).children('ul').sortable({ opacity: 0.6, cursor: 'move'});
+            $(element).children('ul').sortable({ opacity: 0.6, cursor: 'move', axis: "y" });
         }
     }    
 })
