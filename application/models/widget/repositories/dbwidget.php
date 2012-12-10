2c439b697165336dc66d3838d7f5b92d520e1968
jamongkad <wrm932@gmail.com>
2012-12-10 13:59:12 +0800

    wq

--- application/models/widget/repositories/dbwidget.php
+++ application/models/widget/repositories/dbwidget.php
@@ -13,10 +13,13 @@ class DBWidget extends S36DataObject {
         //if widget has children....kill them...
         if($obj->children) {
 
-            $wc = $this->dbh->prepare("DELETE FROM WidgetClosure WHERE ancestor_id = ?; DELETE FROM WidgetStore WHERE widgetStoreId = ?"); 
+            $wc = $this->dbh->prepare("DELETE FROM WidgetClosure WHERE ancestor_id = ?;"); 
+            $ws = $this->dbh->prepare("DELETE FROM WidgetStore WHERE widgetStoreId = ?");
+
             foreach($obj->children as $rows) {
                 $child_id = $rows->widgetstoreid;
                 $wc->execute(Array($child_id));
+                $ws->execute(Array($child_id));
                 /*
                 $sth = $this->dbh->prepare("DELETE FROM WidgetClosure WHERE ancestor_id = :child_id"); 
                 $sth->bindParam(':child_id', $child_id, PDO::PARAM_STR);
