e343b54009b94dd74bdf76d7e7ba800706b3554e
jamongkad <wrm932@gmail.com>
2012-12-11 18:19:07 +0800

    I've done bad things.

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -41,7 +41,7 @@ angular.module('formbuilder', [])
                             url: "/feedsetup/buildmetadata_options",
                             data: $("ul[id^=frmb-]").serializeFormList({prepend: "frmb"}) + "&form_id=" + widget_store_id + "&company_id=" + company_id
                         });
-                        //window.location = formcode_url;
+                        window.location = formcode_url;
                     }
                 });
                 e.preventDefault();
