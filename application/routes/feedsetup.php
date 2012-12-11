f01e236999b1bde609d5f3af2356a649496768c9
jamongkad <wrm932@gmail.com>
2012-12-11 17:44:33 +0800

    wq wq

--- application/routes/feedsetup.php
+++ application/routes/feedsetup.php
@@ -196,11 +196,21 @@ return array(
             $form = new Widget\Services\Formbuilder\Formbuilder(Input::get());
             $data = $form->get_encoded_form_array();
 
-            return DB::table('WidgetFormMetadata', 'master')->insert(Array(
-                'widgetStoreId' => Input::get('form_id')
-              , 'companyId'     => Input::get('company_id')
-              , 'formStructure' => $data['form_structure']
-            ));
+            $metadata_exists = DB::table('WidgetFormMetadata', 'master')->where('widgetStoreId', '=', Input::get('form_id'))->first();
+
+            if($metadata_exists) { 
+                return DB::table('WidgetFormMetadata', 'master')->update(Array(
+                    'widgetStoreId' => Input::get('form_id')
+                  , 'companyId'     => Input::get('company_id')
+                  , 'formStructure' => $data['form_structure']
+                ));
+            } else {                
+                return DB::table('WidgetFormMetadata', 'master')
+                           ->where('widgetStoreId', '=', Input::get('form_id'))
+                           ->where('companyId', '=', Input::get('company_id'))
+                           ->update(array('formStructure' => $data['form_structure']));
+            }
+
         } 
     },
     
