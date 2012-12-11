0a163290dff498354fc8eef71f36b513b7ab03bf
jamongkad <wrm932@gmail.com>
2012-12-11 18:07:46 +0800

    wq

--- application/routes/feedsetup.php
+++ application/routes/feedsetup.php
@@ -199,16 +199,22 @@ return array(
                                    ->where('widgetStoreId', '=', Input::get('form_id'))->first();
 
             if(!$metadata_exists) { 
+                Helpers::dump('No metadata');
+                /*
                 return DB::table('WidgetFormMetadata', 'master')->update(Array(
                     'widgetStoreId' => Input::get('form_id')
                   , 'companyId'     => Input::get('company_id')
                   , 'formStructure' => $data['form_structure']
                 ));
+                */
             } else {                
+                Helpers::dump('Existing metadata');
+                /*
                 return DB::table('WidgetFormMetadata', 'master')
                            ->where('widgetStoreId', '=', Input::get('form_id'))
                            ->where('companyId', '=', Input::get('company_id'))
                            ->update(array('formStructure' => $data['form_structure']));
+                */
             }
         } 
     },
