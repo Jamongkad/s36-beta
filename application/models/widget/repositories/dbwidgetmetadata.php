c5e170447ee3421711ac0f44bf21234e336f4384
jamongkad <wrm932@gmail.com>
2012-12-12 15:16:09 +0800

    wq

--- application/models/widget/repositories/dbwidgetmetadata.php
+++ application/models/widget/repositories/dbwidgetmetadata.php
@@ -15,18 +15,28 @@ class DBWidgetMetadata extends S36DataObject {
     }
 
     public function metadata_exists() {
-        
+        return DB::table($this->table, $this->db)->where('widgetStoreId', '=', $this->widgetstore_id)->first();
     }
 
     public function save() {
+        return DB::table($this->table, $this->db)->insert(Array(
+            'widgetStoreId' => $this->widgetstore_id 
+          , 'companyId'     => $this->company_id
+          , 'formStructure' => $this->form_structure_data
+        ));
 
     }
 
-    public function update() {
-    
+    public function update() { 
+        return DB::table($this->table, $this->db)
+                   ->where('widgetStoreId', '=', $this->widgetstore_id)
+                   ->where('companyId', '=', $this->company_id)
+                   ->update(array('formStructure' => $this->form_structure_data));
     }
 
-    public function delete() {
-        
+    public function delete() { 
+        return DB::table($this->table, $this->db)
+                   ->where('widgetStoreId', '=', $this->widgetstore_id)
+                   ->delete();
     }
 }
