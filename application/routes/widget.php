aee155bdc24b7f1ee5fb6a6ebf0e79b3bbdf7804
jamongkad <wrm932@gmail.com>
2012-12-13 18:34:13 +0800

    wq

--- application/routes/widget.php
+++ application/routes/widget.php
@@ -15,12 +15,11 @@ return array(
     //these routes are used by js loaders for source construction
     'GET /widget/tab_position' => function() {
         echo Helpers::tab_position_css_output();
-
     },
      
     'GET /widget/js_output' => function() { 
-        $wl = new Widget\Services\WidgetLoader( Input::get('widgetId') ); 
-        $js = new Widget\Services\ClientRender( $wl->load() );
+        $wl = new Widget\Services\WidgetLoader(Input::get('widgetId'), $load_submission_form=True); 
+        $js = new Widget\Services\ClientRender($wl->load());
         return $js->js_output();
     },
     //end of these muthafuckas
