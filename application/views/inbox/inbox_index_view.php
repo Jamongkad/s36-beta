041d0f4953c027206a9208359333b4134b5378b3
jamongkad <wrm932@gmail.com>
2012-12-14 13:42:39 +0800

    wq

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -11,7 +11,7 @@
                     <p></p>
                     <? $id = $feed->id ?>
                     
-                    <div class="dialog-form" feedid="<?=$id?>" style="height:538px !important;overflow:hidden;"> 
+                    <div class="dialog-form" feedid="<?=$id?>"> 
                         <?=Form::open('feedback/reply_to', 'POST', array('class' => 'reply-form'))?>
                             <?=View::make('feedback/reply_to_view', array(
                                    'user' => $admin_check, 'feedback'=> $feed, 'reply_message' => $reply_message
