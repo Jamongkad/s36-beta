3684e58692203a3f724043ab15555402606cddcd
jamongkad <wrm932@gmail.com>
2012-12-20 00:49:23 +0800

    wq

--- application/views/widget/widget_submissionform_new_view.php
+++ application/views/widget/widget_submissionform_new_view.php
@@ -150,7 +150,7 @@ FB.init({appId: '<?=$fb_app_id?>', status: true, cookie: true});
                         <?if($form_render):?>
                             <?=$form_render->render_metadata()?>
                         <?endif?>
-
+                        <!--
                         <div class="form-field-block">
                             <input type="text" class="regular-custom-field" value="" title="" name="custom-field-1" />
                         </div>
@@ -160,6 +160,7 @@ FB.init({appId: '<?=$fb_app_id?>', status: true, cookie: true});
                         <div class="form-field-block">
                             <input type="text" class="regular-custom-field" value="" title="Please Enter Product Code" name="custom-field-1" />
                         </div>
+                        -->
 
                         <div class="form-field-block">
                             <input type="text" class="regular-custom-field" value="" title="" name="custom-field-1" />
