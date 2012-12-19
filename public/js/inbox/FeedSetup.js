085bd2d1960cbc59ab4f5574aa1055d9c0c90822
jamongkad <wrm932@gmail.com>
2012-12-19 17:39:18 +0800

    wq

--- public/js/inbox/FeedSetup.js
+++ public/js/inbox/FeedSetup.js
@@ -41,7 +41,7 @@ jQuery(function($) {
             , data: {submit_form_text: $("input[name=submit_form_text]").val(), submit_form_question: $("textarea[name=submit_form_question]").val()}
             , dataType: 'json'
             , success: function(data) { 
-                  s36Lightbox(data.width, data.height, data.html_view);
+                  console.log(s36Lightbox(data.width, data.height, data.html_view));
               } 
         });
         e.preventDefault();
