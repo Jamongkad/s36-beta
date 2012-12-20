6f038144e56611076525dbcae0f888ef73b641e5
jamongkad <wrm932@gmail.com>
2012-12-20 15:50:01 +0800

    wq

--- application/models/widget/services/formbuilder/formbuilder.php
+++ application/models/widget/services/formbuilder/formbuilder.php
@@ -539,7 +539,12 @@ class Formbuilder {
         }
 	
 		if(isset($field['title']) && !empty($field['title'])){
-			$html .= sprintf('<label for="%s">%s</label>' . "\n", $this->elemId($field['title']), $field['title']);
+            if($metadata_render == False) { 
+    			$html .= sprintf('<label for="%s">%s</label>' . "\n", $this->elemId($field['title']), $field['title']);
+            } else { 
+        	    $html .= sprintf('<div class="title">%s</div>' . "\n", $field['title']);        
+            }
+
 		}
 		$field['values'] = (array)$field['values'];
 		if(isset($field['values']) && is_array($field['values'])){
