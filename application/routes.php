f6cfa47474baae732f0fa2c2fecdcdde20b75c37
jamongkad <wrm932@gmail.com>
2012-12-21 18:03:26 +0800

    wq

--- application/routes.php
+++ application/routes.php
@@ -82,13 +82,16 @@ return array(
 
     'POST /submit_feedback' => function() use($company_name, $company, $hosted_settings){
         //Helpers::dump(Input::get('metadata'));
+        $_ = new Underscore;
         if(Input::has('metadata')) {
             $collection = Array();
             foreach(Input::get('metadata') as $data) {
                 $collection[$data['name']][] = $data['value'];
             }
 
-            Helpers::dump($collection);
+            //Helpers::dump($collection);
+            $group = $_->groupBy(Input::get('metadata'), 'name'); 
+            Helpers::dump($group);
         }
       
        
