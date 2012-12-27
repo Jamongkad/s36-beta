b7d37facfbc2843ccedfdd54b9613f22a85e7855
jamongkad <wrm932@gmail.com>
2012-12-27 14:50:37 +0800

    wq

--- application/views/widget/widget_submissionform_new_view.php
+++ application/views/widget/widget_submissionform_new_view.php
@@ -144,6 +144,14 @@ FB.init({appId: '<?=$fb_app_id?>', status: true, cookie: true});
                         </div>
                     </div>
                     <!-- end of star ratings -->
+
+                    <!-- start of form title -->
+                    <div class="form-feedback-title">
+                        <div class="form-field-block">
+                            <input type="text" class="regular-custom-field" value="" title="Title of your feedback" name="feedback_title" />
+                        </div>
+                    </div>
+                    <!-- end of form title -->
                      
                     <!-- start of feedback form -->
                     <div class="form-feedback-textbox">
