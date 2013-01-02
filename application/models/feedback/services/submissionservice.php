59e570180a5cd07ec55d032335b1df3e07a671a8
jamongkad <wrm932@gmail.com>
2013-01-02 19:02:14 +0800

    Adding additional comments to submissionservice class

--- application/models/feedback/services/submissionservice.php
+++ application/models/feedback/services/submissionservice.php
@@ -28,9 +28,12 @@ class SubmissionService {
     public function perform() {        
        
         try { 
+            //let's generate data for the contacts table
             $contact_data   = $this->contact_details->generate_data(); 
             $new_contact_id = $this->dbcontact->insert_new_contact($contact_data);
-            $feedback_data              = $this->feedback_details->generate_data();
+
+            //then we attach the contact id to the feedback object
+            $feedback_data = $this->feedback_details->generate_data();
             $feedback_data['contactId'] = $new_contact_id; 
 
             $new_feedback_id = DB::table('Feedback')->insert_get_id($feedback_data);
@@ -39,62 +42,66 @@ class SubmissionService {
                 'feedback_text' => $feedback_data['text'], 
                 'feed_id'       => $new_feedback_id
             );
-
+            
+            //we check if there is any profanity in the feedback...if so we flip the hasProfanity column to true
             $feedbackservice = new FeedbackService(new DBFeedback, new DBBadWords);
             $feedbackservice->save_feedback($post);
+            
+            //we determine the origin of the feedback and tag it as coming from the submissio form
+            DB::Table('FeedbackContactOrigin', 'master')->insert(Array(
+                'contactId'  => $new_contact_id
+              , 'feedbackId' => $new_feedback_id
+              , 'origin'     => 's36'
+              , 'socialId'   => $new_feedback_id
+            ));
+
+            //this creates metadata tag relationship between metadata and feedback
+            /*
+            if($post_metadata = $this->post_data->get('metadata')) { 
+
+                foreach($post_metadata as $data) {
+                    //this data goes into MetadataTags Table
+                    $insert_id = DB::Table('MetadataTags', 'master')->insert_get_id(Array(
+                        'tagName' => $data['name']
+                      , 'tagValue' => $data['value']
+                    ));
+
+                    //Feedback MetadataTags junction table
+                    DB::Table('FeedbackMetadataTagMap', 'master')->insert(Array(
+                         'feedbackId' => $new_feedback_id
+                       , 'tagId' => $insert_id
+                    ));
+
+                }
+
+            }   
+
+            //upon successful feedback submission let's send an email to all parties concern
+            $submission_data = new NewFeedbackSubmissionData; 
+            $feedback = $this->dbfeedback->pull_feedback_by_id($new_feedback_id);
+            $account_users = $this->dbuser->pull_user_emails_by_company_id($this->post_data->get('company_id'));
+
+            $submission_data->set_feedback($feedback)
+                            ->set_sendtoaddresses($account_users);
+
+            $emailservice = new EmailService($submission_data);
+            $emailservice->send_email();
+      
+            //let's also update the summary for the dashboard analytics
+            $this->dbdashboard->company_id = $this->post_data->get('company_id');
+            $this->dbdashboard->write_summary();
+
+            //Upon new feedback always invalidate cache       
+            $this->halcyonic->company_id = $this->post_data->get('company_id');
+            $this->halcyonic->save_latest_feedid(); 
+
+            //Why are we checking for feedbackservice object return result?
+            return ($new_feedback_id) ? DB::table('Feedback')->where('feedbackId', '=', $new_feedback_id)->first() : false;
+            */
         } catch (Exception $e) {
             die("Feedback Submission Failed!");
         }
-        /*
-        //$feedback_attachments = $this->feedback_attachments->generate_data($new_feedback_id);
-        
-        //This is for users how have used the submission form
-        DB::Table('FeedbackContactOrigin', 'master')->insert(Array(
-            'contactId' => $contact_data['contact_id']
-          , 'feedbackId' => $new_feedback_id
-          , 'origin' => 's36'
-          , 'socialId' => $new_feedback_id
-        ));
-        
-        //this creates metadata tag relationship between metadata and feedback
-        if($post_metadata = $this->post_data->get('metadata')) { 
-
-            foreach($post_metadata as $data) {
-                //this data goes into MetadataTags Table
-                $insert_id = DB::Table('MetadataTags', 'master')->insert_get_id(Array(
-                    'tagName' => $data['name']
-                  , 'tagValue' => $data['value']
-                ));
-
-                //Feedback MetadataTags junction table
-                DB::Table('FeedbackMetadataTagMap', 'master')->insert(Array(
-                     'feedbackId' => $new_feedback_id
-                   , 'tagId' => $insert_id
-                ));
-
-            }
-
-        }   
-
-        $submission_data = new NewFeedbackSubmissionData; 
-        $feedback = $this->dbfeedback->pull_feedback_by_id($new_feedback_id);
-        $account_users = $this->dbuser->pull_user_emails_by_company_id($this->post_data->get('company_id'));
-
-        $submission_data->set_feedback($feedback)
-                        ->set_sendtoaddresses($account_users);
-
-        $emailservice = new EmailService($submission_data);
-        $emailservice->send_email();
-  
-        $this->dbdashboard->company_id = $this->post_data->get('company_id');
-        $this->dbdashboard->write_summary();
-
-        //Upon new feedback always invalidate cache       
-        $this->halcyonic->company_id = $this->post_data->get('company_id');
-        $this->halcyonic->save_latest_feedid(); 
-        //Why are we checking for feedbackservice object return result?
-        return ($feedbackservice) ? DB::table('Feedback')->where('feedbackId', '=', $new_feedback_id)->first() : false;
-        */
+        //$feedback_attachments = $this->feedback_attachments->generate_data($new_feedback_id); 
     }
 
     public function metric_response() {
