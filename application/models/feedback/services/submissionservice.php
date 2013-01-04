6e7d6408591088e43e0b477ecd5feb5b88c6cd54
jamongkad <wrm932@gmail.com>
2013-01-04 10:19:14 +0800

    The Mac is Back!

--- application/models/feedback/services/submissionservice.php
+++ application/models/feedback/services/submissionservice.php
@@ -27,17 +27,22 @@ class SubmissionService {
     }
 
     public function perform() {         
+
         if($feedback_created = $this->_create_feedback()) {
+
             $company_id = $this->post_data->get('company_id');
             //this creates metadata tag relationship between metadata and feedback
             $this->_create_metadata($feedback_created->feedback_id);
             $this->_send_feedbacksubmission_email($feedback_created->feedback_obj, $this->dbuser->pull_user_emails_by_company_id($company_id));
             $this->_calculate_dashboard_analytics($company_id);
             $this->_save_latest_feedid($company_id);
+
             return $feedback_created->feedback_obj;
+
         } else {
             throw new Exception("Feedback Submission Failed!");
         }
+
     }
 
     public function _create_feedback() {
