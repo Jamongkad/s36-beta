4e344851fdda4e13213991d1745f5b943e17e573
jamongkad <wrm932@gmail.com>
2013-01-07 15:49:11 +0800

    crap

--- application/models/feedback/services/submissionservice.php
+++ application/models/feedback/services/submissionservice.php
@@ -35,9 +35,9 @@ class SubmissionService {
         if($feedback_created = $this->_create_feedback()) {
 
             $company_id = $this->post_data->get('company_id');
- 
-            $this->_create_metadata($feedback_created->feedback_id);    
+
             //this creates metadata tag relationship between metadata and feedback 
+            $this->_create_metadata($feedback_created->feedback_id);    
             //$this->_send_feedbacksubmission_email($feedback_created->feedback_obj, $this->dbuser->pull_user_emails_by_company_id($company_id));
             //$this->_calculate_dashboard_analytics($company_id);
             //$this->_save_latest_feedid($company_id);
