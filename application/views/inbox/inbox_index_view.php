8f2007f53270c1272b1edfb3e7584c0a4648f358
jamongkad <wrm932@gmail.com>
2013-01-08 19:21:54 +0800

    wq

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -285,6 +285,7 @@
                                             <div class="custom-meta-list grids">
                                             <? 
                                             //start metadata 
+                                            Helpers::dump($metadata);
                                             if($metadata):
                                                 foreach($metadata as $key => $val):?>
                                                     <div class="custom-meta">
