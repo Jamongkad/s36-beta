a37510b59fd4e2728d309f90745f2e0d5acb9ab1
Robert <robert.mordido@microsourcing.ph>
2013-01-09 18:27:04 +0800

    wq

--- application/routes/inbox.php
+++ application/routes/inbox.php
@@ -28,7 +28,6 @@ return array(
         $inbox->ignore_cache = True;
         $feedback = $inbox->present_feedback();
 
-        Helpers::dump($feedback);
 
         $admin_check = S36Auth::user();  
         $user_id = S36Auth::user()->userid;
