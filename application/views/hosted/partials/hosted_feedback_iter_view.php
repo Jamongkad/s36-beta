ae2c554028971673c365ecb0026e752554ea1bd0
jamongkad <wrm932@gmail.com>
2013-01-10 20:38:10 +0800

    deleted iter_view && unit_view; added new partial

--- application/views/hosted/partials/hosted_feedback_iter_view.php
+++
@@ -1,9 +0,0 @@
-<?if($collection->head):?>
-    <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $collection->head, 'type' => 'featured'))?>
-<?endif?>
-
-<?if($collection->children):?>
-    <?foreach($collection->children as $child):?> 
-        <?=View::make('hosted/partials/hosted_feedback_unit_view', Array('feed' => $child, 'type' => 'normal'))?>
-    <?endforeach?>
-<?endif?>
