daa0b4fe29b42afa9bf6cc6b5ab03780f2196a09
jamongkad <wrm932@gmail.com>
2013-01-14 16:19:49 +0800

    wq

--- application/routes/feedsetup.php
+++ application/routes/feedsetup.php
@@ -145,8 +145,10 @@ return array(
 
         $hosted->set_hosted_settings(Array('companyId'  =>  Input::get('companyId')));
         $hosted_settings = $hosted->hosted_settings();
+        $hosted_background = Input::all('hosted_background');
         Helpers::dump(Input::all());
-        Helpers::dump(Input::all('hosted_background') == True);
+
+        Helpers::dump($hosted_background['name'] == True);
         /*
         $input['background_image'] = $hosted_settings->background_image;
         if(isset($_FILES['hosted_background']) && !empty($_FILES['hosted_background']['name'])){
