4a26775ca20ab1c6d9d04bd60d4cf4f6df19b191
jamongkad <wrm932@gmail.com>
2013-01-15 19:36:08 +0800

    dead rising

--- application/models/widget/repositories/dbwidget.php
+++ application/models/widget/repositories/dbwidget.php
@@ -224,7 +224,8 @@ class DBWidget extends S36DataObject {
                     ON Site.companyId = WidgetStore.companyId
             LEFT JOIN
                 WidgetFormMetadata
-                    ON (WidgetFormMetadata.companyId = WidgetStore.companyId) OR (WidgetFormMetadata.widgetStoreId = WidgetStore.widgetStoreId)
+                    ON WidgetFormMetadata.widgetStoreId = WidgetStore.widgetStoreId 
+                    /*(WidgetFormMetadata.companyId = WidgetStore.companyId) OR (WidgetFormMetadata.widgetStoreId = WidgetStore.widgetStoreId)*/
             WHERE 1=1
                 $statement
         ";
