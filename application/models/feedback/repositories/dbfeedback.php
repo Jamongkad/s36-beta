9bcaa21b88acf7407e2d9e911bc20797cdc02c49
jamongkad <wrm932@gmail.com>
2013-01-17 20:36:03 +0800

    wq

--- application/models/feedback/repositories/dbfeedback.php
+++ application/models/feedback/repositories/dbfeedback.php
@@ -489,7 +489,7 @@ class DBFeedback extends S36DataObject {
                 , (SELECT COUNT(useful) FROM FeedbackActions WHERE Feedback.feedbackId = FeedbackActions.feedbackId) AS vote_count
                 , FeedbackActions.useful
                 , FeedbackActions.flagged
-                , (SELECT * FROM User WHERE User.userId = FeedbackAdminReply.userId) AS user 
+                , FeedbackAdminReply.userId
             FROM 
                 Feedback
             LEFT JOIN
