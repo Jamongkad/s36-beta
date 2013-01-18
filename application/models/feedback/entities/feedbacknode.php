ee3709c68a22beec4c8d50f56cd967722f146745
jamongkad <wrm932@gmail.com>
2013-01-18 11:51:43 +0800

    wq

--- application/models/feedback/entities/feedbacknode.php
+++ application/models/feedback/entities/feedbacknode.php
@@ -1,11 +1,83 @@
 <?php namespace Feedback\Entities;
 
+use StdClass;
+
 class FeedbackNode {
 
     private $data;
 
     public function __construct($data) {
         $this->data = $data;     
+    }
+
+    public function generate() {
+        $node = new StdClass; 
+        foreach($this->data as $key => $value) {
+            print_r($key);
+        }
+        /*
+        $node->id          = $data->id;      
+        $node->firstname   = $data->firstname;
+        $node->lastname    = $data->lastname;
+        $node->logintype   = $data->logintype;
+        $node->countryname = $data->countryname;
+        $node->countrycode = $data->countrycode;
+        $node->profilelink = $data->profilelink;
+        $node->date        = $data->date;
+        $node->head_date_format = $data->head_date_format;
+        $node->status      = $data->status;
+        $node->text        = $data->text;
+        $node->attachments = $data->attachments;
+        $node->categoryid  = $data->categoryid;  
+        $node->category    = $data->category;  
+        $node->priority    = $data->priority;  
+        $node->rating      = $data->rating;
+        $node->ispublished = $data->ispublished;
+        $node->isdeleted   = $data->isdeleted;
+        $node->isfeatured  = $data->isfeatured;
+        $node->permission_css = $data->permission_css;
+        $node->permission = $data->permission;
+        $node->contactid  = $data->contactid;
+        $node->siteid     = $data->siteid;
+        $node->perm_val   = $data->perm_val;
+        $node->email      = $data->email;
+        $node->unix_timestamp = $data->unix_timestamp;
+        $node->daysago    = $data->daysago;
+        $node->sitedomain = $data->sitedomain;
+        $node->avatar     = $data->avatar;
+        $node->origin     = $data->origin;
+        $node->socialid   = $data->socialid;
+        
+        $node->admin_reply    = $data->admin_reply;
+        $node->admin_userid   = $data->admin_userid;
+        $node->admin_username = $data->admin_username;
+        $node->admin_fullname = $data->admin_fullname;
+        $node->admin_email    = $data->admin_email;
+        $node->admin_companyname = $data->admin_companyname;
+        $node->admin_fullpagecompanyname = $data->admin_fullpagecompanyname;
+
+        if( property_exists($data, 'attachments') ) $node->attachments = json_encode($node->attachments);
+        if( property_exists($data, 'metadata') ) $node->metadata = json_encode($node->metadata);
+        if( property_exists($data, 'flagged') ) $node->flagged = $data->flagged;
+        if( property_exists($data, 'useful') ) $node->useful = $data->useful;
+        if( property_exists($data, 'vote_count') ) $node->vote_count = $data->vote_count;
+        
+        /*
+        $attachments = Null;
+        if(!empty($data->attachments)) {
+            $attachments = json_decode($data->attachments);
+        }
+
+        $metadata = Null;
+        if(!empty($data->metadata)) {
+            $metadata = json_decode($data->metadata);
+        }
+
+        $node->attachments = $attachments;
+        $node->metadata    = $metadata;
+        */ 
+        //return $node;
+
     }
 
     
