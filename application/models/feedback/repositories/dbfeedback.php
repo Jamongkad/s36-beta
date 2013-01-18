4a1b6f757c49e9ff07175dab04fd2bfb46ff8242
jamongkad <wrm932@gmail.com>
2013-01-18 18:39:48 +0800

    fuuuu

--- application/models/feedback/repositories/dbfeedback.php
+++ application/models/feedback/repositories/dbfeedback.php
@@ -486,7 +486,7 @@ class DBFeedback extends S36DataObject {
         $sql = ' 
             SELECT
                 '.$this->select_vars.' 
-                , (SELECT COUNT(useful) FROM FeedbackActions WHERE Feedback.feedbackId = FeedbackActions.feedbackId) AS vote_count
+                /*, (SELECT COUNT(useful) FROM FeedbackActions WHERE Feedback.feedbackId = FeedbackActions.feedbackId) AS vote_count*/
                 , FeedbackActions.useful 
                 , FeedbackActions.flagged AS flagged_as_inappr
                 , FeedbackAdminReply.userId AS admin_userid
