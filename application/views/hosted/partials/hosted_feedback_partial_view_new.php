e9d8b2d75fd9dcb6e2e102ddb45b9ccdf6c95b09
jamongkad <wrm932@gmail.com>
2013-01-21 15:42:40 +0800

    wq

--- application/views/hosted/partials/hosted_feedback_partial_view_new.php
+++ application/views/hosted/partials/hosted_feedback_partial_view_new.php
@@ -132,7 +132,6 @@ foreach ($collection as $feed_group => $feed_list) :
                 <?
                 $companyname = (isset($user->fullpagecompanyname)) ? $user->fullpagecompanyname : Null;
                 $admin_avatar = (isset($feed->feed_data->admin_avatar)) ? $feed->feed_data->admin_avatar : '/img/48x48-blank-avatar.jpg';
-                print_r(isset($feed->feed_data->admin_avatar));
                 print_r($feed->feed_data->admin_avatar);
                 $admin_companyname = (isset($feed->feed_data->admin_fullpagecompanyname)) ? $feed->feed_data->admin_fullpagecompanyname : $feed->feed_data->admin_companyname;
                 ?>
