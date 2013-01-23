5538a8b1d631b2ca123dbff2fbb70b9bab2ead27
jamongkad <wrm932@gmail.com>
2013-01-23 10:45:59 +0800

    removing debug output

--- application/routes.php
+++ application/routes.php
@@ -31,9 +31,7 @@ return array(
     'GET /' => function() use($company_name, $hosted_settings, $company, $user, $feedback, $company_social, $hosted_page_url) {
         //consider placing this into a View Object
         $company_info = $company->get_company_info($company_name);
-
-        Helpers::dump($company_info);
-        
+ 
         //Feeds
         $hosted = new Feedback\Services\HostedService($company_name); 
         $hosted->page_number = 1; 
