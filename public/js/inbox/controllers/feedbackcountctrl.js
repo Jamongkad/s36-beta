86123078d47e030c2d5848a5f9be337dc6f72459
jamongkad <wrm932@gmail.com>
2013-01-24 18:17:59 +0800

    wq

--- public/js/inbox/controllers/feedbackcountctrl.js
+++ public/js/inbox/controllers/feedbackcountctrl.js
@@ -4,13 +4,16 @@ function FeedbackCountCtrl($scope, FeedbackService) {
     $scope.counts = FeedbackService.feedback_count;
 
     $scope.get_feedback_count = function() {
+        console.log($scope.counts);
         return $scope.counts;     
     }
 
     //Broadcast Messages
+    /*
     $scope.$on('requestFeedbackCount', function()  {
         $scope.$apply(function() {
             $scope.counts = FeedbackService.feedback_count;
         })
     });
+    */
 }
