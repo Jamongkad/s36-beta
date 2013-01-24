e490bf2e4bc32db3174e75aedef4bc95506203e5
jamongkad <wrm932@gmail.com>
2013-01-24 18:11:04 +0800

    eq

--- public/js/inbox/services/SettingsService.js
+++ public/js/inbox/services/SettingsService.js
@@ -68,4 +68,27 @@ angular.module('Services', [])
     }
     
     return shared_service;
+})
+.service('FeedbackService', function($rootScope) {
+
+    var shared_service = {};
+    shared_service.feedback_count;
+
+    shared_service.get_feedback_count = function() { 
+        $.ajax({
+            type: 'GET'    
+          , dataType: 'json'
+          , async: false
+          , url: '/feedback/get_feedback_count'
+          , success: function(data) {
+                shared_service.feedback_count = data;
+            }
+        });
+    }
+
+    shared_service.register_feedback_count = function()  {
+        $rootScope.$broadcast('requestFeedbackCount');
+    }
+
+    return shared_service;
 });
