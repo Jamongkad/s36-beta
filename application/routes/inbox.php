20fa84e3240c4420ff25a0a0cc5b376c3dd055aa
jamongkad <wrm932@gmail.com>
2013-01-28 18:45:14 +0800

    wq

--- application/routes/inbox.php
+++ application/routes/inbox.php
@@ -30,10 +30,9 @@ return array(
         
 
         $admin_check = S36Auth::user();  
-        $user_id = S36Auth::user()->userid;
+        //$user_id = S36Auth::user()->userid;
         $company_id = S36Auth::user()->companyid;
-        $redis->hset("user:$user_id:$company_id", "feedid_checked", 1);
-
+        //$redis->hset("user:$user_id:$company_id", "feedid_checked", 1);
         
         //Resets UI code for clicky action function
         reset_inbox_ui($company_id, $redis);
