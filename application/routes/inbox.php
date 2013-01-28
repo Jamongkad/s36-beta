1b03a34b5b4d9183009bf9c77fb2d96a25703c96
jamongkad <wrm932@gmail.com>
2013-01-28 18:32:46 +0800

    wq

--- application/routes/inbox.php
+++ application/routes/inbox.php
@@ -27,11 +27,13 @@ return array(
         $inbox->set_filters($filters);
         $inbox->ignore_cache = True;
         $feedback = $inbox->present_feedback();
-
+        
+        /*
         $admin_check = S36Auth::user();  
         $user_id = S36Auth::user()->userid;
         $company_id = S36Auth::user()->companyid;
         $redis->hset("user:$user_id:$company_id", "feedid_checked", 1);
+        */
         
         //Resets UI code for clicky action function
         reset_inbox_ui($company_id, $redis);
