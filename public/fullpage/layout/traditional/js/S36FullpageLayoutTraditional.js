4c7a9790af7d623d4c891a6321ca416123c740d9
Robert <robert.mordido@microsourcing.ph>
2013-01-31 17:44:31 +0800

    updated fullpage assets

---
+++ public/fullpage/layout/traditional/js/S36FullpageLayoutTraditional.js
@@ -0,0 +1,15 @@
+/*=========================================
+||
+|| JS File for the Traditional Layout
+||
+||=========================================*/
+
+var S36FullpageLayoutTraditional = function(){
+	/* ========================================
+	|| Function needed to run by document ready
+	==========================================*/
+	this.init_fullpage_layout = function(){	
+		/* nothing to initialize here! */
+	}
+	this.layout_name = "traditional";
+}
