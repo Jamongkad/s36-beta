353431d01bdec48bec78dbcf7e4b165d826552d7
jamongkad <wrm932@gmail.com>
2013-01-31 18:38:25 +0800

    introspect me

--- application/models/message/services/inboxmessage.php
+++ application/models/message/services/inboxmessage.php
@@ -1,7 +1,10 @@
 <?php namespace Message\Services;
 
+use redisent\Redis;
 use Exception;
 
 class InboxMessage {
-    
+    public function __construct() {
+        $this->redis = new Redis; 
+    }
 }
