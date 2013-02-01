7366e0066b6cb9bf85549383aa9ecfe8dfb608c5
jamongkad <wrm932@gmail.com>
2013-02-01 18:47:53 +0800

    wq

--- application/models/message/services/messagedirector.php
+++ application/models/message/services/messagedirector.php
@@ -1,16 +1,21 @@
 <?php namespace Message\Services;
 
 use Message\Repositories\UserDirectory;
+use redisent\Redis;
 use Helpers;
 
 class MessageDirector {
     
     public function __construct() {
         $this->directory = new UserDirectory;
+        $this->redis     = new Redis;    
     }
 
     public function send_message($message) {
         $user_dir = $this->directory->fetch_users();
-        Helpers::dump($user_dir);
+        
+        foreach($user_dir as $user) {
+            Helpers::dump($user);
+        }
     }
 }
