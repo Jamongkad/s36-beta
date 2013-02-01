fc51b7016344d8862be6c8d33bb1156b845e6d3d
jamongkad <wrm932@gmail.com>
2013-02-01 18:55:53 +0800

    wq

--- application/models/message/services/messagedirector.php
+++ application/models/message/services/messagedirector.php
@@ -13,6 +13,8 @@ class MessageDirector {
 
     public function send_message($message) {
         $user_dir = $this->directory->fetch_users();
+
+        Helpers::dump(get_class($message));
         
         foreach($user_dir as $user) {
             $this->redis->hset($user->user_id, "admin:inbox", $message->get_message());
