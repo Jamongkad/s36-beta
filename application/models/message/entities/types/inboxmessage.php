0a4176f2685511f70689af874dffced0aebe8342
jamongkad <wrm932@gmail.com>
2013-02-04 19:00:29 +0800

    wq

--- application/models/message/entities/types/inboxmessage.php
+++ application/models/message/entities/types/inboxmessage.php
@@ -4,7 +4,7 @@ use Exception;
 
 class InboxMessage extends Message {
 
-    private $redis_read_key = "admin:inbox:notification";
+    protected $redis_read_key = "admin:inbox:notification";
 
     public function __construct($message) {
         parent::__construct($message);
