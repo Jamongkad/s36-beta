8ac316a0de0e08e8561003fa91efef2af7e994c9
jamongkad <wrm932@gmail.com>
2013-02-06 18:42:28 +0800

    wq

--- public/js/inbox/directives/myfeedbackcount.js
+++ public/js/inbox/directives/myfeedbackcount.js
@@ -2,18 +2,18 @@ angular.module('feedback', [])
 .directive('myFeedbackcount', function(FeedbackService) {
     return {
         restrict: 'A'     
-      , template: "<b>Momoji</b>"
-      /*
       , link: function(scope, element, attrs) {
-
+            $(element).bind('mouseover', function(e) {
+                console.log("Pwet"); 
+            });
+            /*
             FeedbackService.get_feedback_count();
             var feedback = FeedbackService.feedback_count;
 
             if(feedback.checked) {
                 $(element).html("<sup class='count'>" + feedback.feedback_count + "</sup>");     
             }
-           
+            */ 
         }
-        */
     }    
 })
