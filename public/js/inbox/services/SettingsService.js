d1b8d44ebf5d1afdc869901600e6083e1e471363
jamongkad <wrm932@gmail.com>
2013-02-07 18:45:13 +0800

    wq

--- public/js/inbox/services/SettingsService.js
+++ public/js/inbox/services/SettingsService.js
@@ -87,7 +87,7 @@ angular.module('Services', [])
         });
     }
 
-    shared_service.set_inbox_as_read(pathname) {
+    shared_service.set_inbox_as_read = function(pathname) {
         /*
         $.ajax({
             type: 'GET'    
