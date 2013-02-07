63ec263c2e0abfa7fc7249e6fbc43acbf1b8accb
jamongkad <wrm932@gmail.com>
2013-02-07 17:10:26 +0800

    blank

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -388,7 +388,7 @@ return array(
 
             $auth = S36Auth::user();
             $inbox = new Message\Entities\UserInbox("{$auth->username}:messages");
-            $inbox->edit("inbox:notification:newfeedback", "Mathew rocks");
+            $inbox->edit("inbox:notification:newfeedback", "");
             Helpers::dump($inbox->read_all());
             Helpers::dump($inbox->read("inbox:notification:newfeedback"));
         });
