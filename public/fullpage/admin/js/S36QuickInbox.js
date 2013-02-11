9c581fa8cccf03dce4054bdc7aee7032819476dd
jamongkad <wrm932@gmail.com>
2013-02-11 17:49:34 +0800

    MAKING IMPROVEMEnts

--- public/fullpage/admin/js/S36QuickInbox.js
+++ public/fullpage/admin/js/S36QuickInbox.js
@@ -3,14 +3,20 @@ var app = angular.module("QuickInbox", []);
 app.controller("AppCtrl", function($scope) {
     
     var feedback = [
-        {"feedid": 285, "text": "Mathew is Kewl!", "media": [  
-            {"media_id": 280, "text": "Video + (girl, girl)"}
-          , {"media_id": 288, "text": "Video + (boy, boy)"}
-        ]} 
-      , {"feedid": 286, "text": "Mathew is Hot!", "media": [
-            {"media_id": 280, "text": "Video + (girl)"}
-          , {"media_id": 288, "text": "Video + (boy)"}
-        ]} 
+        {   "feedid": 285
+          , "text": "Mathew is Kewl!"
+          , "media": [  
+                { "media_id": 280, "text": "Video + (girl, girl)" }
+              , { "media_id": 288, "text": "Video + (boy, boy)" }
+            ]
+        } 
+      , {  "feedid": 286
+         , "text": "Mathew is Hot!"
+         , "media": [
+                { "media_id": 280, "text": "Video + (girl)" }
+              , { "media_id": 288, "text": "Video + (boy)" }
+            ]
+        } 
     ];
 
     this.fetch_inbox_feeds = function() {
