15c3f4cfac8d53448e07354e7d5f78c46966c02b
jamongkad <wrm932@gmail.com>
2013-02-12 15:56:23 +0800

    wq

--- application/routes/hosted.php
+++ application/routes/hosted.php
@@ -30,6 +30,7 @@ return array(
          ,  Array('id' => 3, 'name' => 'Meagan Wong', 'text' => 'HEY I LOVE ICECREAM :))') 
          ,  Array('id' => 4, 'name' => 'Robert Mordido', 'text' => 'DOTA 2 Rocks.') 
          ,  Array('id' => 5, 'name' => 'Leica Chang', 'text' => 'I am a cute futtie.') 
+         ,  Array('id' => 7, 'name' => 'Derps Matusoc', 'text' => 'Hi am a derp!') 
         );
     
         echo json_encode($data);
