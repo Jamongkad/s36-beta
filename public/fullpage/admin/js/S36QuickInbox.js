1b5fc18cbcca3e18f8688519b7ec2663ff5429f2
jamongkad <wrm932@gmail.com>
2013-02-12 20:48:02 +0800

    wq

--- public/fullpage/admin/js/S36QuickInbox.js
+++ public/fullpage/admin/js/S36QuickInbox.js
@@ -32,7 +32,7 @@ app.controller("AppCtrl", function($scope, $http, $timeout, QuickInboxService) {
 
     $scope.metadata_block = function(data) {
         if(data.hasOwnProperty('metadata')) { 
-            for(var i=0; i<data.length; i++) {
+            for(var i=0; i<data['metadata'].length; i++) {
                 console.log(data[i].key);
                 console.log(data[i].value);
             }
