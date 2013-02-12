7cb0f9a171f5a789d23584f49d1b85d591187ab5
jamongkad <wrm932@gmail.com>
2013-02-12 17:58:40 +0800

    wq

--- public/fullpage/admin/js/S36QuickInbox.js
+++ public/fullpage/admin/js/S36QuickInbox.js
@@ -5,7 +5,7 @@ app.controller("AppCtrl", function($scope, $http, $timeout, QuickInboxService) {
     $scope.feedbacks = [];
 
     (function feed_request() { 
-
+        /*
         $.ajax({
             type: 'GET'    
           , dataType: 'json'
@@ -17,14 +17,14 @@ app.controller("AppCtrl", function($scope, $http, $timeout, QuickInboxService) {
                 setTimeout(function() { feed_request(); }, 5000);
             }
         });
+        */
 
-
-        /*
         $http.get('/hosted/quick_inbox').success(function(data) {
             $scope.feedbacks = data;
+            $scope.$apply($scope.feedbacks);
             $timeout(feed_request, 5000);
         })
-        */
+
     })();
     /*
     var feedback = [
