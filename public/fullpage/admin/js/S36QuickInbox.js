d6ba35d862e76147e302e6b66da919ec76d58768
jamongkad <wrm932@gmail.com>
2013-02-14 17:28:02 +0800

    wq

--- public/fullpage/admin/js/S36QuickInbox.js
+++ public/fullpage/admin/js/S36QuickInbox.js
@@ -1,4 +1,4 @@
-var app = angular.module("QuickInbox", ['S36QuickInboxDirectives', 'S36QuickInboxServices', 'ngSanitize']);
+var app = angular.module("QuickInbox", ['S36QuickInboxDirectives', 'S36QuickInboxServices']);
 
 app.controller("AppCtrl", function($scope, $compile, QuickInboxService) {
 
