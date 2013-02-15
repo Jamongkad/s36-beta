f05bab0b81d69f6e3b452f0743db2ccbf559d30c
jamongkad <wrm932@gmail.com>
2013-02-15 16:31:30 +0800

    wq

--- public/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js
+++ public/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js
@@ -5,6 +5,15 @@ angular.module('S36QuickInboxServices', [])
 
     shared_service.change_feedback_state = function(id, state) {
         console.log("Changing State: " + state + " Changing Id:" + id);
+         
+        $.ajax({
+            type: 'POST'    
+          , data: {'state': state, 'feedid': id}
+          , url: '/hosted/change_feedback_state'
+          , success: function(data) {  
+                console.log(data);
+            }
+        });
     }
  
     return shared_service;
