e0bd8c6584e78566bc577206844a2ad9b3738d68
jamongkad <wrm932@gmail.com>
2013-02-15 15:27:19 +0800

    mamitas

--- public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
+++ public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
@@ -7,7 +7,7 @@ angular.module('S36QuickInboxDirectives', [])
                 console.log(scope.published);
                 var me = this;
                 $(me).parents('.widget-item').children().fadeOut(400, function() { 
-                    $(me).parents('.widget-item').fadeIn().html($compile('<span punch>Undo?</span>')(scope));
+                    $(me).parents('.widget-item').fadeIn().html($compile('<span undo>Undo?</span>')(scope));
                     $('.widget-list').jScrollPane();
                 });
 
@@ -45,6 +45,15 @@ angular.module('S36QuickInboxDirectives', [])
                 console.log("PUNCH");
             })
         }
-    }     
-    
+    }         
+})
+.directive('undo', function() {
+    return {
+        restrict: 'A'     
+      , link: function(scope, element, attrs) {
+            $(element).bind('click', function(e) {  
+                console.log($(this).parents('.widget-item').children().fadeIn());
+            })
+        }
+    }          
 })
