ac15c6386d6d91a8ab43d8ff28b495dd8703efa2
jamongkad <wrm932@gmail.com>
2013-02-18 18:52:05 +0800

    wq

--- application/views/hosted/partials/fullpage_admin_panel_view.php
+++ application/views/hosted/partials/fullpage_admin_panel_view.php
@@ -32,7 +32,7 @@
                                         <div class="widget-content">
                                             <div class="widget-submitter">
                                                 <span class="name">{{feeds.firstname}} {{feeds.lastname}}</span> 
-                                                <span class="social-src" social login="{{feeds.logintype}}">
+                                                <span class="social-src">
                                                    {{feeds.logintype}}
                                                 </span>
                                             </div>
