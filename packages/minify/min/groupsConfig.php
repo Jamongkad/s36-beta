dad5cb0927ad203795aaf15d5fca41615aaf4ca0
Robert <robert.mordido@microsourcing.ph>
2013-02-18 11:15:43 +0800

    added new package --minify

---
+++ packages/minify/min/groupsConfig.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+     'Global'	=> array(
+     			 '//js/jquery-ui-1.8.24.custom.min.js'
+     			,'//js/helpers.js'
+     			,'//js/jquery.iframe-transport.js'
+     			,'//js/jquery.ui.widget.js'
+     			,'//js/jquery.fileupload.js'
+     			,'//js/jquery.raty.min.js'
+     ),
+     'FullpageCommon' => array(
+     			 '//fullpage/common/css/S36FullpageCommon.css'
+     			,'//fullpage/common/css/master.css'
+     			,'//fullpage/common/css/flags.css'
+     			,'//fullpage/common/css/grids.css'
+     			,'//fullpage/common/css/s36_client_style.css'
+     			,'//fullpage/common/js/S36FullpageCommon.js'
+     			,'//fullpage/common/js/masonry.js'
+     			,'//fullpage/common/js/modernizr.js'
+     			,'//fullpage/common/js/feedbackactions.js'
+     			,'//fullpage/common/js/s36_client_script.js'
+     ),
+     'FullpageAdmin' => array(
+				 '//fullpage/admin/css/S36FullpageAdmin.css'
+				,'//fullpage/admin/css/jcarousel.skin.css'
+				,'//fullpage/admin/css/minicolors.css'
+				,'//fullpage/admin/js/jcycle.js'
+				,'//fullpage/admin/js/jquery.jcarousel.min.js'
+				,'//fullpage/admin/js/minicolors.js'
+				,'//fullpage/admin/js/colors.min.js'
+				,'//fullpage/admin/js/jquery.mousewheel.min.js'
+				,'//fullpage/admin/js/jquery.scroll.js'
+				,'//fullpage/admin/js/S36FullpageAdmin.js'
+     ),
+);
+
+
+
+
+
+
+
