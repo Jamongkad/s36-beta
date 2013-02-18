13029046843dae7d7dfb16b2f6ab5f7dae07ccec
jamongkad <wrm932@gmail.com>
2013-02-18 20:02:18 +0800

    wq

--- public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
+++ public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
@@ -72,7 +72,7 @@ angular.module('S36QuickInboxDirectives', [])
       , link: function(scope, element, attrs) {
             attrs.$observe('date', function(nv) {
                 var d = Date.parse(nv);
-                console.log(d);
+                console.log(d.toString('MMMM M, yyyy'));
             }) 
         }
       //, template: '<spa ng-bind-html-unsafe="socialsrc"></span>'
