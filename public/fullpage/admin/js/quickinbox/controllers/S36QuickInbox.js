9819eac1a7eab64c4101b6da8821d623dc92d93a
jamongkad <wrm932@gmail.com>
2013-02-19 19:49:11 +0800

    adding console messages testing cache

--- public/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js
+++ public/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js
@@ -29,12 +29,12 @@ app.controller("AppCtrl", function($scope, $compile, QuickInboxService) {
 
         $('#quickInbox').unbind('mouseenter.widget').bind('mouseenter.widget', function() { 
             timer.pause();
-            console.log("Stopping");
+            console.log("Stopping Poll");
         });
 
         $('#quickInbox').unbind('mouseleave.widget').bind('mouseleave.widget', function() { 
             timer.resume();
-            console.log("Starting");
+            console.log("Starting Poll");
         });
     })();
 
