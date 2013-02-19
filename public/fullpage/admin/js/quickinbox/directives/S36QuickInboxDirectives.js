cdad1b1d020332a4626847fd51b82bdebf30b621
jamongkad <wrm932@gmail.com>
2013-02-19 11:19:43 +0800

    shpaing

--- public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
+++ public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
@@ -119,6 +119,7 @@ angular.module('S36QuickInboxDirectives', [])
             attrs.$observe('load', function(at) {
                 if(at) {
                     var data = angular.fromJson(at);
+                    console.log(data);
                     scope.mtemplate = '<div class="uploaded-images-and-links grids">';
                     for(var prop in data) {
                         var links = data[prop];
