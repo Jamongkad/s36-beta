a35c4af227d415fa7fc6b20cc68ae287ed933c35
jamongkad <wrm932@gmail.com>
2013-02-19 20:11:56 +0800

    wq

--- application/views/hosted/partials/fullpage_timeline_layout_view.php
+++ application/views/hosted/partials/fullpage_timeline_layout_view.php
@@ -1,5 +1,5 @@
 <?php if($collection): ?>
-<div id="timelineLayout">
+<div id="timelineLayout" class="timeline">
 <?php foreach ($collection as $feed_group => $feed_list) : ?>
 <div class="feedback-block">
     <div class="feedback-spine"></div>                
