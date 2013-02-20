eb3b978e47da955b456ac10bbc1a1109a3f2b7b9
jamongkad <wrm932@gmail.com>
2013-02-20 17:46:04 +0800

    wq

--- application/models/feedback/repositories/dbfeedback.php
+++ application/models/feedback/repositories/dbfeedback.php
@@ -588,6 +588,8 @@ class DBFeedback extends S36DataObject {
         $row_count = $this->dbh->query("SELECT FOUND_ROWS()");
         $result = $sth->fetchAll(PDO::FETCH_CLASS);
 
+        Helpers::dump($result);
+
         $result_obj = new StdClass;
         $result_obj->result = $this->_return_feedback_nodes($result);
         $result_obj->total_rows = $row_count->fetchColumn();
