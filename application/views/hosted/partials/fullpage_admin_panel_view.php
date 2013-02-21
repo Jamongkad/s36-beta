967f73176226f28edc99462e8d83508144a3e8d4
jamongkad <wrm932@gmail.com>
2013-02-21 13:56:17 +0800

    wq

--- application/views/hosted/partials/fullpage_admin_panel_view.php
+++ application/views/hosted/partials/fullpage_admin_panel_view.php
@@ -27,6 +27,9 @@
                             <div class="widget-list" ng-app="QuickInbox">
                                 <div ng-controller="AppCtrl">
                                     <div class="widget-item clear" ng-repeat="feeds in feedbacks">                                     
+                                        <div>
+                                             <input type="checkbox" name="feedid" value"{{feeds.id}}" />
+                                        </div>
                                         <div class="widget-avatar">
                                             <img src="/uploaded_images/avatar/48x48/{{feeds.avatar}}" />
                                         </div>
