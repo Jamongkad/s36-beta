20793a04a97e70a3b5bc270b56719b706ab24258
jamongkad <wrm932@gmail.com>
2013-02-22 15:46:55 +0800

    wq

--- public/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js
+++ public/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js
@@ -25,12 +25,12 @@ angular.module('S36QuickInboxServices', [])
                 if(theme == 'treble') { 
                     var fullpageLayout = new S36FullpageLayoutTreble;
                 }
-                /*
+
                 fullpageLayout.init_fullpage_layout(); // initialize document ready of the current layout javascripts
                 fullpageCommon.init_fullpage_common(); // initialize document ready of the common javascript
 
                 S36FeedbackActions.initialize_actions(fullpageLayout);
-                */
+
                 $("#feedbackContainer").html(data.view);
             }
         });
