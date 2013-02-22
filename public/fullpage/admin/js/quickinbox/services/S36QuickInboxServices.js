72ed135b20c851ce0b7e118e64d9d6e2f69c7235
jamongkad <wrm932@gmail.com>
2013-02-22 16:05:40 +0800

    wq

--- public/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js
+++ public/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js
@@ -18,26 +18,22 @@ angular.module('S36QuickInboxServices', [])
                 console.log(fullpageCommon);
 
                 if(theme == 'timeline') { 
-                    var fullpageLayout = new S36FullpageLayoutTimeline;
+                    fullpageLayout = new S36FullpageLayoutTimeline;
                     console.log("Timeline");
-                    console.log(fullpageLayout);
                 }
 
                 if(theme == 'traditional') { 
-                    var fullpageLayout = new S36FullpageLayoutTraditional;
+                    fullpageLayout = new S36FullpageLayoutTraditional;
                     console.log("Traditional");
-                    console.log(fullpageLayout);
                 }
 
                 if(theme == 'treble') { 
-                    var fullpageLayout = new S36FullpageLayoutTreble;
+                    fullpageLayout = new S36FullpageLayoutTreble;
                     console.log("Treble");
-                    console.log(fullpageLayout);
                 }
 
                 console.log(fullpageLayout.init_fullpage_layout()); // initialize document ready of the current layout javascripts
                 console.log(fullpageCommon.init_fullpage_common()); // initialize document ready of the common javascript
-
                 console.log(S36FeedbackActions.initialize_actions(fullpageLayout));
                 $("#feedbackContainer").html(data.view);
             }
