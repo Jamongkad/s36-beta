9b8aac5599dbfdaf8fea4273806d1e474965d055
jamongkad <wrm932@gmail.com>
2013-02-22 18:42:37 +0800

    wq

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -177,6 +177,9 @@
                     }
                 });
             }
+
+            fullpageLayout.init_fullpage_layout(); // initialize document ready of the current layout javascripts
+            fullpageCommon.init_fullpage_common(); // initialize document ready of the common javascript
             S36FeedbackActions.initialize_actions(fullpageLayout);
         }
         //rate limit this bitch
