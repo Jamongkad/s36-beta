c363946ea39cd0a78dbde432fdc811d1d170c77b
kennwel <kennwel.labarda@microsourcing.ph>
2013-02-26 10:49:27 +0800

    fixed fullpage layout initialization bug.

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -179,7 +179,7 @@
                 });
             }
 
-            fullpageLayout.init_fullpage_layout(); // initialize document ready of the current layout javascripts
+            fullpageLayout.init_fullpage_layout(fullpageCommon); // initialize document ready of the current layout javascripts
             fullpageCommon.init_fullpage_common(); // initialize document ready of the common javascript
             S36FeedbackActions.initialize_actions(fullpageLayout);
         }
