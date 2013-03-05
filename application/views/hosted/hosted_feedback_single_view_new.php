e3177e5813a248df8f9f621ad8e2021eb5fe4d62
jamongkad <wrm932@gmail.com>
2013-03-05 12:42:13 +0800

    wq

--- application/views/hosted/hosted_feedback_single_view_new.php
+++ application/views/hosted/hosted_feedback_single_view_new.php
@@ -155,7 +155,20 @@
                 </div>
             </div>
             <!-- end of lightbox container -->
-            
+            <?php
+
+            $admin_avatar               = ($feedback->admin_avatar) ? $feedback->admin_avatar : '/img/48x48-blank-avatar.jpg';
+            $feedback_id                = $feedback->id;
+            $tw_marker                  = ($feedback->origin=='tw') ? '<div class="twitter-marker"></div>' : '';
+            $avatar                     = Config::get('application.avatar48_dir').'/'.$feedback->avatar;
+            $attachments                = (!empty($feedback->attachments)) ? $feedback->attachments : false;
+            $vote_count                 = $feedback->vote_count;
+            $voted                      = $feedback->useful;
+            $flagged                    = $feedback->flagged_as_inappr;
+            $metadata                   = $feedback->metadata;
+            $is_recommended             = $feedback->isrecommended;
+
+            ?>
             <div id="feedbackContainer">
             	<!-- this is where the magic begins -->
                 <div id="threeColumnLayout"> 
@@ -166,25 +179,46 @@
                                 <!-- feedback header -->
                                 <div class="feedback-header clear">
                                     <div class="author">
-                                        <div class="author-avatar"><img src="img/samchloe.png" width="48" height="48" /></div>	
+                                        <div class="author-avatar"><img src="<?=$avatar?>" width="48" height="48" /></div>	
                                         <div class="author-information">
-                                            <div class="author-name clear"><span class="first_name">Sam Chloe</span> <span class="last_name">Ranada</span></div>
-                                            <div class="author-company"><span class="job">Marketing</span><span class="company_comma">,</span> <span class="company">BMW Singapore</span></div>
+                                            <div class="author-name clear">                                            
+                                                <span class="first_name"><?= HTML::entities($feedback->firstname); ?></span>
+                                                <span class="last_name"><?= HTML::entities($feedback->lastname); ?></span>
+                                                <span class="last_name_ini"><?= HTML::entities(substr($feedback->lastname, 0, 1)); ?>.</span>
+                                            </div>
+                                            <div class="author-company">
+                                                <span class="job"><?= HTML::entities($feedback->position); ?><span class="company_comma">, </span></span>
+                                                <span class="company"><?= HTML::entities($feedback->companyname); ?></span>
+                                            </div>
                                             <div class="author-location-info clear">
-                                                <div class="author-location"><span class="city">Quezon City</span><span class="location_comma">,</span> <span class="country">Philippines</span></div><div class="flag flag-ph"></div>
+                                                <div class="author-location">
+                                                    <span class="city"><?= HTML::entities($feedback->city); ?><span class="location_comma">, </span></span>
+                                                    <span class="country"><?= HTML::entities($feedback->countryname); ?></span>
+                                                </div>
+                                                <div class="flag flag-<?=strtolower($feedback->countrycode)?>"></div>
+                                            </div>
+                                            <div class="custom-meta-data clear">
+                                                <?php if( ! is_null($metadata) ): ?>
+                                                    <?php foreach( $metadata as $group ): ?>
+                                                        <?php foreach( $group as $item ): ?>
+                                                            <div class="meta-data">
+                                                                <span class="meta-name"><?= HTML::entities( ucwords(str_replace('_', ' ', $item[0]->name)) ); ?> : </span>
+                                                                <span class="meta-value"><?= HTML::entities($item[0]->value); ?></span>
+                                                            </div>
+                                                        <?php endforeach; ?>
+                                                    <?php endforeach; ?>
+                                                <?php endif; ?>
                                             </div>
                                         </div>	
                                     </div>
+
                                     <div class="reviews clear">
                                         <div class="ratings clear">
-                                            <div class="feedback-timestamp">Posted 3 hours ago</div>
-                                            <div class="stars blue clear">
-                                                <div class="star full"></div>
-                                                <div class="star full"></div>
-                                                <div class="star full"></div>
-                                                <div class="star full"></div>
-                                                <div class="star half"></div>    
-                                            </div>
+                                            <div class="star_rating" rating="<?=$feedback->int_rating;?>"></div>
+                                            <div class="feedback-timestamp"><?=$feedback->daysago?></div>
+                                        </div>
+                                        <div class="rating-stat" style="display: <?= ($vote_count == 0 ? 'none' : ''); ?>;">
+                                            <span class="vote_count"><?= $vote_count; ?></span> people found this useful
                                         </div>
                                     </div>
                                 </div>
@@ -193,75 +227,103 @@
                                 <!-- feedback text bubble -->
                                 <div class="feedback-text-bubble">
                                     <div class="feedback-tail"></div>
-                                    <div class="rating-stat">87 of 98 people found this useful</div>
-                                    <div class="custom-meta-data clear">
-                                        <div class="meta-data"><span class="meta-name">Product Purchased : </span><span class="meta-value"> Spaghetti Bolognese</span></div>
-                                        <div class="meta-data"><span class="meta-name">Quality : </span><span class="meta-value"> Excellent</span></div>
-                                    </div>
                                     <div class="feedback-text">
-                                        <p>Our company strives to bring only the best possible products and services suitable for our clients specific needs. Our business is simple: you describe, we create. Visit us at www.charleskeith.com today and experience the love.</p>
+                                        <?= HTML::entities($feedback->text);?>
                                     </div>
                                     <!-- are there any additional info uploaded?? -->
+                                    <?php if($attachments): ?>
                                     <div class="additional-contents">
                                         <!-- is it an image? -->
-                                         
-                                        <div class="uploaded-video">
-                                            <div class="padded-5">
-                                                <iframe width="717" height="346" src="http://www.youtube.com/embed/e9iDr1kFZDo" frameborder="0" allowfullscreen></iframe>
+                                        <?php if(isset($attachments->uploaded_images)): ?>
+                                            <div class="uploaded-images clear">
+                                                <?php foreach($attachments->uploaded_images as $uploaded_image): ?>
+                                                <div class="uploaded-image">
+                                                    <div class="padded-5">
+                                                        <div class="the-thumb">
+                                                            <input type="hidden" class="large-image-url" value="<?=$uploaded_image->large_url?>"/>
+                                                            <img src="<?=$uploaded_image->small_url?>" width="100%" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <?php endforeach; ?>
                                             </div>
-                                        </div>
-                                        <!-- or just a preview link? -->
-                                        
-                                    </div>
+                                        <?php endif; //endif uploaded images ?>
 
-                                    <div class="admin-comment-block">
-                                        <div class="admin-comment-box">
-                                            <div class="admin-comment-textbox-container">
-                                                <textarea class="admin-comment-textbox"></textarea>
-                                            </div>
-                                            <div class="admin-comment-leave-a-reply">
-                                                <span class="admin-logged-session">Logged in as <a href="#">Chris Davidson</a></span><input type="button" class="regular-button" value="Post Comment" />
-                                            </div>
-                                        </div>
+                                        <?php if(isset($attachments->attached_link)): ?>                       
+                                            <div class="uploaded-link">
+                                                <div class="padded-5">
+                                                        <div class="form-video-meta">
+                                                            <?php if($attachments->attached_link->video=='yes'): ?>
+                                                                <div class="video-thumb">
+                                                                    <div class="video-circle"></div>
+                                                                    <div class="the-thumb">
+                                                                        <input type="hidden" class="link-url" value="<?=$attachments->attached_link->url?>"/>
+                                                                        <img src="<?=$attachments->attached_link->image?>" width="100%">
+                                                                    </div>
+                                                                </div>
+                                                            <?php else: ?>
+                                                                <div class="video-thumb">
+                                                                    <a href="<?=$attachments->attached_link->url?>" target="_blank">
+                                                                        <img src="<?=$attachments->attached_link->image?>" width="100%">
+                                                                    </a>
+                                                                </div>
+                                                            <?php endif; ?>
 
-                                        <div class="admin-comment">
-                                            <div class="admin-name">Amy from Acme Inc says..</div>
-                                            <div class="admin-message clear">
-                                                <div class="admin-avatar"><img src="fullpage/common/img/samchloe.png" width="32" height="32" /></div>
-                                                <div class="message">Great choice!</div>
+                                                            <div class="video-details">
+                                                                <h3><?=$attachments->attached_link->title?></h3>
+                                                                <p><?=$attachments->attached_link->description?></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                             </div>
+                                        <?php endif; //endif attached link ?>
                                         </div>
-                                        
-                                    </div>
-                                    <br />
+                                    <?php endif; ?>
                                 </div>
                                 <!-- end of feedback text bubble -->
                                 <!-- feedback user actions -->
                                 <div class="feedback-options clear">
                                     <div class="feedback-recommendation">
-                                        <div class="green-thumb">Recommended by Leica to friends</div>
-                                    </div>
-                                    <div class="feedback-vote">
-                                        <span class="vote-action">Was this useful? <a href="#" class="small-btn-pin">Yes</a> <a href="#" class="small-btn-pin">No</a></span>
+                                        <?php if( $is_recommended ): ?>
+                                            <div class="green-thumb">Recommended by <?= HTML::entities($feedback->firstname); ?> to friends</div>
+                                        <?php endif; ?>
+                                        <div class="vote-block">
+                                            <span class="vote-action <?= ($voted != 1 ? '' : 'hidden'); ?>">
+                                                Was this useful? <a href="#" class="small-btn-pin">Yes</a>
+                                            </span>
+                                            <span class="undo_vote <?= ($voted == 1 ? '' : 'hidden'); ?>">
+                                                Undo vote
+                                            </span>
+                                        </div>
                                     </div>
                                     <div class="feedback-actions clear">
-                                        <span class="flag-as">Flag as inappropriate</span>
+                                        <span class="flag-as <?= ($flagged != 1 ? '' : 'hidden'); ?>">
+                                            Flag as inappropriate
+                                        </span>
+                                        <span class="undo_flag <?= ($flagged == 1 ? '' : 'hidden'); ?>">
+                                            Undo flag
+                                        </span>
                                         <span class="share-button">
                                             Share
                                             <div class="share-box">
-                                                <div class="share-box-arrow"></div>
+                                             <div class="share-box-arrow"></div>
                                                 <div class="btn-block">
-                                                    facebook
+                                                    <div class="fb_like_dummy" 
+                                                        data-href="<?=URL::to('single/'.$feedback->id)?>"
+                                                        data-layout="button_count"
+                                                        data-send="false" 
+                                                        data-width="80" 
+                                                        data-show-faces="false"></div>
                                                 </div>
                                                 <div class="btn-block">
-                                                    twitter
-                                                </div>
-                                                <div class="btn-block">
-                                                    google+
+                                                    <a href="<?=URL::to('single/'.$feedback->id)?>"
+                                                        data-url="<?=URL::to('single/'.$feedback->id)?>"
+                                                        data-text="<?=$feedback->text?>"
+                                                        class="tw_share_dummy">Tweet</a>
                                                 </div>
                                             </div>
                                         </span>
-                                    </div>    
+                                    </div>
                                 </div>
                                 <!-- end of feedback user actions -->
                             </div>
