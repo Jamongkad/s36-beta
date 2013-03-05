87042cb3a5bf557e6b58eeb6272715e3edd59101
jamongkad <wrm932@gmail.com>
2013-03-05 12:53:37 +0800

    wq

--- application/views/hosted/hosted_feedback_single_view_new.php
+++ application/views/hosted/hosted_feedback_single_view_new.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns:fb="http://ogp.me/ns/fb#">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Hosted Page Timeline Layout</title>
 <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
         <meta property="og:title" content="<?=strip_tags($feedback->title)?>"/> 
+        <meta property="og:description" content="<?=strip_tags($feedback->text)?>"/> 
+        <meta property="og:type" content="article"/> 
+        <?if($feedback->avatar):?>
+            <meta property="og:image" content='<?=URL::to(Config::get('application.avatar150_dir').'/'.$feedback->avatar)?>'/> 
+        <?else:?>
+            <meta property="og:image" content='<?=URL::to('img/36logo2.png')?>'/> 
+        <?endif?>
+        <meta property="og:url" content="<?=URL::to('single/'.$feedback->id)?>"/> 
+        <meta property="og:site_name" content="36Stories: Feedback made easy."/> 
+        <meta property="fb:app_id" content="<?=$fb_id?>"/>
+
         <?= HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'); ?>
         <?= HTML::script('https://platform.twitter.com/widgets.js'); ?>
         <?= HTML::script('https://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js'); ?>
