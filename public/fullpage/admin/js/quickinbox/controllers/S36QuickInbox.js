f93f62a8b260d80c8326c74a53d6b780dcd2e2a8
jamongkad <wrm932@gmail.com>
2013-03-08 23:23:29 +0800

    wq

--- public/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js
+++ public/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js
@@ -23,8 +23,13 @@ app.controller("AppCtrl", function($scope, $compile, QuickInboxService) {
                     feed_request();  
                     $('.widget-list').jScrollPane();
                 }, 10000); 
+                /*
                 $scope.feedbacks = data;
                 $scope.$apply($scope.feedbacks); 
+                */
+                $scope.$apply(function() {
+                    $scope.feedbacks = data;     
+                })
             }
         });
 
