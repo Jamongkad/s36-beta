286b1a04ea049389b5fc7a1a579b6e8daa547375
jamongkad <wrm932@gmail.com>
2013-03-10 23:00:01 +0800

    wq

--- public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
+++ public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
@@ -190,14 +190,7 @@ angular.module('S36QuickInboxDirectives', [])
         restrict: 'A'
       , link: function(scope, element, attrs) { 
             scope.$watch(function() {
-                var pane = $('.widget-list').jScrollPane();
-                var api = pane.data('jsp');
-                if(api) {
-                    api.destroy();
-                }
-                setTimeout(function() {
-                    $('.widget-list').jScrollPane();
-                }, 0);
+                $('.widget-list').jScrollPane();
                 var length = element.find('.widget-item').length;
                 return length
             }, function(length) {
