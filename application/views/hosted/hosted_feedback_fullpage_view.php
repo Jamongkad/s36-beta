a3b635b2299c6e8dd3ad07ffea69d186833869eb
jamongkad <wrm932@gmail.com>
2013-03-10 19:19:59 +0800

    wq

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -158,7 +158,7 @@
         fullpageCommon.init_fullpage_common(); // initialize document ready of the common javascript
         
         <?php if($user): //then display the admin bar by default ?>
-            widgetscroll();
+            //widgetscroll();
             var fullpageAdmin  = new S36FullpageAdmin(fullpageLayout);
             fullpageAdmin.init_fullpage_admin();
             fullpageCommon.init_toggle_bar(0);
