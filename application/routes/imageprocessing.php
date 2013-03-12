c879e7ed07708c6c3fc2c1331520300dde03dd4d
jamongkad <wrm932@gmail.com>
2013-03-12 12:23:08 +0800

    wq

--- application/routes/imageprocessing.php
+++ application/routes/imageprocessing.php
@@ -86,8 +86,8 @@ return array(
                     'max_height'    => 1200,
                 ),
                 'medium' => array(
-                    'max_width'     => 350,
-                    'max_height'    => 350,
+                    'max_width'     => 150,
+                    'max_height'    => 150,
                 ),
                 'small' => array(
                     'max_width'     => 80,
