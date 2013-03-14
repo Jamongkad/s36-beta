a1af27a2911288ca5e7d9057b81b72449b4f0980
jamongkad <wrm932@gmail.com>
2013-03-14 14:50:30 +0800

    iwq

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -194,22 +194,22 @@
           closeEffect : 'none'
          });
         $(".fancybox-video").click(function() {
-        $.fancybox({
-            'padding'       : 0,
-            'autoScale'     : false,
-            'transitionIn'  : 'none',
-            'transitionOut' : 'none',
-            'title'         : this.title,
-            'width'         : 640,
-            'height'        : 385,
-            'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-            'type'          : 'swf',
-            'swf'           : {
-            'wmode'             : 'transparent',
-            'allowfullscreen'   : 'true'
-            }
-        });
-        return false;
+            $.fancybox({
+                'padding'       : 0,
+                'autoScale'     : false,
+                'transitionIn'  : 'none',
+                'transitionOut' : 'none',
+                'title'         : this.title,
+                'width'         : 640,
+                'height'        : 385,
+                'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+                'type'          : 'swf',
+                'swf'           : {
+                    'wmode'             : 'transparent',
+                    'allowfullscreen'   : 'true'
+                }
+            });
+            return false;
         });
     });
 </script>
