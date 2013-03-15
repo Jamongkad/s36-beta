7157e98db6d323ab9be1191bf315a9cc4aed2db7
jamongkad <wrm932@gmail.com>
2013-03-15 12:47:08 +0800

    wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -416,7 +416,7 @@ return array(
             $filter = Array(
                 'company_id' => 6
               , 'rating' => 'positive'
-              , 'privacy_policy' => 'all'
+              , 'privacy_policy' => 'public'
             );
             $feedback = $tf->data->dbfeedback->newfeedback_by_company($filter); 
             $tf->dump($feedback);
