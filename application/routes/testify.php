8dc1380dfaa62699dc0d82bc4ad79254ebf58ddf
jamongkad <wrm932@gmail.com>
2013-03-15 12:15:02 +0800

    wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -415,7 +415,6 @@ return array(
         $tf->test("Quick Inbox: DBFeedback", function($tf) {  
             $filter = Array(
                 'rating' => 'positive'
-              , 'privacy' => 'public'
             );
             $feedback = $tf->data->dbfeedback->newfeedback_by_company(6, $filter); 
             $tf->dump($feedback);
