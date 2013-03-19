1a56d86cd41f556ac2dde1898855cd4d617c152e
jamongkad <wrm932@gmail.com>
2013-03-19 19:26:45 +0800

    wq

--- public/js/inbox/Status.js
+++ public/js/inbox/Status.js
@@ -3,15 +3,17 @@ function Status() {
     this.notification = "#notification";
 }
 
-Status.prototype.notify = function(msg, delay) { 
+Status.prototype.notify = function(msg, delay, callback) { 
     var me = this;
 	var delay = delay;
+    var callback = (typeof callback === "undefined") ? null : callback;
 
 	$(me.message).empty().html(msg).show();    
     $(me.notification).animate({height: '50', opacity: '100'}, 'fast', '', function() { 
 		if(delay){
 			setTimeout(function() {
                 $(me.notification).animate({ height: 0, opacity: 0 }, 'fast'); 
+                callback();
             }, delay);		
 		}
     });
