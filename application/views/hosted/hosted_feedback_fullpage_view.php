086dac640d6677dacb315158d09e87253f9d8ac0
jamongkad <wrm932@gmail.com>
2013-03-19 18:25:51 +0800

    wq

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -47,13 +47,13 @@
                 <!-- social link icons 1/28/2013 -->
                 <div id="socialLinkIcons" class="clear">
                     <?php if(!empty($panel->facebook_url)): ?>
-                        <div class="social-icon fb"><a id="fb_url" href="<?= $panel->facebook_url; ?>">
+                        <div class="social-icon fb"><a id="fb_url" href="<?= $panel->facebook_url; ?>" target="newtab">
                             <img src="/fullpage/common/img/facebook.png" title="Facebook Page" />
                         </a>
                         </div>
                     <?php endif; ?>
                     <?php if(!empty($panel->twitter_url)): ?>
-                        <div class="social-icon tw"><a href="<?= $panel->twitter_url; ?>">
+                        <div class="social-icon tw"><a href="<?= $panel->twitter_url; ?>" target="newtab">
                             <img src="/fullpage/common/img/twitter.png" title="Twitter Page" />
                         </a>
                         </div>
