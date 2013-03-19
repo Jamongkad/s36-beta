9bffb4651ddc0b89d981217f2ed3058c95021469
jamongkad <wrm932@gmail.com>
2013-03-19 13:53:42 +0800

    wq

--- public/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js
+++ public/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js
@@ -38,7 +38,7 @@ app.controller("AppCtrl", function($scope, $compile, QuickInboxService) {
         });
     })();
 
-    var update_selected = function(action, id) {
+    $scope.update_selected = function(action, id) {
         if (action == 'add' & $scope.selected.indexOf(id) == -1)
             $scope.selected.push(id);
         if (action == 'remove' && $scope.selected.indexOf(id) != -1)
@@ -48,7 +48,7 @@ app.controller("AppCtrl", function($scope, $compile, QuickInboxService) {
     $scope.update_selection = function($event, feed) {
         var checkbox = $event.target;
         var action = (checkbox.checked ? 'add' : 'remove');
-        update_selected(action, feed);
+        $scope.update_selected(action, feed);
     }
 
     $scope.is_selected = function(id) {
