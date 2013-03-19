c3bad4be722bfbdd2ecfcfebca2b5be50c1a4d85
jamongkad <wrm932@gmail.com>
2013-03-19 19:14:20 +0800

    wq

--- public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
+++ public/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js
@@ -246,10 +246,9 @@ function ucwords(str) {
 }
 
 function feedback_process_message() { 
-    var delay = 2000;
     $('#notification-message').empty().html("Processing feedback...");
     $('#notification').animate({ height: '50', opacity: '100' }, 'fast','',function() { 
-        setTimeout(redirect_and_hide(), delay);       
+        redirect_and_hide();
     });
 }
 
