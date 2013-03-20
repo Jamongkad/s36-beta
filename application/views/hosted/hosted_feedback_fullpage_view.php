d8cbba1cfb2dbc77d6ff6bade135d49365125b9e
kennwel <kennwel.labarda@microsourcing.ph>
2013-03-20 14:08:53 +0800

    save.

--- application/views/hosted/hosted_feedback_fullpage_view.php
+++ application/views/hosted/hosted_feedback_fullpage_view.php
@@ -222,7 +222,6 @@
 <?= HTML::script('/fullpage/admin/js/quickinbox/controllers/S36QuickInbox.js'); ?>
 <?= HTML::script('/fullpage/admin/js/quickinbox/directives/S36QuickInboxDirectives.js'); ?>
 <?= HTML::script('/fullpage/admin/js/quickinbox/services/S36QuickInboxServices.js'); ?>
-<?= HTML::script('/fullpage/admin/js/S36FullpageAdmin.js'); ?>
 <? endif ?>
 <?php 
 /*
