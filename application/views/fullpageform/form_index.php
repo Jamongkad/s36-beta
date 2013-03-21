4192f3d46b7b0d717bd651c90e622a349ee18af1
jamongkad <wrm932@gmail.com>
2013-03-21 18:17:22 +0800

    wq

--- application/views/fullpageform/form_index.php
+++ application/views/fullpageform/form_index.php
@@ -1,5 +1,4 @@
 <h1>Hello Muthafuckas! {{name}}</h1>
-{{firstname}}
-<p><input type="text" value="" name="firstname" ng-model="firstname"/></p>
+<p><input type="text" value="{{firstname}}" name="firstname" ng-model="firstname"/></p>
 <p><input type="text" value="" name="lastname" /></p>
 <a href="../submit/#/profile">Next</a>
