8904212d3b2f2433cb950567740bef7325f12539
jamongkad <wrm932@gmail.com>
2013-03-21 17:09:41 +0800

    wq

--- application/routes/api.php
+++ application/routes/api.php
@@ -6,7 +6,7 @@ $feedback = new DBFeedback;
 
 return array(
     'GET /api/fullpage_form' => function() {  
-         echo View::make('fullpageform/form_index');
+         return View::make('fullpageform/form_index');
      },
 
     'POST /api/login' => function() {
