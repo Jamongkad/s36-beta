24c44a136ddbb0dedebac780e8da0891e1d824de
jamongkad <wrm932@gmail.com>
2013-03-21 16:54:55 +0800

    playing with routes

--- application/views/partials/company_footer.php
+++ application/views/partials/company_footer.php
@@ -1,8 +1,2 @@
-
-<?=HTML::script('js/jquery.cycle.all.min.js')?>
-<?=HTML::script('js/jquery.mousewheel.js')?>
-<?=HTML::script('js/jquery.scroll.js')?> 
-<?=HTML::script('js/masonry.js')?> 
-<?=HTML::script('js/modernizr.js')?> 
 </body>
 </html>
