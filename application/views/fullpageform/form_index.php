6686483d862c7f07929fd11bd249ed9a3131cfa2
jamongkad <wrm932@gmail.com>
2013-03-21 19:26:17 +0800

    wq

--- application/views/fullpageform/form_index.php
+++ application/views/fullpageform/form_index.php
@@ -1,6 +1,6 @@
 <h1>Hello Muthafuckas!</h1>
 <div ng-controller="FormCtrl">
-    <p><input type="text" value="" name="firstname" ng-model="data.firstname" required/></p>
-    <p><input type="text" value="" name="lastname" ng-model="data.lastname" required/></p>
+    <p><input type="text" value="" name="firstname" ng-model="data.firstname" /></p>
+    <p><input type="text" value="" name="lastname" ng-model="data.lastname" /></p>
 </div>
 <a href="../submit/#/profile">Next</a>
