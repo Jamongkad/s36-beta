2eebfc3d4fdd5db234d8babf8928ce7e798f6756
jamongkad <wrm932@gmail.com>
2013-03-21 17:42:17 +0800

    wq

--- application/views/fullpageform/form_index.php
+++ application/views/fullpageform/form_index.php
@@ -1,4 +1,4 @@
 <h1>Hello Muthafuckas! {{name}}</h1>
 <p><input type="text" value="" name="firstname" /></p>
 <p><input type="text" value="" name="lastname" /></p>
-<a href="profile">Next</a>
+<a href="submit/#/profile">Next</a>
