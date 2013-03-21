a78b8f9bdf1ef453aff9478cc604b8587f981ef9
jamongkad <wrm932@gmail.com>
2013-03-21 17:48:31 +0800

    wq

--- application/views/fullpageform/form_profile.php
+++ application/views/fullpageform/form_profile.php
@@ -1,2 +1,4 @@
 <h1>Profile Page</h1>
+<p><input type="text" value="" name="firstname" /></p>
+<p><input type="text" value="" name="lastname" /></p>
 <a href="../submit/#/">Prev</a> --- <a href="../submit/#/review">Next</a>
