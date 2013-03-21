d983c10425c541b4271bf04a2dd9fe6704d1735e
jamongkad <wrm932@gmail.com>
2013-03-21 18:31:03 +0800

    wq

--- application/views/fullpageform/form_index.php
+++ application/views/fullpageform/form_index.php
@@ -1,4 +1,4 @@
 <h1>Hello Muthafuckas! {{name}}</h1>
 <p><input type="text" value="" name="firstname" /></p>
 <p><input type="text" value="" name="lastname" /></p>
-<a href="../<?=$company_name?>/submit/#/profile">Next</a>
+<a href="../<?=Config::get('application.subdomain')?>/submit/#/profile">Next</a>
