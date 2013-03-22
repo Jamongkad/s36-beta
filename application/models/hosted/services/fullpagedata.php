9f6a4dbf5f863ad3e3f5d023d7a438b718dbdac8
jamongkad <wrm932@gmail.com>
2013-03-22 15:40:52 +0800

    wq

--- application/models/hosted/services/fullpagedata.php
+++ application/models/hosted/services/fullpagedata.php
@@ -19,7 +19,7 @@ class FullpageData {
         //TODO: caching candidate
         $result = new StdClass;
         $result->todays_count = $this->feedback->count_todays_feedback($this->company_params['company_id']); 
-        $result->published_feed_count = $this->feedback->televised_feedback_alt($this->company_params['company_name']);
+        $result->published_feed_count = $this->feedback->televised_feedback_alt($this->company_params['company_name'])->total_rows;
         return $result; 
     }
 }
