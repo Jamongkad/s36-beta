f24e95741cf81fe556da92993e606c5a737255f5
jamongkad <wrm932@gmail.com>
2013-03-22 13:23:31 +0800

    hidden inputs

--- application/views/fullpageform/form_profile.php
+++ application/views/fullpageform/form_profile.php
@@ -1,5 +1,9 @@
 <h1>Profile Page</h1>
 <div ng-controller="ProfileCtrl">
+
+    <input type="hidden" value="" name="firstname" ng-model="data.firstname" />
+    <input type="hidden" value="" name="lastname" ng-model="data.lastname" />
+
     <p><input type="text" value="" name="firstname" ng-model="data.firstname"/></p>
     <p><input type="text" value="" name="lastname" ng-model="data.lastname"/></p>
 </div>
