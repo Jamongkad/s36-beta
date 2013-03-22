e38432aee9c8f5485e2e93997dbb3d63e9088fea
jamongkad <wrm932@gmail.com>
2013-03-22 12:26:54 +0800

    wq

--- application/views/fullpageform/form_index.php
+++ application/views/fullpageform/form_index.php
@@ -3,4 +3,4 @@
     <p><input type="text" value="" name="firstname" ng-model="data.firstname" /></p>
     <p><input type="text" value="" name="lastname" ng-model="data.lastname" /></p>
 </div>
-<a href="../<?=Config::get('application.subdomain')?>/submit/#/profile">Next</a>
+<a href="../submit/#/profile">Next</a>
