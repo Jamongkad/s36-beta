79d582354b1b619c166dc8e82c1c4576ef0750fe
jamongkad <wrm932@gmail.com>
2013-03-22 13:36:14 +0800

    ok here we go!

--- application/views/fullpageform/submission_send.php
+++ application/views/fullpageform/submission_send.php
@@ -6,4 +6,4 @@
     <p><input type="text" value="" name="firstname" ng-model="data.firstname"/></p>
     <p><input type="text" value="" name="lastname" ng-model="data.lastname"/></p>
 </div>
-<a href="#/review">Prev</a> --- <a href="#/">Send</a>
+<a href="#/review">Prev</a> --- <a href="#/send_now">Send</a>
