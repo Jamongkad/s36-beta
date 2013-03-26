9f1d327f8bbd1174aa6766e2e074f9c9c16cb84c
jamongkad <wrm932@gmail.com>
2013-03-26 15:55:39 +0800

    ok

--- application/views/hosted/hosted_feedback_form_view.php
+++ application/views/hosted/hosted_feedback_form_view.php
@@ -1,7 +1,21 @@
 <?//=HTML::style('css/widget_master/hosted-form.css');?>
 <?//=$company_header?>
 <?//=$widget?>
-<div ng-app="Form">
+<?=HTML::style('css/widget_master/new_form.css');?>
+<?=HTML::script('js/jcycle.js');?>
+<?=HTML::script('js/jquery.ui.widget.js');?>
+<?=HTML::script('js/jquery.iframe-transport.js');?>
+<?=HTML::script('js/jquery.fileupload.js');?>
+<?=HTML::script('https://cloud.github.com/downloads/bytespider/jsOAuth/jsOAuth-1.3.6.min.js');?>
+<?=HTML::script('js/jquery.oauth.js');?>
+<!-- file upload js requirements -->
+<!-- link preview js -->
+<?=HTML::script('js/link.preview.js');?>
+<!-- link preview js -->
+<!--[if lt IE 8]>
+    <script src="js/ie7.js"></script>
+<![endif]-->
+<div id="formBox" ng-app="Form">
     <ng-view></ng-view>
 </div>
 <div class="block" style="height:20px;"></div>
