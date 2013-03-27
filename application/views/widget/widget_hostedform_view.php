2bd78f856a88917b3f42769e2c83ce5b97536528
jamongkad <wrm932@gmail.com>
2013-03-27 12:05:36 +0800

    pwet

--- application/views/widget/widget_hostedform_view.php
+++ application/views/widget/widget_hostedform_view.php
@@ -340,24 +340,24 @@ if($facebook = $company_social->fetch_social_account('facebook')) {
                             <div class="step-contents">
 
                                 <div id="hostform_info" style="width:50%;float:left;">
-                                <table id="s36_form" width="100%" border="0" cellpadding="4" cellspacing="4">
-                                    <tr><td colspan="2"><strong>Required Fields</strong></td></tr>
-                                    <tr><td><input type="text" id="your_fname" class="regular-text required" title="First Name" value="" /></td><td><input type="text" id="your_lname" class="regular-text required" title="Last Name" value="" /></td></tr>
-                                    <tr><td colspan="2"><input type="text" id="your_email" class="regular-text required long" title="Email Address" value="" /></td></tr>
-                                </table>
-                                <table id="form_complete" class="form_complete" width="100%" border="0" cellpadding="4" cellspacing="4">
-                                    <tr><td><input type="text" id="your_city" class="regular-text required" title="City" value="" /></td><td>
-                                    <select id="your_country" class="regular-select required" title="Country">
-                                        <option>Country</option>
-                                        <?foreach($country as $countries):?>
-                                            <option value="<?=$countries->code?>"><?=$countries->name?></option>
-                                        <?endforeach?>         
-                                    </select>
-                                        </td></tr>
-                                        <tr><td colspan="2"><strong>Optional info - but great to include!</strong></td></tr>
-                                        <tr><td><input type="text" id="your_company" value="" class="regular-text" title="Company Name" /></td><td><input type="text" value="" id="your_occupation" title="Occupation" class="regular-text required" /></td></tr>
-                                        <tr><td colspan="2"><input type="text" id="your_website" class="regular-text long" value="" title="Website Address" /></td></tr>
-                                </table>
+                                    <table id="s36_form" width="100%" border="0" cellpadding="4" cellspacing="4">
+                                        <tr><td colspan="2"><strong>Required Fields</strong></td></tr>
+                                        <tr><td><input type="text" id="your_fname" class="regular-text required" title="First Name" value="" /></td><td><input type="text" id="your_lname" class="regular-text required" title="Last Name" value="" /></td></tr>
+                                        <tr><td colspan="2"><input type="text" id="your_email" class="regular-text required long" title="Email Address" value="" /></td></tr>
+                                    </table>
+                                    <table id="form_complete" class="form_complete" width="100%" border="0" cellpadding="4" cellspacing="4">
+                                        <tr><td><input type="text" id="your_city" class="regular-text required" title="City" value="" /></td><td>
+                                        <select id="your_country" class="regular-select required" title="Country">
+                                            <option>Country</option>
+                                            <?foreach($country as $countries):?>
+                                                <option value="<?=$countries->code?>"><?=$countries->name?></option>
+                                            <?endforeach?>         
+                                        </select>
+                                            </td></tr>
+                                            <tr><td colspan="2"><strong>Optional info - but great to include!</strong></td></tr>
+                                            <tr><td><input type="text" id="your_company" value="" class="regular-text" title="Company Name" /></td><td><input type="text" value="" id="your_occupation" title="Occupation" class="regular-text required" /></td></tr>
+                                            <tr><td colspan="2"><input type="text" id="your_website" class="regular-text long" value="" title="Website Address" /></td></tr>
+                                    </table>
                                 </div>
 
                                 <div id="hostform_photo" style="width:50%;float:left;">
