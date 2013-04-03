72b3a5a403dc22763f7619dd0ab4f64a3cf74379
jamongkad <wrm932@gmail.com>
2013-04-03 15:51:59 +0800

    Story#82

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -42,6 +42,7 @@ angular.module('formbuilder', [])
                             var formcode_url = $("#formcode-manager-url").attr('hrefaction') + "/" + widget_key;
                             $.ajax({
                                 type: "POST"
+                              , dataType: 'json'       
                               , url: "/feedsetup/buildmetadata_options"
                               , data: $("ul[id^=frmb-]").serializeFormList({ prepend: "frmb" }) + "&form_id=" + widget_store_id + "&company_id=" + company_id
                               , success: function(msg) {
