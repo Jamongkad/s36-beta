44322ecdb89646d04617b93243a60d1c638e256d
jamongkad <wrm932@gmail.com>
2013-04-03 15:27:48 +0800

    Story#82 adding validation

--- public/js/inbox/directives/myformbuilder.js
+++ public/js/inbox/directives/myformbuilder.js
@@ -43,7 +43,7 @@ angular.module('formbuilder', [])
                             $.ajax({
                                 type: "POST"
                               , url: "/feedsetup/buildmetadata_options"
-                              , data: $("ul[id^=frmb-]").serializeFormList({prepend: "frmb"}) + "&form_id=" + widget_store_id + "&company_id=" + company_id
+                              , data: $("ul[id^=frmb-]").serializeFormList({ prepend: "frmb" }) + "&form_id=" + widget_store_id + "&company_id=" + company_id
                               , success: function() {
                                     //window.location = formcode_url;                             
                                 }
