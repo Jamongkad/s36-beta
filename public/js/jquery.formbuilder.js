e5b89d81adf5bc83c8c83dd4c5c2a6c32a60c81a
jamongkad <wrm932@gmail.com>
2013-04-05 15:56:29 +0800

    yummey

--- public/js/jquery.formbuilder.js
+++ public/js/jquery.formbuilder.js
@@ -190,11 +190,9 @@
 						for (i = 0; i < values.length; i++) {
 							field += checkboxFieldHtml(values[i]);
 						}
-                        console.log("A");
-					}
-					else {
+					} else {
 						field += checkboxFieldHtml('');
-                        console.log("B");
+                        console.log($('div.chk_group'));
 					}
 					field += '<div class="add-area"><a href="#" class="add add_ck">' + opts.messages.add + '</a></div>';
 					field += '</div>';
