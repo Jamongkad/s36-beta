eb855e5bc328c0aea08c501273c04094a2ace6b9
jamongkad <wrm932@gmail.com>
2013-04-11 15:39:26 +0800

    Story #79

--- application/views/dashboard/dashboard_index_view.php
+++ application/views/dashboard/dashboard_index_view.php
@@ -34,12 +34,6 @@
                       , label: {
                            show: true
                          , radius: 1/2
-                         /*
-                         , background: {
-                            opacity: 0.5     
-                          , color: '#000'
-                         }
-                         */
                          , formatter: function(label, series) {
                            return '<div style="font-size:8pt;text-align:center;color:white">'+label+'<br/>'+Math.round(series.percent)+'%</div>';
                          } 
@@ -94,14 +88,8 @@
             <div class="dashboard-updates-list negative">
                 <strong><?=HTML::link('inbox/negative', $dashboard_summary->dashscores->negativefeed." new entries")?></strong> are rated negatively 
             </div>
-            <!--<div class="dashboard-updates-list ignored"><strong>5 entries</strong> have been ignored </div>-->
         </div>
         <div class="dashboard-pie-chart">
-            <!--
-            <div id="chart_div">
-            </div>
-            -->
-            <!--<div class="overall">Overall 4.5/5 <span class="rating excellent">EXCELLENT</span></div>-->
             <h3 style="text-align:center">Feedback Rating Distribution</h3>
             <div id="chart_div" style="width:200px;height:200px"></div>
         </div>
@@ -111,22 +99,15 @@
             <div class="g1of3">
                 <h3>Contacts</h3>
                 <p><strong><?=$dashboard_summary->dashscores->newfeed?> incoming</strong> entries from contacts </p>
-                <!--<p><strong>3 of your contacts</strong> have not yet responded to your feedback requests</p>-->
             </div>
             <div class="g1of3">
                 <h3>Feedback Statistics</h3>
                 <p><strong><?=$dashboard_summary->dashscores->feedfeatured?></strong> featured entries </p>
                 <p><strong><?=$dashboard_summary->dashscores->feedpublished?></strong> published entries </p>
-                <!--
-                <p><strong>800</strong> feedback requests  </p>
-                <p><strong>372,824</strong> impressions </p>
-                <p><strong>239,493</strong> clicks</p>
-                -->
             </div>
             <div class="g1of3">
                 <h3>Feedback Submitters</h3>
                 <p>Top country:<strong><?=$dashboard_summary->dashscores->topcountry?></strong> </p>
-                <!--<p>Average time on site: <strong>2:93</strong> </p>-->
                 <p>Unique submitters: <strong> <?=$dashboard_summary->dashscores->contacttotal?> </strong> </p>
             </div>
         </div>
