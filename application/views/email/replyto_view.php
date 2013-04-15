6084e35163b0dd8ae8d35d0fdb3f7e7aa2f9851f
jamongkad <wrm932@gmail.com>
2013-04-15 15:02:59 +0800

    wq

--- application/views/email/replyto_view.php
+++ application/views/email/replyto_view.php
@@ -50,121 +50,14 @@
                         	<td colspan="3">
                             	<table>
                                     <?=$profile_partial_view?>
-                                    <?
-                                        $metadata = (!empty($feedback->metadata)) ? json_decode($feedback->metadata) : false; 
-                                        $attachments = (!empty($feedback->attachments)) ? json_decode($feedback->attachments) : false; 
-                                    ?>
-                                    <tr>
-                                    <?if($metadata || $attachments):?>
-                                        <div style="float:left;background:#f7f7f7;border:1px solid #dde7ef;padding:15px;margin-top:10px;
-                                        ">
-                                            <div style="margin-bottom: 10px;float:left">
-                                                <? 
-                                                //start metadata 
-                                                if($metadata):
-                                                    foreach($metadata as $key => $val):?>
-                                                        <?foreach($val as $k => $v):?>
-                                                            <div style="float:left">
-                                                                <div style=" 
-                                                                    font-weight:bold;
-                                                                    font-size:11px;
-                                                                    color:#6499cd;
-                                                                    float:left;
-                                                                    margin-right:10px;
-                                                                ">
-                                                                    <?if($key == 'select'):?>
-                                                                        <?=ucwords($k)?>: 
-                                                                    <?endif?>
-
-                                                                    <?if($key == 'checkbox' || $key == 'radio' || $key == 'text'):?>
-                                                                        <?=ucwords(str_replace("_", " ", $k));?>:
-                                                                    <?endif?>
-
-                                                                    <?
-                                                                        $prefix = "";
-                                                                        $value_list = "";
-                                                                        foreach($v as $d) {
-                                                                            $value_list .= "<span style='color:#a2a2a2;'>" . $prefix . $d->value . "</span>";    
-                                                                            $prefix = ", ";
-                                                                        }
-                                                                        echo $value_list;
-                                                                    ?>
-                                                                </div>
-                                                            </div>
-                                                        <?endforeach?> 
-                                                    <?endforeach?>
-                                                <?endif?>
-                                            </div>
-                                    <?php if($attachments): ?>
-                                        <div style="float:left;">
-                                            <?php 
-                                            /*
-                                            | Start Video and Web Link Attachment
-                                            */
-                                            if(isset($attachments->attached_link)):?>
-                                                <div style="float:left;width:100%;margin-bottom:15px">
-                                                        <?php 
-                                                        //video attachments
-                                                        if($attachments->attached_link->video=='yes'):?>
-                                                            <a class="inbox-fancybox-video" href="<?=str_replace('http','https',$attachments->attached_link->url)?>" rel="inbox-videos-<?=$id?>" style="display:block">
-                                                            <div class="the-thumb">
-                                                                <img src="<?=$attachments->attached_link->image?>"  />
-                                                            </div>
-                                                            </a>
-                                                        <?else:?>
-                                                            <div class="attached-link-thumb">
-                                                                <a href="<?=$attachments->attached_link->url?>" target="_blank">
-                                                                    <img src="<?=$attachments->attached_link->image?>" />
-                                                                </a>
-                                                            </div>
-                                                            <div class="attached-link-details">
-                                                                <h3><?=$attachments->attached_link->title?></h3>
-                                                                <p style="font-size:10px"><?=$attachments->attached_link->description?></p>
-                                                            </div>   
-                                                        <?endif?>
-                                                </div>
-                                                <br/>
-                                            <?php 
-                                            /*
-                                            | End Video and Web Link Attachment
-                                            */
-                                            endif;
-                                            /*
-                                            | Start Image Attachments
-                                            */
-                                            if(isset($attachments->uploaded_images)):?>
-                                                <?php
-                                                if(count($attachments->uploaded_images) == 1) $width='100%';
-                                                if(count($attachments->uploaded_images) == 2) $width='50%';
-                                                if(count($attachments->uploaded_images) == 3) $width='33%';
-                                                ?>
-                                                <?php foreach($attachments->uploaded_images as $uploaded_image): ?>
-                                                    <div  style="float:left;width:<?=$width;?>">
-                                                        <div class="the-thumb">
-                                                            <a class="inbox-fancybox-image" href="<?=Config::get('application.url')?><?=Config::get('application.attachments_large').'/'.$uploaded_image->name?>">
-
-                                                            <?if(count($attachments->uploaded_images) == 1):?>
-                                                                <img src="<?=Config::get('application.url')?><?=Config::get('application.attachments_large').'/'.$uploaded_image->name?>" />
-                                                            <?else:?>
-                                                                <img src="<?=Config::get('application.url')?><?=Config::get('application.attachments_medium').'/'.$uploaded_image->name?>"  />
-                                                            <?endif?>
-
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                <?php endforeach; ?>
-                                            <?php
-                                            /*
-                                            | End Image Attachments
-                                            */
-                                            endif;
-                                            ?>
-                                        </div>
-                                        <?php endif;?>
-                                    <?php endif;?>
-                                    </tr>
                                 </table>
                             </td>
+                        </tr>
+                        <tr height="80">
+                        	<td colspan="3"></td>
+                        </tr> 
+                        <!-- end of contents -->
+                        
                         <!-- sig -->
                         <tr>
                         	<td colspan="3">
@@ -180,26 +73,9 @@
                         	<td colspan="3"><a href="#" style="color:#6e8cca;">No - I am not interested, do not ever mail me again.</a></td>
                         </tr>
                         -->
-                        <tr>
-                            <td style="font-size:10px;padding:0px 30px;line-height:16px;">
-                                This message was intended for <?=$emailto?>.  <br />
-
-                                <p style="">
-                                    <b>36Stories Inc</b> 340 Lemon Ave #6168 Walnut CA, 91789 United States
-                                </p>
-                                <!--
-                                If you do not wish to receive this type of email from 36Stories in the future, please click here to unsubscribe.<br />
-                                36Stories, Inc. P.O. Box 10005, Palo Alto, CA 94303
-                                -->
-                           </td>
-                        </tr>
-                        <!-- end footer -->    
-
-                        </tr>
-                        <tr height="80">
+                        <tr height="20">
                         	<td colspan="3"></td>
-                        </tr> 
-                        <!-- end of contents -->
+                        </tr>
                         
                         <!-- end sig -->
                     </table>
@@ -208,6 +84,20 @@
         </td>
     </tr>
     <!-- footer -->
+    <tr>
+        <td style="font-size:10px;padding:0px 30px;line-height:16px;">
+            This message was intended for <?=$emailto?>.  <br />
+
+            <p style="">
+                <b>36Stories Inc</b> 340 Lemon Ave #6168 Walnut CA, 91789 United States
+            </p>
+            <!--
+            If you do not wish to receive this type of email from 36Stories in the future, please click here to unsubscribe.<br />
+            36Stories, Inc. P.O. Box 10005, Palo Alto, CA 94303
+            -->
+       </td>
+    </tr>
+    <!-- end footer -->    
 </table>
 </body>
 </html>
