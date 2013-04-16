0e16be7b172dcd185888ac19f7da58fb7de3fef8
jamongkad <wrm932@gmail.com>
2013-04-16 19:27:07 +0800

    wq

--- application/views/feedback/partials/editform_view.php
+++ application/views/feedback/partials/editform_view.php
@@ -2,7 +2,7 @@
     <div class="modal-configure">  
         <h4>Configure Message</h4>
         <br/>
-        <a href="#" ng-click="cancel_reply()">Back</a>
+        <a href="#" ng-click="cancel_reply()" goback>Back</a>
         <br/>
         <textarea class="regular-text" style="width: 692px; height: 140px" name="msg"></textarea><br/>
         <input type="hidden" name="msgid" value="" id="msgid"/>
