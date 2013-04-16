96df093f68e0186ee2948f82ff25b0efb07e4a24
jamongkad <wrm932@gmail.com>
2013-04-16 19:11:03 +0800

    dasd

--- application/views/feedback/partials/editform_view.php
+++ application/views/feedback/partials/editform_view.php
@@ -1,6 +1,7 @@
 <div class="lightbox-form">
     <div class="modal-configure">  
         <h4>Configure Message</h4>
+        <a href="#" ng-click="cancel_reply()">Back</a>
         <textarea class="regular-text" style="width: 692px; height: 140px" name="msg"></textarea><br/>
         <input type="hidden" name="msgid" value="" id="msgid"/>
         <input type="hidden" name="msgtype" value="" id="msgtype"/>
