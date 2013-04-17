74277b8a5d145915c8922b58c77b927242e8984c
jamongkad <wrm932@gmail.com>
2013-04-17 18:00:45 +0800

    wq

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -5,7 +5,7 @@
     </div>
     <div id="footer">
         <h4>Feedback & Customer Satisfaction Simplified.</h4>
-        <h4>36Stories © 2011. </h4>
+        <h4>FDBack © 2011. </h4>
         <p>Keep in touch by following us on <a href="#">Facebook</a>, <a href="#">Twitter</a>, subscribing to our <a href="#">blog's feed</a> and joining our <a href="#">email newsletter</a>.</p>
     </div>
     <?if(Config::get('application.env_name') == 'production'):?>
