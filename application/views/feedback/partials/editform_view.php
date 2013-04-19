157306b9c830520baee1b5ad49c59af72c64e305
jamongkad <wrm932@gmail.com>
2013-04-19 15:54:10 +0800

    wq

--- application/views/feedback/partials/editform_view.php
+++ application/views/feedback/partials/editform_view.php
@@ -1,11 +1,11 @@
 <div class="lightbox-form">
     <div class="modal-configure">  
         <h4>Configure Message</h4>
-        <textarea class="regular-text" style="width: 692px; height: 140px" ng-model="tmplvar.msg.text">
+        <textarea class="regular-text" style="width: 692px; height: 140px" ng-model="tmplvar.text">
 <?//=($msg) ? $msg->text : Null ?>
         </textarea><br/>
         <!--<input type="hidden" ng-model="messages.msgid" ng-init="messages.msgid='<?//=($msg) ? $msg->id : Null?>'"/>-->
-        <input type="hidden" ng-model="tmplvar.msg.msgid"/>
+        <input type="hidden" ng-model="tmplvar.msgid"/>
         <input type="hidden" name="msgtype" value="msg" id="msgtype"/>
         <!--
         <div class="add-msg-box-buttons">
