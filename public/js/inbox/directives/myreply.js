406d6a504f98a84b13b9432f0063e0f5ebb36c5a
jamongkad <wrm932@gmail.com>
2013-04-22 17:46:23 +0800

    wq

--- public/js/inbox/directives/myreply.js
+++ public/js/inbox/directives/myreply.js
@@ -177,7 +177,7 @@ angular.module('reply', [])
                 console.log($(this).attr('feedid'));
                 console.log($(this).attr('email'));
 
-                console.log($('textarea[name=bcc][feedid=' + $(this).attr('feedid')+ ']'));
+                console.log($('textarea[name=bcc][feedid=' + $(this).attr('feedid') + ']'));
                 e.preventDefault();
             });
 
