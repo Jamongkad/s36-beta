a1d9e58ec60f1c9dd7879092b15e78f002eae058
jamongkad <wrm932@gmail.com>
2013-04-22 16:28:01 +0800

    tring

--- application/views/feedback/partials/replyform_index.php
+++ application/views/feedback/partials/replyform_index.php
@@ -31,14 +31,12 @@
                 <label style="font-size:11px;font-weight:bold;">+ Bcc: </label>
             </td>
             <td>
-                <ul class="add-bcc" bcc load="replybody.user">
-                    <!--
+                <ul class="add-bcc">
                     <li>
-                        <a href="#" reply-bcc class="linky" feedid="replybody.feedid" email="replybody.user.ffemail1">{{replybody.user.alias1}} {{replybody.user.ffemail1}}</a>
-                        <a href="#" reply-bcc class="linky" feedid="replybody.feedid" email="replybody.user.ffemail2">{{replybody.user.alias2}} {{replybody.user.ffemail2}}</a>
-                        <a href="#" reply-bcc class="linky" feedid="replybody.feedid" email="replybody.user.ffemail3">{{replybody.user.alias3}} {{replybody.user.ffemail3}}</a>
+                        <a href="#" class="linky" feedid="replybody.feedid" email="replybody.user.ffemail1">{{replybody.user.alias1}} {{replybody.user.ffemail1}}</a>
+                        <a href="#" class="linky" feedid="replybody.feedid" email="replybody.user.ffemail2">{{replybody.user.alias2}} {{replybody.user.ffemail2}}</a>
+                        <a href="#" class="linky" feedid="replybody.feedid" email="replybody.user.ffemail3">{{replybody.user.alias3}} {{replybody.user.ffemail3}}</a>
                     </li>
-                    -->
                 </ul>
             </td>
             <td class="small" valign="top"></td>
