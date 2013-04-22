423f0852354162dcca67eb9bcd48213c49a0150f
jamongkad <wrm932@gmail.com>
2013-04-22 16:18:55 +0800

    wq

--- application/views/feedback/partials/replyform_index.php
+++ application/views/feedback/partials/replyform_index.php
@@ -34,8 +34,6 @@
                 <ul class="add-bcc">
                     <li>
                         <a href="#" reply-bcc class="linky" feedid="replybody.feedid" email="replybody.user.ffemail1">{{replybody.user.alias1}} {{replybody.user.ffemail1}}</a>
-                        <a href="#" reply-bcc class="linky" feedid="replybody.feedid" email="replybody.user.ffemail2">{{replybody.user.alias2}} {{replybody.user.ffemail2}}</a>
-                        <a href="#" reply-bcc class="linky" feedid="replybody.feedid" email="replybody.user.ffemail3">{{replybody.user.alias3}} {{replybody.user.ffemail3}}</a>
                     </li>
                 </ul>
             </td>
