16872affde982023f923a50bc73778b2fe2d1852
jamongkad <wrm932@gmail.com>
2013-04-23 18:58:17 +0800

    wq

--- application/views/feedback/partials/editform_view.php
+++ application/views/feedback/partials/editform_view.php
@@ -1,6 +1,6 @@
 <div class="lightbox-form">
     <div class="modal-configure">  
-        <h4>Configure Message</h4>
+        <h4>Edit Message</h4>
         <textarea class="regular-text" style="width: 692px; height: 140px" ng-model="tmplvar.text">
         </textarea><br/>
         <input type="hidden" ng-model="tmplvar.msgid"/>
