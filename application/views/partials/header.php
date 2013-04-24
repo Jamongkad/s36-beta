7c885e1e0eb8b560949e454866192f1c2645498f
jamongkad <wrm932@gmail.com>
2013-04-24 18:56:43 +0800

    wq

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -66,7 +66,6 @@
             <?
                 $company = new Company\Repositories\DBCompany;
                 $company_info = $company->get_company_info(S36Auth::user()->companyid);
-                print_r($company_info->widgetkey);
             ?>
 
             <div class="left-buttons">
