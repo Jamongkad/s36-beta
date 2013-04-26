6f8398aaf260b22aaaeca8836097587dded7bb1f
jamongkad <wrm932@gmail.com>
2013-04-26 14:13:48 +0800

    bad

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -477,7 +477,9 @@
                                     <?php endif;?>
 
                                     <?if($reports):?>
-                                        <?print_r($reports);?>
+                                        <?foreach($reports as $report):?>
+                                            <?print_r($report);?>
+                                        <?endforeach?>
                                     <? 
                                     /*
                                     | End Image Attachments
