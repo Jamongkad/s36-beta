a00941fe38e5dfb9095445d2e3ae917a36dc5972
jamongkad <wrm932@gmail.com>
2013-05-29 12:13:39 +0800

    wq wq

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -4,8 +4,8 @@
         <div class="c"></div>
     </div>
     <div id="footer">
+        <h4>FDBack © 2011</h4>
         <h4>Feedback & Customer Satisfaction Simplified.</h4>
-        <h4>FDBack © 2011. </h4>
         <!--
         <p>Keep in touch by following us on <a href="#">Facebook</a>, <a href="#">Twitter</a>, subscribing to our <a href="#">blog's feed</a> and joining our <a href="#">email newsletter</a>.</p>
         -->
