de3c390677410cc18a385fb0e56bd421a6d0346c
jamongkad <wrm932@gmail.com>
2013-06-05 18:28:59 +0800

    wq

--- application/models/email/entities/newfeedbacksubmission.php
+++ application/models/email/entities/newfeedbacksubmission.php
@@ -8,9 +8,13 @@ class NewFeedbackSubmission extends EmailFixture {
     public function gather($email_data) {
         $this->address = $email_data->get_addresses();
         $this->feedback_data = $email_data->get_feedback();
+        $this->hosted_data = $email_data->get_hosteddata();
     }
 
     public function send() {
+
+        Helpers::dump($this->hosted_data);
+
         foreach($this->address as $address) {
 
             $login_url = Helpers::make_forward_url($address->companyid, '/feedback/modifyfeedback/'.$this->feedback_data->id);
