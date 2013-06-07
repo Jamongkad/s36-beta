696393efacb40bf526a72ce98e1d1e197bd2e84e
jamongkad <wrm932@gmail.com>
2013-06-07 12:47:50 +0800

    wq

--- application/views/email/thankyou_view.php
+++ application/views/email/thankyou_view.php
@@ -8,7 +8,15 @@
             <?if(is_object($activity_check)):?>
                 <h2>This feedback has already been published by <?=ucfirst($activity_check->username)?>.</h2>
             <?else:?>
-                <h2>Sweet! You have just published <?=$contact_name?>'s feedback onto your website!</h2>
+                <?if($status == "publish"):?>
+                    <h2>Sweet! You have just published <?=$contact_name?>'s feedback onto your website!</h2>
+                <?else:?>
+                    <h2>Sweet! You have just unpublished <?=$contact_name?>'s feedback onto your website! </h2>
+
+                    <span style="font-size:12px;color:#333;">
+                        Based on your auto-posting settings, we have automatically published this feedback. (<a href="<?=$settings_url?>">Change</a>)
+                    </span>
+                <?endif?>
             <?endif?>
             </div>
             <br/>
