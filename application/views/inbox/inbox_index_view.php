e9d45a47714df79de2e7fbf463d5c3e3989086f3
jamongkad <wrm932@gmail.com>
2013-06-10 14:30:12 +0800

    clearing sample html

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -125,17 +125,15 @@
                                             <?endforeach?>
                                         </div>
                                     <?endif?>
+                                    <?if($attachments):?>
+                                        <div class="feedback-custom-attachments">
+                                            
+                                        </div>
+                                    <?endif?>
+                                    <?if($reports):?>
+
+                                    <?endif?>
                                     <!--
-                                    <div class="feedback-custom-metas grids">
-                                        <div class="feedback-custom-meta">
-                                            <span class="custom-meta-name">Favorite Exercise : </span>
-                                            <span class="custom-meta-value">Benchpress</span>
-                                        </div>
-                                        <div class="feedback-custom-meta">
-                                            <span class="custom-meta-name">Favorite Trainor : </span>
-                                            <span class="custom-meta-value">Henry Castor</span>
-                                        </div>
-                                    </div>
                                     <div class="feedback-custom-attachments">
                                         <div class="feedback-custom-att-container">
                                             <div class="grids">
@@ -256,146 +254,7 @@
                 <?php endforeach ?>
             </div>
         <?endforeach?>
-        <div class="dashboard-feedback grids">
-            <div class="custom-checkbox"><input type="checkbox" /></div>
-            <div class="feedback-avatar"><img src="/fullpage/admin/img/sample-inbox-image2.jpg" width="100%" /></div>
-            <div class="feedback-contents">
-                <div class="responsive-padding">
-                    <div class="feedback-title">Stay fit and healthy. This is so cool! </div>
-                    <div class="feedback-text">I've been to a few gyms before</div>
-                    <div class="feedback-author">
-                        <span class="feedback-author-name">Kc Cayamanda</span>
-                        <span class="feedback-author-location">Philippines, PH</span>
-                        <span class="splitter">|</span>
-                        <span class="feedback-author-date">March 29, 2013</span>
-                        <span class="feedback-author-time">04:31:03 pm</span>
-                        <span class="feedback-author-src">Facebook Verified</span>
-                    </div>
-                    <div class="feedback-custom-block">
-                        <div class="feedback-custom-metas grids">
-                            <div class="feedback-custom-meta">
-                                <span class="custom-meta-name">Favorite Exercise : </span>
-                                <span class="custom-meta-value">Benchpress</span>
-                            </div>
-                            <div class="feedback-custom-meta">
-                                <span class="custom-meta-name">Favorite Trainor : </span>
-                                <span class="custom-meta-value">Henry Castor</span>
-                            </div>
-                        </div>
-                        <div class="feedback-custom-attachments">
-                            <div class="feedback-custom-att-container">
-                                <div class="grids">
-                                    <div class="custom-att">
-                                        <img src="/fullpage/admin/img/budoc.jpg" width="100%"/>
-                                    </div>
-                                    <div class="custom-att g1of3">
-                                        <img src="/fullpage/admin/img/gels.jpg" width="100%"/>
-                                    </div>
-                                    <div class="custom-att g1of3">
-                                        <img src="/fullpage/admin/img/yassi.jpg" width="100%"/>
-                                    </div>
-                                    <div class="custom-att g1of3">
-                                        <img src="/fullpage/admin/img/jasmin.jpg" width="100%"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="feedback-details">
-                        <span class="feedback-details-rating good">
-                            GOOD
-                        </span>
-                        <span class="feedback-details-privacy">PRIVATE</span>
-                        <span class="feedback-details-status">
-                            Status : <small>New</small>
-                        </span>
-                        <span class="feedback-details-status">
-                            Priority : <small>High</small>
-                        </span>
-                        <span class="feedback-details-modify"><a href="#">Modify Additional Info</a></span>
-                    </div>
-                </div>
-            </div>
-            <div class="dashboard-feedback-actions grids">
-                <div class="feedback-action-menu">
-                    <div class="grids">
-                        <div class="action-sprite action-delete"></div>
-                        <div class="action-delete-tooltip"></div>
-                    </div>
-                    <div class="action-gap"></div>
-                    <div class="grids">
-                        <ul class="action-gray-menu">
-                            <li class="action-sprite publish">
-                                <div class="action-tooltip">
-                                    <span>Publish Feedback</span>
-                                    <div class="action-tooltip-arrow"></div>
-                                </div>
-                            </li>
-                            <li class="action-sprite feature">
-                                <div class="action-tooltip">
-                                    <span>Feature Feedback</span>
-                                    <div class="action-tooltip-arrow"></div>
-                                </div>
-                            </li>
-                            <li class="action-sprite reply">
-                                <div class="action-tooltip">
-                                    <span>Reply to User</span>
-                                    <div class="action-tooltip-arrow"></div>
-                                </div>
-                            </li>
-                            <li class="action-sprite contact">
-                                <div class="action-tooltip">
-                                    <span>Fast Forward</span>
-                                    <div class="action-tooltip-arrow"></div>
-                                </div>
-                            </li>
-                            <li class="action-sprite save">
-                                <div class="action-tooltip">
-                                    <span>Categorize Feedback</span>
-                                    <div class="action-tooltip-arrow"></div>
-                                </div>
-                                <div class="the-categories-menu">
-                                    <div class="the-category-arrow"></div>
-                                    <div class="the-categories-menu-content">
-                                        <div class="the-categories">
-                                            <h3>File this feedback as : </h3>
-                                            <ul class="grids">
-                                                <li><a href="#">General</a></li>
-                                                <li><a href="#">Miscelleanous</a></li>
-                                                <li><a href="#">Price</a></li>
-                                                <li><a href="#">Problems/Bugs</a></li>
-                                                <li><a href="#">Suggestions</a></li>
-                                            </ul>
-                                            <a class="manage-categories-link" href="#">Manage Categories</a>
-                                        </div>
-                                        <div class="false-border"></div>
-                                        <div class="the-categories-options">
-                                            <div class="categories-combo">
-                                                <span class="category-combo-name">Status </span><span><select><option>New</option></select></span>
-                                            </div>
-                                            <div class="categories-combo">
-                                                <span class="category-combo-name">Priority </span><span><select><option>New</option></select></span>
-                                            </div>
-                                        </div>
-                                        <div class="false-border"></div>
-                                        <div class="the-categories-delete">
-                                            <a href="#" class="delete-button"></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="action-sprite flag-action">
-                                <div class="action-tooltip">
-                                    <span>Flag Feedback</span>
-                                    <div class="action-tooltip-arrow"></div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <!--
         <div class="dashboard-feedback grids">
             <div class="custom-checkbox"><input type="checkbox" /></div>
             <div class="feedback-avatar"><img src="/fullpage/admin/img/sample-inbox-image2.jpg" width="100%" /></div>
@@ -463,85 +322,6 @@ I'd be looking forward to my next visit. </p></div>
                 </div>
             </div>
         </div>
-        <div class="dashboard-feedback grids">
-            <div class="custom-checkbox"><input type="checkbox" /></div>
-            <div class="feedback-avatar"><img src="/fullpage/admin/img/sample-inbox-image2.jpg" width="100%" /></div>
-            <div class="feedback-contents">
-                <div class="responsive-padding">
-                    <div class="feedback-title">Stay fit and healthy. This is so cool! </div>
-                    <div class="feedback-text">I've been to a few gyms before</div>
-                    <div class="feedback-author">
-                        <span class="feedback-author-name">Kc Cayamanda</span>
-                        <span class="feedback-author-location">Philippines, PH</span>
-                        <span class="splitter">|</span>
-                        <span class="feedback-author-date">March 29, 2013</span>
-                        <span class="feedback-author-time">04:31:03 pm</span>
-                        <span class="feedback-author-src">Facebook Verified</span>
-                    </div>
-                    <div class="feedback-custom-block">
-                        <div class="feedback-custom-metas grids">
-                            <div class="feedback-custom-meta">
-                                <span class="custom-meta-name">Favorite Exercise : </span>
-                                <span class="custom-meta-value">Benchpress</span>
-                            </div>
-                            <div class="feedback-custom-meta">
-                                <span class="custom-meta-name">Favorite Trainor : </span>
-                                <span class="custom-meta-value">Henry Castor</span>
-                            </div>
-                        </div>
-                        <div class="feedback-custom-attachments">
-                            <div class="feedback-custom-att-container">
-                                <div class="grids">
-                                    <div class="custom-att">
-                                        <img src="/fullpage/admin/img/budoc.jpg" width="100%"/>
-                                    </div>
-                                    <div class="custom-att g1of3">
-                                        <img src="/fullpage/admin/img/gels.jpg" width="100%"/>
-                                    </div>
-                                    <div class="custom-att g1of3">
-                                        <img src="/fullpage/admin/img/yassi.jpg" width="100%"/>
-                                    </div>
-                                    <div class="custom-att g1of3">
-                                        <img src="/fullpage/admin/img/jasmin.jpg" width="100%"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="feedback-details">
-                        <span class="feedback-details-rating good">
-                            GOOD
-                        </span>
-                        <span class="feedback-details-privacy">PRIVATE</span>
-                        <span class="feedback-details-status">
-                            Status : <small>New</small>
-                        </span>
-                        <span class="feedback-details-status">
-                            Priority : <small>High</small>
-                        </span>
-                        <span class="feedback-details-modify"><a href="#">Modify Additional Info</a></span>
-                    </div>
-                </div>
-            </div>
-            <div class="dashboard-feedback-actions grids">
-                <div class="feedback-action-menu">
-                    <div class="grids">
-                        <div class="action-sprite action-delete"></div>
-                        <div class="action-delete-tooltip"></div>
-                    </div>
-                    <div class="action-gap"></div>
-                    <div class="grids">
-                        <ul class="action-gray-menu">
-                            <li class="action-sprite publish"></li>
-                            <li class="action-sprite feature"></li>
-                            <li class="action-sprite reply"></li>
-                            <li class="action-sprite contact"></li>
-                            <li class="action-sprite save"></li>
-                            <li class="action-sprite flag-action"></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>                                                       
+        -->
     </div>
 </div>
