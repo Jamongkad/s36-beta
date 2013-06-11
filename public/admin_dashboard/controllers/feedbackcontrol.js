72f225e87c744ef5f11c12b7a4bb377d02a1e7f4
jamongkad <wrm932@gmail.com>
2013-06-11 17:12:13 +0800

    wq

--- public/admin_dashboard/controllers/feedbackcontrol.js
+++ public/admin_dashboard/controllers/feedbackcontrol.js
@@ -1,7 +1,6 @@
 function FeedbackControl($scope) { 
 
     var feedid = $scope.feedid;
-    console.log($scope.catid);
     
     $scope.delete_feedback = function(id) {
         console.log("delete id:" + id);
