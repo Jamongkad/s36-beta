3abe6e3c7fb72b947b9a060889ff24bfbf762ff3
jamongkad <wrm932@gmail.com>
2013-06-12 12:22:50 +0800

    wq

--- public/admin_dashboard/directives/myfeedbackcontrol.js
+++ public/admin_dashboard/directives/myfeedbackcontrol.js
@@ -2,9 +2,13 @@ angular.module('feedbackcontrol', [])
 .directive('feature', function() {
     return {
         restrict: 'A'     
+      , scope: {
+            feedid: "=feedid"    
+        }
       , link: function(scope, element, attrs) {
             $(element).bind('click', function(e) { 
                 console.log("Feature Directive: " + scope.feedid);
+                console.log(scope);
             });
         }
     } 
