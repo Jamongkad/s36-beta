445e9418da3ebd70753ede865ee318111933ab45
jamongkad <wrm932@gmail.com>
2013-06-14 14:23:32 +0800

    wq

--- public/admin_dashboard/directives/myfeedbackcontrol.js
+++ public/admin_dashboard/directives/myfeedbackcontrol.js
@@ -2,16 +2,16 @@ angular.module('feedbackcontrol', [])
 .directive('feature', function(FeedbackControlService, $compile) {
     return {
         restrict: 'A'     
-      , link: function($scope, element, attrs) {
+      , link: function(scope, element, attrs) {
             $(element).bind('click', function(e) { 
-                var me = $(element);
+                var me = $(element)
                 me.parents('.dashboard-feedback').fadeOut(500, function() {
                     var child_count = $(me).parents('.feedback-group').children('.dashboard-feedback:visible');
                     if(child_count.length == 0) {
                         $(me).parents('.feedback-group').fadeOut(500);
                     }
 
-                    var html_str = $compile("<span undo></span>")($scope);
+                    var html_str = $compile("<span undo></span>")(scope);
 
                     $(".checky-bar").html("Undo this shit mah nigguh? " + html_str);
                 });
