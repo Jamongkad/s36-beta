b4eb5a8ac2450546f15d326776bb0bf2cb2a5a57
jamongkad <wrm932@gmail.com>
2013-06-14 10:53:13 +0800

    tanga

--- public/admin_dashboard/controllers/feedbackcontrol.js
+++ public/admin_dashboard/controllers/feedbackcontrol.js
@@ -1,7 +1,8 @@
 function FeedbackControl($scope, FeedbackControlService) { 
 
     $scope.feedback_status = function(id, feed_status) { 
-        console.log(feed_status + " id:" + id);
+        console.log(feed_status + " id: " + id);
+        FeedbackControlService.change_status(id, feed_status);
     }
 
     $scope.reply_feedback = function(id) {
