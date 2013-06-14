07213cd16d54d898f25fcbd26a637aeb439e1baa
jamongkad <wrm932@gmail.com>
2013-06-14 17:28:41 +0800

    wq

--- public/admin_dashboard/directives/myfeedbackcontrol.js
+++ public/admin_dashboard/directives/myfeedbackcontrol.js
@@ -1,27 +1,25 @@
 angular.module('feedbackcontrol', [])
-.directive('feature', function(FeedbackControlService, $compile, $parse) {
+.directive('feature', function(FeedbackControlService, $compile) {
     return {
         restrict: 'A'     
-      , compile: function(element, attrs) {
-            return function(scope, element, attrs) {
-                $(element).bind('click', function(e) { 
-                    var me = $(element)
-                    me.parents('.dashboard-feedback').fadeOut(500, function() {
+      , link: function(scope, element, attrs) {
+            $(element).bind('click', function(e) { 
+                var me = $(element)
+                me.parents('.dashboard-feedback').fadeOut(500, function() {
 
-                        var child_count = $(me).parents('.feedback-group').children('.dashboard-feedback:visible');
+                    var child_count = $(me).parents('.feedback-group').children('.dashboard-feedback:visible');
 
-                        if(child_count.length == 0) {
-                            $(me).parents('.feedback-group').fadeOut(500);
-                        }
-                        
-                        var str = $parse("<a undo class='undo'>Undo</a>");
-                        $(".checky-bar").html( str );
+                    if(child_count.length == 0) {
+                        $(me).parents('.feedback-group').fadeOut(500);
+                    }
+                    
+                    var str = "<a undo class='undo'>Undo</a>"; 
+                    $(".checky-bar").html($compile(str)(scope));
 
-                    });
-                    console.log("Feature Directive");
                 });
-            }      
-        }    
+                console.log("Feature Directive");
+            });
+        }
     } 
 })
 .directive('undo', function() {
