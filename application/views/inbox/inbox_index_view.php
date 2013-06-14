442ab35d53564a06200deb53beddf54b4405d77e
jamongkad <wrm932@gmail.com>
2013-06-14 12:31:50 +0800

    ulol

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -1,4 +1,5 @@
 <script>
+//this enables the checky-bar to be a fixture
 $(window).scroll(function(){
     $(".checky-bar").css({"position": "fixed", "margin-left": "-165px", "margin-top": "-100px"});
 });
