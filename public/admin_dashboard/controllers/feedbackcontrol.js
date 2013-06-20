1c59e9906fdac30cb86c03dcce347df87713a8ae
jamongkad <wrm932@gmail.com>
2013-06-20 16:06:34 +0800

    wq

--- public/admin_dashboard/controllers/feedbackcontrol.js
+++ public/admin_dashboard/controllers/feedbackcontrol.js
@@ -57,6 +57,15 @@ function FeedbackControl($scope, FeedbackControlService, FeedbackSignal, Templat
             if(child_count.length == 0) {  
                 entity_parent.hide(); 
             }      
+
+            var feed = {
+                id: $scope.selected     
+              , status: $scope.status_select_value
+              , catid: Template.default_category_id
+              , origin: Template.current_inbox_state
+            }
+
+            FeedbackControlService.change_status(feed);
         }
     }
 
