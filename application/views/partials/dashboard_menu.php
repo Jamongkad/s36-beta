1c2fe4241331369bb0ffe3dc80c622d168cfebc3
Robert <robert.mordido@microsourcing.ph>
2013-06-21 09:45:09 +0800

    updated dashboard css and added submenu listing for dashboard

--- application/views/partials/dashboard_menu.php
+++ application/views/partials/dashboard_menu.php
@@ -7,6 +7,12 @@
         <li><a href="/feedsetup">Feedback Setup</a></li>
         <li><a href="/dashboard">Reports</a></li>
         <li><a href="/settings/display">Display Setup</a></li>
-        <li><a href="javascript:;">Settings</a></li>
+        <li><a href="javascript:;">Settings</a>
+            <ul class="submenu">
+                <li class="first-menu-subchild"><a href="#">Background</a></li>
+                <li><a href="#">Layout</a></li>                                
+                <li class="last-menu-subchild"><a href="#">Other Settings</a></li>                                
+            </ul>
+        </li>
     </ul>
 </div>
