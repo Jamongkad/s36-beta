9f3e371750de576cb459bef5310273fa5cf3527d
jamongkad <wrm932@gmail.com>
2013-07-01 14:30:31 +0800

    non

--- public/admin_dashboard/controllers/modifyfeedbackcontrol.js
+++ public/admin_dashboard/controllers/modifyfeedbackcontrol.js
@@ -1,3 +1,3 @@
-function ModifyFeedbackControl($scope, FeedbackControl, FeedbackService, FeedbackSignal) {
+function ModifyFeedbackControl($scope, FeedbackControlService, FeedbackService, FeedbackSignal) {
 
 }
