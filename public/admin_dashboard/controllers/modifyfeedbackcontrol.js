dfdbbac80e60ac50fc31b9c068fe295ec55e5e65
jamongkad <wrm932@gmail.com>
2013-07-01 17:20:41 +0800

    wq

--- public/admin_dashboard/controllers/modifyfeedbackcontrol.js
+++ public/admin_dashboard/controllers/modifyfeedbackcontrol.js
@@ -1,13 +1,15 @@
 function ModifyFeedbackControl($scope, FeedbackControlService, FeedbackService, FeedbackSignal) {
 
-    $scope.feedid = $(".feedid").val();
-    $scope.text = $(".feedback-textarea").val();
 
     $scope.fast_forward = function(email, feedid) {
         FeedbackService.send_fastforward(email, feedid);
     }
 
     $scope.save_feedback = function() {
+
+        $scope.feedid = $(".feedid").val();
+        $scope.text = $(".feedback-textarea").val();
+
         console.log($scope.feedid);     
         console.log($scope.text);     
     }
