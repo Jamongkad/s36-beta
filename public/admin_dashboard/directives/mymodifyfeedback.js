c005593b8b43ea04bdf1564805b9423d402f25e4
jamongkad <wrm932@gmail.com>
2013-07-01 16:02:57 +0800

    fixing

--- public/admin_dashboard/directives/mymodifyfeedback.js
+++ public/admin_dashboard/directives/mymodifyfeedback.js
@@ -4,7 +4,7 @@ angular.module('modifyfeedback', [])
         restrict: 'A'     
       , link: function(scope, element, attrs) { 
             $(element).hover(function(e) {
-                $(this).siblings('.the-categories-menu').css({'display' : 'block'}).hover(function() {}, function() {
+                $(this).siblings('.the-modify-categories-menu').css({'display' : 'block'}).hover(function() {}, function() {
                     $(this).hide();
                 })
             });
