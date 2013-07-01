13121a34eddd8da01e08655393d544ffe42f115c
jamongkad <wrm932@gmail.com>
2013-07-01 15:45:23 +0800

    wq

--- public/admin_dashboard/controllers/modifyfeedbackcontrol.js
+++ public/admin_dashboard/controllers/modifyfeedbackcontrol.js
@@ -1,3 +1,5 @@
 function ModifyFeedbackControl($scope, FeedbackControlService, FeedbackService, FeedbackSignal) {
-
+    $scope.fast_forward = function(email, feedid) {
+        FeedbackService.send_fastforward(email, feedid);
+    }
 }
