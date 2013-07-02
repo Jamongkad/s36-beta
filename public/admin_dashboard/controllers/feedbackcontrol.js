2635a0d17e4636188ad127b5756fc92f96d10569
jamongkad <wrm932@gmail.com>
2013-07-02 16:53:03 +0800

    wq

--- public/admin_dashboard/controllers/feedbackcontrol.js
+++ public/admin_dashboard/controllers/feedbackcontrol.js
@@ -285,6 +285,7 @@ function CheckyBox($scope, FeedbackSignal, FeedbackControlService) {
 
     $scope.close = function() { 
         console.log($scope.id);
+        console.log(checkboxes);
         if(checkboxes.length == $scope.id.length) {
             console.log("Well that is all of them");
         }
