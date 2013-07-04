2be2344663050c4c89db75c89f7abda9c8101aee
jamongkad <wrm932@gmail.com>
2013-07-04 11:54:01 +0800

    wq

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -373,9 +373,7 @@ var backend_vars = {
                                          ng-click="feedback_status($event)"
                                          data-feed='{"id": "<?=$feed->id?>", "catid": "<?=$feed->categoryid?>", "status": "delete"}'
                                          ></div>
-                                    <div class="action-tooltip">
-                                        PWET 
-                                    </div>
+                                    <div class="action-delete-tooltip"></div>
                                 </div>
                                 <div class="action-gap"></div>
                                 <div class="grids">
