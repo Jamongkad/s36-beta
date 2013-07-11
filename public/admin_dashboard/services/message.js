e7bb8c3f89dbcce6d381dc653ce80234cee53896
jamongkad <wrm932@gmail.com>
2013-07-11 17:56:42 +0800

    wq

--- public/admin_dashboard/services/message.js
+++ public/admin_dashboard/services/message.js
@@ -31,6 +31,7 @@ angular.module('MessageService', [])
           , data: {"type": type}
           , success: function(data) {
                 shared_service.message = data;
+                console.log(data);
                 shared_service.register_reply_message();
             }
         });
