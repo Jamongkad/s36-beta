56f6f2cd288b4d91af419b6e58cdd5626bf1fa03
jamongkad <wrm932@gmail.com>
2013-07-12 12:46:40 +0800

    wq

--- application/routes/testify.php
+++ application/routes/testify.php
@@ -159,8 +159,8 @@ return array(
             $filters = array(
                   'limit'   => 3
                 , 'site_id' => false 
-                , 'filter' => 'all' //(new arrivals) all (show only) featured published
-                , 'choice' => false //positive negative neutral profanity flagged mostcontent
+                , 'filter' => 'published' //(new arrivals) all (show only) featured published
+                , 'choice' => 'all' //positive negative neutral profanity flagged mostcontent
                 , 'date'   => false //date_new date_old
                 , 'rating' => false //5 4 3 2 1
                 , 'category' => false 
