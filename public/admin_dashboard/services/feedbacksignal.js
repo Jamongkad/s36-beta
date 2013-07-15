393714952927cf952aea9f5f98e5fc31784e756f
jamongkad <wrm932@gmail.com>
2013-07-15 17:25:10 +0800

    wq

--- public/admin_dashboard/services/feedbacksignal.js
+++ public/admin_dashboard/services/feedbacksignal.js
@@ -6,37 +6,14 @@ angular.module('FeedbackSignal', [])
     shared_service.data;
 
     shared_service.current_state = function(data) {
-        /*
-        $.ajax({
-            type: 'POST'    
-          , dataType: 'json'
-          , data: {'data': data}
-          , async: false
-          , url: '/feedback/set_current_feedback_state'
-          , success: function(data) {
-                shared_service.data = data;
-            }
-        }); 
-        */
         this.data = data;
         this.broadcast_now();
     }
     
-    /*
     shared_service.get_data = function() { 
-        var result;
-        $.ajax({
-            type: 'POST'    
-          , dataType: 'json'
-          , async: false
-          , url: '/feedback/get_current_feedback_state'
-          , success: function(data) {
-                result = data;
-            }
-        }); 
-        return result;
+        return this.data;
     }
-    */
+   
 
     shared_service.broadcast_now = function() {
         $rootScope.$broadcast('checkFeedbackStatus');
