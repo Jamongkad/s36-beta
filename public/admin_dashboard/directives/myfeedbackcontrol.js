9c8d1b7d7ad6a7eccf33ad1e4b2ba3a53041dcda
jamongkad <wrm932@gmail.com>
2013-07-16 11:12:16 +0800

    wq

--- public/admin_dashboard/directives/myfeedbackcontrol.js
+++ public/admin_dashboard/directives/myfeedbackcontrol.js
@@ -291,7 +291,7 @@ angular.module('feedbackcontrol', [])
                 }
 
                 FeedbackControlService.flag_feedback(modfeed);
-                //FeedbackSignal.broadcast_now()
+                FeedbackSignal.broadcast_now()
                 //FeedbackSignal.flag_feedback(modfeed);
                 e.preventDefault();
             });
