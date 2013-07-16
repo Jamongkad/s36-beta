1930c3ba7ce50b4039a25bc4fd721a9d5f48f888
jamongkad <wrm932@gmail.com>
2013-07-16 13:11:11 +0800

    fafist

--- public/admin_dashboard/directives/myfeedbackcontrol.js
+++ public/admin_dashboard/directives/myfeedbackcontrol.js
@@ -16,16 +16,14 @@ angular.module('feedbackcontrol', [])
                     if(data.status == 'flag' || data.status == 'unflag') { 
                         var feed = $.parseJSON($(this).attr('data-feed')); 
                         var policy = $(this).attr("return-policy");
-                        if(policy == 1) {
+                        if(feed.status == "unflag") {
                             feed.status = "flag";
                             $(this).removeAttr("style");
-                            $(this).attr("return-policy", 0);
                             var repackaged_json = JSON.stringify(feed); 
                             $(this).attr('data-feed', repackaged_json);
                         } else { 
                             feed.status = "unflag";
                             $(this).attr("style", "background-position: -194px -31px");
-                            $(this).attr("return-policy", 1);
                             var repackaged_json = JSON.stringify(feed); 
                             $(this).attr('data-feed', repackaged_json);
                         } 
