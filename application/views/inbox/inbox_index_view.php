56448adc0f56494d66ff2e79acd0d1df9aa85a09
jamongkad <wrm932@gmail.com>
2013-07-17 09:48:12 +0800

    you cheap fuc

--- application/views/inbox/inbox_index_view.php
+++ application/views/inbox/inbox_index_view.php
@@ -370,7 +370,7 @@ var backend_vars = {
                                     <span class="feedback-details-rating <?=strtolower($feed->rating)?>">
                                         <?=$feed->rating?>
                                     </span>
-                                    <span class="feedback-details-flag">FLAGGED</span>
+                                    <span class="feedback-details-flag"></span>
                                     <span class="feedback-details-privacy"><?=$feed->permission?></span>
                                     <span class="feedback-details-status">
                                         Status : <small class="status-line"><?=$feed->status?></small>
