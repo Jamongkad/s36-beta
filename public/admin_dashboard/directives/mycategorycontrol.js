184d5837a68242fc23cff874cbf0b147a85c7df2
jamongkad <wrm932@gmail.com>
2013-07-22 15:37:47 +0800

    I love you

--- public/admin_dashboard/directives/mycategorycontrol.js
+++ public/admin_dashboard/directives/mycategorycontrol.js
@@ -25,15 +25,18 @@ angular.module('categorycontrol', [])
 
                 if(that.text() == "Update") {
                     var ctgy_nm_val = $('input[name="ctgy_nm"]').val()
+                    
+                    if(ctgy_nm_val == 'undefined' || ctgy_nm_val == null || ctgy_nm_val == "")  { 
+                        alert("Cannot be blank!");
+                    } else { 
+                        that.text("Rename") 
+                            .parents('div')
+                            .siblings('div')
+                            .children('.ctgy-name')
+                            .html( ctgy_nm_val ).end().end().end();
 
-                    that.text("Rename") 
-                        .parents('div')
-                        .siblings('div')
-                        .children('.ctgy-name')
-                        .html( ctgy_nm_val ).end().end().end();
-
-                    Category.modify({ctgy_id: scope.catid, ctgy_nm: ctgy_nm_val});
-
+                        Category.modify({ctgy_id: scope.catid, ctgy_nm: ctgy_nm_val});
+                    }
                 } else {
                     that.text("Update")     
                         .parents('div')
