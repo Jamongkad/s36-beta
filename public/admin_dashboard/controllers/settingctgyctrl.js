294bc80c6c5f4776d910a152d287de17aadd4377
jamongkad <wrm932@gmail.com>
2013-07-22 14:25:50 +0800

    ok

--- public/admin_dashboard/controllers/settingctgyctrl.js
+++ public/admin_dashboard/controllers/settingctgyctrl.js
@@ -1,4 +1,7 @@
-function SettingCtgyCtrl($scope, MessageService) {
+function SettingCtgyCtrl($scope, Category) {
+
+    Category.fetch();
+
     $scope.add = function() {
         if($scope.category_name == 'undefined' || $scope.category_name == null)  {
             alert("Please provide a category name.");
