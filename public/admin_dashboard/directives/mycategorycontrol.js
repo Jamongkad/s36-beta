ce1321d66a1b1b20806413e737911ffe99a60be3
jamongkad <wrm932@gmail.com>
2013-07-22 15:04:02 +0800

    doing you right

--- public/admin_dashboard/directives/mycategorycontrol.js
+++ public/admin_dashboard/directives/mycategorycontrol.js
@@ -14,7 +14,26 @@ angular.module('categorycontrol', [])
         restrict: 'A'       
       , link: function(scope, element, attrs) {
             $(element).bind('click', function(e) {
-                console.log("Nigguhs");
+                var that = $(this);
+
+                var input = $('<input type="text" name="ctgy_nm"/>').val(
+                    that.parents('div').siblings('div').children('.ctgy-name').html()
+                );
+
+                if(that.text() == "Update") {
+                    var ctgy_nm_val = $('input[name="ctgy_nm"]').val()
+                    that.text("Rename") 
+                        .parents('div')
+                        .siblings('div')
+                        .children('.ctgy-name')
+                        .html( ctgy_nm_val ).end().end().end()
+                } else {
+                    that.text("Update")     
+                        .parents('div')
+                        .siblings('div')
+                        .children('.ctgy-name')
+                        .html( input ).end().end().end()
+                }
                 e.preventDefault();
             });
         }
