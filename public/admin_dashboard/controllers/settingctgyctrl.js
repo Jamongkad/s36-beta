85192d2668b53837894d5607b938e6ec796edf28
jamongkad <wrm932@gmail.com>
2013-07-29 15:06:46 +0800

    wq

--- public/admin_dashboard/controllers/settingctgyctrl.js
+++ public/admin_dashboard/controllers/settingctgyctrl.js
@@ -11,7 +11,8 @@ function SettingCtgyCtrl($scope, Category) {
         if($scope.category_name == 'undefined' || $scope.category_name == null)  {
             alert("Please provide a category name.");
         } else { 
-            var result = Category.write($scope.category_name);
+            Category.write($scope.category_name);
+            var result = Category.write_result;
             $scope.category_name = null;
             console.log(result);
         }
