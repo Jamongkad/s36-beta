513f0e298b02cfab15d77e9f23a48953903ad55d
jamongkad <wrm932@gmail.com>
2013-07-31 16:49:17 +0800

    google

--- public/admin_dashboard/directives/myfeedbackcontrol.js
+++ public/admin_dashboard/directives/myfeedbackcontrol.js
@@ -41,6 +41,16 @@ angular.module('feedbackcontrol', [])
                                 if(data.origin == "publish") { 
                                     hide_the_children(me);
                                     $(".checky-box-container").show();
+                                } else { 
+                                    var state = { 
+                                        activate: {'background-position': '-64px -31px'}
+                                      , deactivate_sibling: {'background-position': '-32px 0px'}
+                                      , activation_color: 'featured'
+                                      , state_change_inbox: '{"status": "inbox", "id": ' + data.id + ', "catid": ' + data.catid + ', "origin": "feature"}'
+                                      , state_change: '{"status": "publish", "id": ' + data.id + ', "catid": ' + data.catid + ', "origin": "publish"}'
+                                      , present_status: "feature"
+                                    }
+                                    published_state(me, '.publish', 'Publish Feedback', state);
                                 }
                             }
 
