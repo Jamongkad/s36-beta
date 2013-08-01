766ec4d103fa91a9a5518aef47c864f16cad978b
jamongkad <wrm932@gmail.com>
2013-08-01 17:35:48 +0800

    wq

--- application/models/feedback/repositories/dbfeedback.php
+++ application/models/feedback/repositories/dbfeedback.php
@@ -929,7 +929,7 @@ class DBFeedback extends S36DataObject {
         
         if($feedback) {
             //fuck null is a literal string...
-            if($feedback->attachments != 'null' || !is_null($feedback->attachments)) {
+            if($feedback->attachments != 'null' or !is_null($feedback->attachments)) {
                 $attachments = json_decode($feedback->attachments);
                 Helpers::dump($feedback->attachments);
                 if(property_exists($attachments, 'uploaded_images')) {
