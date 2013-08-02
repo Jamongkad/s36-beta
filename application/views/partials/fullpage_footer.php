d6766a114ae1d00a14c89a857493e9520670352d
jamongkad <wrm932@gmail.com>
2013-08-02 18:38:18 +0800

    wq

--- application/views/partials/fullpage_footer.php
+++ application/views/partials/fullpage_footer.php
@@ -1,6 +1,24 @@
 <?=HTML::script('/admin_dashboard/services/feedbackcontrolservice.js')?>
+<?=HTML::script('/admin_dashboard/services/publictemplate.js')?>
+<?=HTML::script('/admin_dashboard/services/modifyfeedback.js')?>
+<?=HTML::script('/admin_dashboard/services/message.js')?>
+<?=HTML::script('/admin_dashboard/services/feedbacksignal.js')?>
+<?=HTML::script('/admin_dashboard/services/category.js')?>
+
 <?=HTML::script('/admin_dashboard/directives/Components.js')?>
+<?=HTML::script('/admin_dashboard/directives/myreply.js')?>
 <?=HTML::script('/admin_dashboard/directives/myfeedbackcount.js')?>
+<?=HTML::script('/admin_dashboard/directives/myformbuilder.js')?>
+<?=HTML::script('/admin_dashboard/directives/myfeedbackcontrol.js')?>
+<?=HTML::script('/admin_dashboard/directives/mymodifyfeedback.js')?>
+<?=HTML::script('/admin_dashboard/directives/mycategorycontrol.js')?>
+
+<?=HTML::script('/admin_dashboard/controllers/feedbackcontrol.js')?>
+<?=HTML::script('/admin_dashboard/controllers/mainreplyctrl.js')?>
+<?=HTML::script('/admin_dashboard/controllers/settingreplyctrl.js')?>
+<?=HTML::script('/admin_dashboard/controllers/modifyfeedbackcontrol.js')?>
+<?=HTML::script('/admin_dashboard/controllers/settingctgyctrl.js')?>
+
 <?=HTML::script('/admin_dashboard/S36InboxModule.js')?>
 
 </div>
