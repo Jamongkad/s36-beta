01082384c2528ff2e52e09c8cc5a4e5b72876b39
jamongkad <wrm932@gmail.com>
2013-08-02 18:05:04 +0800

    wq

--- public/admin_dashboard/directives/myfeedbackcount.js
+++ public/admin_dashboard/directives/myfeedbackcount.js
@@ -2,7 +2,7 @@ angular.module('feedback', [])
 .directive('feedbackcount', function(FeedbackService) {
     return {
         restrict: 'A'     
-      , link: function(scope, element, attrs) {
+      , link: function($scope, element, attrs) {
             FeedbackService.get_feedback_count();
             var feedback = FeedbackService.feedback;
             var type = $(element).attr("show");
@@ -22,6 +22,7 @@ angular.module('feedback', [])
 
                 if(type == 'msg_topbar') { 
                     if(feedback.msg) {
+                        $scope.$apply();
                         $(element).html("<a href='/inbox/all' inboxclick show='msg'>You have <sup class='count-topbar'>" + feedback.msg + "</sup> new feedback!</a>");
                     }   
                 }
