f9b1328b58e2e3a9b4c4be1f1705a0777332b850
jamongkad <wrm932@gmail.com>
2013-08-02 18:37:02 +0800

    wq

--- application/views/partials/fullpage_footer.php
+++ application/views/partials/fullpage_footer.php
@@ -1,6 +1,8 @@
 <?=HTML::script('/admin_dashboard/services/feedbackcontrolservice.js')?>
 <?=HTML::script('/admin_dashboard/directives/Components.js')?>
 <?=HTML::script('/admin_dashboard/directives/myfeedbackcount.js')?>
+<?=HTML::script('/admin_dashboard/S36InboxModule.js')?>
+
 </div>
 </body>
 </html>
